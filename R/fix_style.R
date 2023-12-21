423022e773035328fedca2ce2da243b5f62a100b
Liz Roten <liz.roten@metc.state.mn.us>
2023-12-21 16:03:13 -0600

    update meta location

--- R/fix_style.R
+++ R/fix_style.R
@@ -2,6 +2,6 @@ if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
   fs::dir_copy("style", "docs/_transportation/style/", overwrite = TRUE)
   fs::dir_copy("style", "docs/_energy/style/", overwrite = TRUE)
   fs::dir_copy("style", "docs/_waste/style/", overwrite = TRUE)
-  fs::dir_copy("style", "docs/meta/style/", overwrite = TRUE)
+  fs::dir_copy("style", "docs/_meta/style/", overwrite = TRUE)
   quit()
 }
