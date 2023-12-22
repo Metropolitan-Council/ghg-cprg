222545ebb2c606a6b458551cfe1adb7ffc7a49b2
Liz Roten <liz.roten@metc.state.mn.us>
2023-12-22 16:24:14 -0600

    create and upload county and ctu zone sets

---
+++ _transportation/data-raw/stl_upload_zone_sets.R
@@ -0,0 +1,50 @@
+# run once
+# Upload county and city geographies to StreetLight, using their API
+source("R/_load_pkgs.R")
+library(streetlightR)
+
+# fetch login email
+login_email <- keyring::key_get("StL_email")
+
+# set key for session
+streetlightR::streetlight_api_key(key = keyring::key_get("StreetLightAPI"))
+
+
+# counties -----
+cprg_counties <- readRDS("R/data/cprg_county.RDS")
+
+# create zone name, passthrough = TRUE
+stl_counties <- cprg_counties %>% 
+  mutate(zone_name = NAME,
+         is_pass = 1) %>% 
+  select(zone_name, is_pass)
+
+# upload counties
+upload_zone_set(
+  login_email = login_email,
+  geom_type = "polygon",
+  zones = stl_counties,
+  zone_set_name = "CPRG_Counties",
+)
+
+
+# ctus -----
+cprg_ctu <- readRDS("R/data/cprg_ctu.RDS")
+
+# create zone name, passthrough = TRUE
+stl_ctu <- cprg_ctu %>% 
+  mutate(zone_name = paste0(CTU_NAME, "_", CTU_CLASS, "_", COUNTY_NAM),
+         is_pass = 1) %>% 
+  select(zone_name, is_pass)
+
+# double check that zone names are unique
+nrow(unique(stl_ctu)) == nrow(cprg_ctu)
+
+# upload
+upload_zone_set(
+  login_email = login_email,
+  geom_type = "polygon",
+  zones = stl_ctu,
+  zone_set_name = "CPRG_CTUs",
+)
+
