2bc2e271cfef4ee4549a75d25f7c65f41b9e3cda
Liz Roten <liz.roten@metc.state.mn.us>
2023-12-26 16:32:30 -0600

    make zones non-passthrough; use name instead of zone_name

--- _transportation/data-raw/stl_upload_zone_sets.R
+++ _transportation/data-raw/stl_upload_zone_sets.R
@@ -15,16 +15,16 @@ cprg_counties <- readRDS("R/data/cprg_county.RDS")
 
 # create zone name, passthrough = TRUE
 stl_counties <- cprg_counties %>% 
-  mutate(zone_name = NAME,
-         is_pass = 1) %>% 
-  select(zone_name, is_pass)
+  mutate(name = NAME,
+         is_pass = 0) %>% 
+  select(name, is_pass)
 
 # upload counties
 upload_zone_set(
   login_email = login_email,
   geom_type = "polygon",
   zones = stl_counties,
-  zone_set_name = "CPRG_Counties",
+  zone_set_name = "CPRG_Counties_NP",
 )
 
 
@@ -33,9 +33,9 @@ cprg_ctu <- readRDS("R/data/cprg_ctu.RDS")
 
 # create zone name, passthrough = TRUE
 stl_ctu <- cprg_ctu %>% 
-  mutate(zone_name = paste0(CTU_NAME, "_", CTU_CLASS, "_", COUNTY_NAM),
-         is_pass = 1) %>% 
-  select(zone_name, is_pass)
+  mutate(name = paste0(CTU_NAME, "_", CTU_CLASS, "_", COUNTY_NAM),
+         is_pass = 0) %>% 
+  select(name, is_pass)
 
 # double check that zone names are unique
 nrow(unique(stl_ctu)) == nrow(cprg_ctu)
@@ -45,6 +45,6 @@ upload_zone_set(
   login_email = login_email,
   geom_type = "polygon",
   zones = stl_ctu,
-  zone_set_name = "CPRG_CTUs",
+  zone_set_name = "CPRG_CTUs_NP",
 )
 
