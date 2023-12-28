08f33ccb6100ae8783ec5524e62431ac99d6eba5
Liz Roten <liz.roten@metc.state.mn.us>
2023-12-28 14:54:50 -0600

    fetch data from separate script

---
+++ _transportation/data-raw/stl_fetch_analyses.R
@@ -0,0 +1,98 @@
+source("R/_load_pkgs.R")
+library(streetlightR)
+
+# fetch login email
+login_email <- keyring::key_get("StL_email")
+
+# set key for session
+streetlightR::streetlight_api_key(key = keyring::key_get("StreetLightAPI"))
+
+
+# fetch results -----
+
+county21 <- readRDS("_transportation/data-raw/analysis_runs/county21.RDS")
+ctu21 <- readRDS("_transportation/data-raw/analysis_runs/ctu21.RDS")
+
+## county -----
+county_status <- check_analysis_status(county21$name) %>% 
+  httr2::resp_body_json()
+
+
+county21_data <- purrr::map(
+  unlist(county_status$analyses[[1]]$metrics),
+  function(x){
+    Sys.sleep(5)
+    streetlightR::get_analysis_data(
+      analysis_name = county21$name,
+      metric = eval(x)
+    ) %>% 
+      mutate(analysis_name = county21$name,
+             metric_group = x) %>% 
+      select(analysis_name, metric_group,
+             everything())
+  }
+)
+
+names(county21_data) <- unlist(county_status$analyses[[1]]$metrics)
+
+saveRDS(county21_data,
+        "_transportation/data-raw/analysis_runs/county21_data.RDS")
+
+
+## CTU -----
+ctu_status <- check_analysis_status(ctu21$name) %>% 
+  httr2::resp_body_json()
+
+
+ctu21_data <- purrr::map(
+  unlist(ctu_status$analyses[[1]]$metrics),
+  function(x){
+    Sys.sleep(5)
+    streetlightR::get_analysis_data(
+      analysis_name = ctu21$name,
+      metric = eval(x)
+    ) %>% 
+      mutate(analysis_name = ctu21$name,
+             metric_group = x) %>% 
+      select(analysis_name, metric_group,
+             everything())
+  }
+)
+
+
+names(ctu21_data) <- unlist(ctu_status$analyses[[1]]$metrics)
+
+saveRDS(
+  ctu21_data,
+  "_transportation/data-raw/analysis_runs/ctu21_data.RDS")
+
+## truck ----
+
+county21_truck <- readRDS("_transportation/data-raw/analysis_runs/county21_truck.RDS")
+
+
+county21_truck_status <- check_analysis_status(county21_truck$name) %>% 
+  httr2::resp_body_json()
+
+
+county21_truck_data <- purrr::map(
+  unlist(county21_truck_status$analyses[[1]]$metrics),
+  function(x){
+    Sys.sleep(5)
+    streetlightR::get_analysis_data(
+      analysis_name = county21_truck$name,
+      metric = eval(x)
+    ) %>% 
+      mutate(analysis_name = county21_truck$name,
+             metric_group = x) %>% 
+      select(analysis_name, metric_group,
+             everything())
+  }
+)
+
+
+names(county21_truck_data) <- unlist(county21_truck_status$analyses[[1]]$metrics)
+
+saveRDS(
+  county21_truck_data,
+  "_transportation/data-raw/analysis_runs/county21_truck_data.rds")
