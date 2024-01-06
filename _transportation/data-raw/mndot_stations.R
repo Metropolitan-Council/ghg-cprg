4676dc05d76af367d5102348fc4340aa8827ddb7
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-05 18:32:06 -0600

    fix issue with GDAL
    note fix in code comments

--- _transportation/data-raw/mndot_stations.R
+++ _transportation/data-raw/mndot_stations.R
@@ -8,13 +8,10 @@ traffic_ratios <- readRDS(paste0("_transportation/data-raw/mndot/most_recent_yea
 tmp_dir <- tempdir()
 tmp_file <- tempfile()
 download.file("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dot/trans_aadt_traffic_count_locs/gpkg_trans_aadt_traffic_count_locs.zip",
-  destfile = tmp_file
+              destfile = tmp_file
 )
 unzip(tmp_file, exdir = tmp_dir)
-list.files(tmp_dir)
-locations <- sf::read_sf(paste0(tmp_dir, "/trans_aadt_traffic_count_locs.gpkg")) %>%
-  sf::st_set_crs(26915)
-rm(tmp_dir, tmp_file)
+locations <- sf::read_sf(paste0(tmp_dir, "/trans_aadt_traffic_count_locs.gpkg"))
 
 ## convert to table
 locations_table <- as.data.table(locations) %>%
@@ -51,48 +48,52 @@ station_list <- readxl::read_xlsx("_transportation/data-raw/mndot/Current_CC_Sta
   )
 
 ## join station list with traffic ratios, locations
-stations_ratios <- inner_join(station_list, traffic_ratios,
+stations_ratios <- inner_join(
+  # note that station_id  = continuous number
+  station_list, traffic_ratios,
   by = c("continuous_number" = "station_id")
 ) %>%
   mutate(sequence_n = as.double(sequence_number)) %>%
   left_join(locations_table, by = "sequence_n") %>%
   st_as_sf()
-# st_transform(crs = 4326) # project to web mercator
 
 
 # read in AADT
-tmp_dir <- tempdir()
-tmp_file <- tempfile()
 download.file("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dot/trans_aadt_traffic_segments/gpkg_trans_aadt_traffic_segments.zip",
-  destfile = tmp_file
+              destfile = tmp_file
 )
 unzip(tmp_file, exdir = tmp_dir)
-list.files(tmp_dir)
 aadt <- sf::read_sf(paste0(tmp_dir, "/trans_aadt_traffic_segments.gpkg")) %>%
-  sf::st_set_crs(26915) %>%
   mutate(aadt_vol = CURRENT_VO)
 rm(tmp_dir, tmp_file)
 
 ## filter AADT (line) and select cols
 aadt_filtered <- aadt %>%
-  filter(SEQUENCE_N %in% traffic_ratios$sequence_number) %>%
+  filter(SEQUENCE_N %in% stations_ratios$sequence_number) %>%
   select(SEQUENCE_N, ROUTE_LABE, LOCATION_D, CURRENT_YE, CURRENT_VO) %>%
-  as.data.table() %>%
-  clean_names()
-
-
-## join AADT filtered and station ratios. Output is lines
-stations_ratios_aadt <- inner_join(aadt_filtered, stations_ratios,
-  by = "sequence_n"
-) %>%
-  st_as_sf() %>%
-  select(sequence_n, continuous_number, passenger, medium_duty, heavy_duty,
-    current_volume = current_vo.y,
-    geom = geom.x
-  ) %>%
-  st_cast(to = "LINESTRING") %>%
+  clean_names() %>%
+  st_cast(to = "LINESTRING", warn = FALSE) %>%
   st_zm()
 
 
+## join AADT filtered and station ratios. Output is lines
+stations_ratios_aadt <- left_join(
+  aadt_filtered, 
+  stations_ratios %>% 
+    select(sequence_n, continuous_number,
+           passenger, medium_duty, heavy_duty) %>% 
+    sf::st_drop_geometry(),
+  by = "sequence_n"
+) %>%
+  select(sequence_n, continuous_number, passenger, medium_duty, heavy_duty,
+         current_volume = current_vo
+  ) %>% 
+  st_transform(4326)
+
+
 ## export
-saveRDS(stations_ratios_aadt, paste0("data/", date, "_2019_plus_stations_ratios.RDS"))
+saveRDS(stations_ratios_aadt, paste0("_transportation/data-raw/mndot/2021_plus_stations_ratios.RDS"))
+
+## note, if you are getting "GDAL Error 1: PROJ: proj_create_from_database: Cannot find proj.db"
+## and are on a Mac,
+## try setting  PROJ_LIB=/usr/local/share/proj in your .Renviron
