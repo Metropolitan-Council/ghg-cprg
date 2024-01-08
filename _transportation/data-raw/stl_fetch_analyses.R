27d141d23f600e155789ab276760d25596ca7fb8
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-08 15:54:03 -0600

    save faux calibrated data and actual calibrated data

--- _transportation/data-raw/stl_fetch_analyses.R
+++ _transportation/data-raw/stl_fetch_analyses.R
@@ -112,3 +112,79 @@ saveRDS(
   county21_truck_data,
   "_transportation/data-raw/analysis_runs/county21_truck_data.rds"
 )
+
+
+
+## truck calibrated ----
+
+county21_truck_calib <- readRDS("_transportation/data-raw/analysis_runs/county21_truck_calib.RDS")
+
+
+county21_truck_calib_status <- check_analysis_status(county21_truck_calib$name) %>%
+  httr2::resp_body_json()
+
+
+county21_truck_calib_data <- purrr::map(
+  unlist(county21_truck_calib_status$analyses[[1]]$metrics),
+  function(x) {
+    Sys.sleep(5)
+    streetlightR::get_analysis_data(
+      analysis_name = county21_truck_calib$name,
+      metric = eval(x)
+    ) %>%
+      mutate(
+        analysis_name = county21_truck_calib$name,
+        metric_group = x
+      ) %>%
+      select(
+        analysis_name, metric_group,
+        everything()
+      )
+  }
+)
+
+
+names(county21_truck_calib_data) <- unlist(county21_truck_calib_status$analyses[[1]]$metrics)
+
+saveRDS(
+  county21_truck_calib_data,
+  "_transportation/data-raw/analysis_runs/county21_truck_calib_data.rds"
+)
+
+## truck single-factor calibrated ----
+
+county21_truck_sfcalib <- "CPRG_County_OD_NP_Truck_SFCalib_2021"
+
+
+county21_truck_calib_status <- check_analysis_status(county21_truck_sfcalib) %>%
+  httr2::resp_body_json()
+
+county21_truck_calib_status
+
+
+county21_truck_calib_data <- purrr::map(
+  unlist(county21_truck_calib_status$analyses[[1]]$metrics),
+  function(x) {
+    Sys.sleep(5)
+    streetlightR::get_analysis_data(
+      analysis_name = county21_truck_calib$name,
+      metric = eval(x)
+    ) %>%
+      mutate(
+        analysis_name = county21_truck_calib$name,
+        metric_group = x
+      ) %>%
+      select(
+        analysis_name, metric_group,
+        everything()
+      )
+  }
+)
+
+
+names(county21_truck_calib_data) <- unlist(county21_truck_calib_status$analyses[[1]]$metrics)
+
+saveRDS(
+  county21_truck_calib_data,
+  "_transportation/data-raw/analysis_runs/county21_truck_sfcalib_data.rds"
+)
--- _transportation/data-raw/stl_run_analyses.R
+++ _transportation/data-raw/stl_run_analyses.R
@@ -81,3 +81,31 @@ county21_truck <- create_streetlight_analysis(
 
 # save analysis identifying information
 saveRDS(county21_truck, "_transportation/data-raw/analysis_runs/county21_truck.RDS")
+
+
+
+
+county21_truck_calib <- create_streetlight_analysis(
+  login_email = login_email,
+  analysis_name = "CPRG_County_OD_NP_Truck_Calib_2021",
+  analysis_type = "OD_Analysis",
+  origin_zone_set = "CPRG_Counties_NP",
+  destination_zone_set = "CPRG_Counties_NP",
+  calibration_zone_set = "CPRG_Freight_Calibration_GateEdit",
+  travel_mode_type = "Truck",
+  output_type = "index",
+  trip_attributes = TRUE,
+  traveler_attributes = TRUE,
+  date_ranges = list(
+    start_date = "01/01/2021",
+    end_date = "12/31/2021"
+  ),
+  tags = c(
+    "streetlightR",
+    "CPRG"
+  )
+)
+
+county21_truck_calib
+# save analysis identifying information
+saveRDS(county21_truck_calib, "_transportation/data-raw/analysis_runs/county21_truck_calib.RDS")
