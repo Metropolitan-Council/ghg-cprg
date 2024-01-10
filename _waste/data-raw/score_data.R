0a24b22ff779c6a8891c1f6bec15614c1d5ea7b6
zeyandell <zeyandell@gmail.com>
2024-01-10 14:01:40 -0600

    import emissions factors
    + other mn counties from score

--- _waste/data-raw/score_data.R
+++ _waste/data-raw/score_data.R
@@ -6,8 +6,19 @@ library(readr)
 
 score_summary <- read_csv("_waste/data-raw/score_summary.csv")
 
+# replace with general-use list
+mn_counties <- c("Anoka", 
+                 "Carver",
+                 "Chisago",
+                 "Dakota",
+                 "Hennepin",
+                 "Ramsey",
+                 "Scott",
+                 "Sherburne",
+                 "Washington")
+
 score_filtered <- score_summary %>%
-  filter(Region == "Metropolitan Area",
+  filter(County %in% mn_counties,
          Year == "2021") %>%
   select(County,
          "Management Category" ="Mangement Method",
@@ -16,3 +27,18 @@ score_filtered <- score_summary %>%
          Tons)
 
 # append emissions factors
+emissions_factors <- readxl::read_xlsx("_waste/data-raw/ghg-emission-factors-hub-2021.xlsx")
+
+emissions_factors_cleaned <- emissions_factors %>%
+  select(2:8)%>%
+  slice(397:457)%>%
+  row_to_names(row_number = 1)%>%
+  rename(Recycled = RecycledA,
+         Landfilled = LandfilledB,
+         Combusted = CombustedC,
+         Composted = CompostedD)
+
+# export
+saveRDS(score_filtered, paste0("_waste/data-raw/mpca_score_waste.RDS"))
+saveRDS(emissions_factors_cleaned, paste0("_waste/data-raw/waste_emissions_factors.RDS"))
+
