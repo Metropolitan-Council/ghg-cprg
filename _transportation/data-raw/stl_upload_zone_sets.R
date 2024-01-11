75e19566f370c914a148cd521f9675d5bea6111c
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-11 16:19:55 -0600

    separate out calibration data creation; save with metadata

--- _transportation/data-raw/stl_upload_zone_sets.R
+++ _transportation/data-raw/stl_upload_zone_sets.R
@@ -53,62 +53,9 @@ upload_zone_set(
 )
 
 
-# freight calibration zone set -----
+# commercial calibration zone set -----
+commercial_calibration_lines <- readRDS("_transportation/data/commercial_calibration_lines.RDS")
 
-mn_stations_lines <- readRDS("_transportation/data-raw/mndot/mn_stations_ratios.RDS") %>%
-  mutate(
-    name = paste0("MN_", sequence_n, "_", continuous_number),
-    year = as.numeric(year)
-  ) %>%
-  select(name, year, passenger, medium_duty, heavy_duty, current_volume, geometry = geom)
-
-wi_stations_lines <- readRDS("_transportation/data-raw/wisdot/wi_stations_ratios.RDS") %>%
-  mutate(name = paste0("WI_", site_id)) %>%
-  select(name, year, passenger, medium_duty, heavy_duty, current_volume, geometry)
-
-freight_calibration_lines <- bind_rows(
-  mn_stations_lines,
-  wi_stations_lines
-) %>%
-  mutate(
-    is_pass = 1,
-    is_bidi = 1,
-    calibration_type = "AADT",
-    personal_traffic_ratio = round(passenger, 2),
-    medium_commercial_ratio = round(medium_duty, 2),
-    heavy_commercial_ratio = round(heavy_duty, 2)
-  ) %>%
-  # streetlight requires that all the ratios total up to EXACTLY 1
-  # we will start by adjusting the personal traffic ratio +/- 0.01
-  # then adjust the medium duty +/- 0.01 until we get a total of 1
-  rowwise() %>%
-  mutate(total = sum(
-    personal_traffic_ratio,
-    medium_commercial_ratio,
-    heavy_commercial_ratio
-  )) %>%
-  mutate(personal_traffic_ratio = case_when(
-    total == 1 ~ personal_traffic_ratio,
-    total > 1 ~ personal_traffic_ratio - 0.01,
-    total < 1 ~ personal_traffic_ratio + 0.01
-  )) %>%
-  mutate(total = sum(
-    personal_traffic_ratio,
-    medium_commercial_ratio,
-    heavy_commercial_ratio
-  )) %>%
-  mutate(medium_commercial_ratio = case_when(
-    total == 1 ~ medium_commercial_ratio,
-    total > 1 ~ medium_commercial_ratio - 0.01,
-    total < 1 ~ medium_commercial_ratio + 0.01
-  )) %>%
-  mutate(total = sum(
-    personal_traffic_ratio,
-    medium_commercial_ratio,
-    heavy_commercial_ratio
-  ))
-
-saveRDS(freight_calibration_lines, "_transportation/data-raw/freight_calibration_lines.RDS")
 
 # one last thing we need is to calculate the angle or  direction
 # https://stackoverflow.com/questions/72276233/calculate-the-angle-between-two-sf-points-in-r-to-calculate-the-direction-of-roa
@@ -119,29 +66,28 @@ to_degrees <- function(rad) {
   ((rad * 180) / (pi)) %% 359
 }
 
-freight_calibration_line_upload <- freight_calibration_lines %>%
+commercial_calibration_line_upload <- commercial_calibration_lines %>%
   # transform into sfnetwork object
   sfnetworks::as_sfnetwork() %>%
   # activate edges
   sfnetworks::activate("edges") %>%
   # calculate direction
   mutate(direction = sfnetworks::edge_azimuth() %>%
-    as.numeric() %>%
-    to_degrees()) %>%
+           as.numeric() %>%
+           to_degrees()) %>%
   st_as_sf() %>%
   select(name, is_pass, direction, is_bidi,
-    calibration_type,
-    calibration_value = current_volume,
-    personal_traffic_ratio, medium_commercial_ratio,
-    heavy_commercial_ratio
+         calibration_type,
+         calibration_value = current_volume,
+         personal_traffic_ratio, medium_commercial_ratio,
+         heavy_commercial_ratio
   )
-
 
 # upload zone set
 upload_zone_set(
   login_email = login_email,
   geom_type = "line",
-  zones = freight_calibration_line_upload,
+  zones = commercial_calibration_line_upload,
   zone_set_name = "CPRG_Freight_Calibration_GateEdit",
   with_calibration = TRUE
 )
