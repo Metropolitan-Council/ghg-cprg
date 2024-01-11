2e641cbc03ea99f45581cea14a78a0fada8ebaa4
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-11 14:42:24 -0600

    write tests for county level population

---
+++ tests/test-cprg-population.R
@@ -0,0 +1,38 @@
+testthat::test_that("County population is as expected", {
+  cprg_population <- readRDS(file.path(here::here(), "R/data/cprg_population.RDS"))
+
+  testthat::expect_equal(nrow(cprg_population), 11)
+  testthat::expect_equal(
+    cprg_population$NAME,
+    c(
+      "Anoka", "Carver", "Chisago", "Dakota", "Hennepin", "Ramsey",
+      "Scott", "Sherburne", "Washington", "Pierce", "St. Croix"
+    )
+  )
+
+  testthat::expect_equal(names(cprg_population), c(
+    "STATE", "STATEFP", "COUNTYFP", "GEOID",
+    "NAME", "NAMELSAD", "population", "population_data_source"
+  ))
+
+  testthat::expect_equal(cprg_population$STATE %>% unique(), c("Minnesota", "Wisconsin"))
+
+  # these values were found on the Census web data viewer
+  # check that they are correct
+
+  cprg_population %>%
+    dplyr::filter(NAME == "Hennepin") %>%
+    magrittr::extract2("population") %>%
+    testthat::expect_equal(1270283)
+
+
+  cprg_population %>%
+    dplyr::filter(NAME == "Ramsey") %>%
+    magrittr::extract2("population") %>%
+    testthat::expect_equal(549377)
+
+  cprg_population %>%
+    dplyr::filter(NAME == "Pierce") %>%
+    magrittr::extract2("population") %>%
+    testthat::expect_equal(42204)
+})
