ccb2d4017fe5617d19eb8bcd371387fb6be8f402
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-19 12:49:26 -0600

    update test name

--- tests/test-transportation.R
+++ tests/test-transportation.R
@@ -1,4 +1,4 @@
-testthat::test_that("1+1 == 2", {
+testthat::test_that("Correct number of calibration zone sets", {
   commercial_calibration_lines <- readRDS(file.path(here::here(), "_transportation/data/commercial_calibration_lines.RDS"))
 
   testthat::expect_equal(nrow(commercial_calibration_lines), 27)
