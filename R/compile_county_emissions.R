c3f5e8f53cdc74ccb5960941c1a92e0963efd38a
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-24 12:37:13 -0600

    style; fix minor bugs

--- R/compile_county_emissions.R
+++ R/compile_county_emissions.R
@@ -2,25 +2,29 @@
 source("R/_load_pkgs.R")
 
 # transportation -----
-transportation_emissions <- readRDS("_transportation/data/county_vmt_emissions.RDS") %>% 
-  ungroup() %>% 
-  rowwise() %>% 
-  mutate(sector = "Transportation",
-         geog_level = "county",
-         geog_name = zone,
-         category = stringr::str_to_sentence(vehicle_type),
-         source = paste0(vehicle_weight_label, " vehicles"),
-         data_source = "StreetLight Data",
-         factor_source = paste0("EPA MOVES (", moves_year, ")")) %>% 
-  select(year,
-         geog_level,
-         geog_name,
-         sector,
-         category,
-         source,
-         emissions_metric_tons_co2e,
-         data_source,
-         factor_source)
+transportation_emissions <- readRDS("_transportation/data/county_vmt_emissions.RDS") %>%
+  ungroup() %>%
+  rowwise() %>%
+  mutate(
+    sector = "Transportation",
+    geog_level = "county",
+    geog_name = zone,
+    category = stringr::str_to_sentence(vehicle_type),
+    source = paste0(vehicle_weight_label, " vehicles"),
+    data_source = "StreetLight Data",
+    factor_source = paste0("EPA MOVES (", moves_year, ")")
+  ) %>%
+  select(
+    year,
+    geog_level,
+    geog_name,
+    sector,
+    category,
+    source,
+    emissions_metric_tons_co2e,
+    data_source,
+    factor_source
+  )
 
 # waste -----
 ## wastewater ----
@@ -32,32 +36,34 @@ transportation_emissions <- readRDS("_transportation/data/county_vmt_emissions.R
 
 # combine and write metadata----
 
-emissions_all <- bind_rows(transportation_emissions) %>% 
+emissions_all <- bind_rows(transportation_emissions) %>%
   mutate(source = factor(source,
-                         c(
-                           # transportation levels
-                           "Light-duty vehicles",
-                           "Medium-duty vehicles",
-                           "Heavy-duty vehicles"
-                           # waste levels
-                           # energy levels
-                           ),
-                         ordered = TRUE))
+    c(
+      # transportation levels
+      "Light-duty vehicles",
+      "Medium-duty vehicles",
+      "Heavy-duty vehicles"
+      # waste levels
+      # energy levels
+    ),
+    ordered = TRUE
+  ))
 
 emissions_all_meta <- tibble::tribble(
   ~"Column", ~"Class", ~"Description",
   "year", class(emissions_all$year), "Emissions estimation year",
   "geog_level", class(emissions_all$geog_level), "Geography level; city or county",
   "geog_name", class(emissions_all$geog_name), "Name of geographic area",
-  "sector", class(emissions_all$sector), paste0("Emissions sector. One of ", 
-                                                paste0(unique(emissions_all$sector), collapse = ", ")),
+  "sector", class(emissions_all$sector), paste0(
+    "Emissions sector. One of ",
+    paste0(unique(emissions_all$sector), collapse = ", ")
+  ),
   "category", class(emissions_all$category), "Category of emissions within given sector",
   "source", class(emissions_all$source), "Source of emissions. Most detailed sub-category in this table",
   "emissions_metric_tons_co2e", class(emissions_all$emissions_metric_tons_co2e), "Annual total metric tons CO~2~ and CO~2~ equivalent attributed to the given geography for given year",
-  "data_source",  class(emissions_all$data_source), "Activity data source",
-  "factor_source",  class(emissions_all$factor_source), "Emissions factor data source"
+  "data_source", class(emissions_all$data_source), "Activity data source",
+  "factor_source", class(emissions_all$factor_source), "Emissions factor data source"
 )
 
 saveRDS(emissions_all, "R/data/cprg_county_emissions.RDS")
 saveRDS(emissions_all_meta, "R/data/cprg_county_emissions_meta.RDS")
-
--- R/plot_county_emissions.R
+++ R/plot_county_emissions.R
@@ -8,47 +8,45 @@
 #'
 #' @return Plotly object
 #' @export
-#' 
+#'
 #' @importFrom plotly plot_ly layout
-#' @importFrom dplyr filter 
+#' @importFrom dplyr filter
 #' @importFrom stringr str_to_title
 #' @importFrom councilR plotly_layout
 plot_county_emissions <- function(county_emissions,
                                   .sector,
-                                  .plotly_source){
-  
-  plot_data <- county_emissions %>% 
+                                  .plotly_source) {
+  plot_data <- county_emissions %>%
     dplyr::filter(sector == .sector)
-  
-  if(nrow(plot_data) == 0){
+
+  if (nrow(plot_data) == 0) {
     return(plot_ly(
       source = .plotly_source,
       type = "bar"
     ))
   }
-  
+
   plot_ly(
     data = plot_data,
     type = "bar",
-    source = plotly_source,
+    source = .plotly_source,
     x = ~geog_name,
     y = ~emissions_metric_tons_co2e,
     color = ~source,
     split = ~source,
     hovertemplate = ~ paste0(
       geog_name, " County", "<br>",
-      sector,  " - ", category, ", ", source, "<br>",
+      sector, " - ", category, ", ", source, "<br>",
       round(emissions_metric_tons_co2e * 1e-6, digits = 2), " million metric tons CO<sub>2</sub>e", "<br>",
       "<extra></extra>"
     )
-  ) %>% 
+  ) %>%
     councilR::plotly_layout(
-      main_title = paste0(sector, " emissions"),
+      main_title = paste0(.sector, " emissions"),
       subtitle = "",
       x_title = stringr::str_to_title(unique(plot_data$geog_level)),
       y_title = "Metric tons CO<sub>2</sub>e",
       legend_title = "Category"
-    ) %>% 
+    ) %>%
     plotly::layout(barmode = "stack")
-  
 }
