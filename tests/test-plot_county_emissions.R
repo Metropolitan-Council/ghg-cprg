1a3282d6cc3550d2df54f5eae2bbaaa663e79f3a
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-24 12:36:47 -0600

    write tests for plot_county_emissions

---
+++ tests/test-plot_county_emissions.R
@@ -0,0 +1,28 @@
+testthat::test_that("Correct plotly configurations", {
+  source(file.path(here::here(), "R/plot_county_emissions.R"))
+
+  county_emissions <- readRDS(file.path(here::here(), "R/data/cprg_county_emissions.RDS"))
+
+  # transportation
+  transpo_plot <- plot_county_emissions(county_emissions,
+    .sector = "Transportation",
+    .plotly_source = "testing"
+  )
+
+  testthat::expect_equal(class(transpo_plot)[[1]], "plotly")
+  # check title
+  testthat::expect_equal(
+    transpo_plot[["x"]][["layoutAttrs"]][[1]][["title"]][["text"]],
+    "Transportation emissions"
+  )
+  # check xaxis title
+  testthat::expect_equal(
+    transpo_plot[["x"]][["layoutAttrs"]][[1]][["xaxis"]][["title"]][["text"]],
+    "County"
+  )
+  # check yaxis title
+  testthat::expect_equal(
+    transpo_plot[["x"]][["layoutAttrs"]][[1]][["yaxis"]][["title"]][["text"]],
+    "Metric tons CO<sub>2</sub>e"
+  )
+})
