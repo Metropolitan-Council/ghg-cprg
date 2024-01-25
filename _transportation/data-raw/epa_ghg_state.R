fd718400b91bc6620da1d0fa292ef7f9a4032dc8
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-25 16:51:38 -0600

    start working on state GHG emissions and removals dataset
    not sure if we need this, but will get it started anyway

---
+++ _transportation/data-raw/epa_ghg_state.R
@@ -0,0 +1,33 @@
+# get data from the 2020 national emissions inventory
+source("R/_load_pkgs.R")
+cprg_county <- readRDS("R/data/cprg_county.RDS")
+
+
+# ipcc sectors -----
+ipcc_sectors <- readxl::read_xlsx("_transportation/data-raw/epa/state_ghg/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx",
+                  sheet = 2) %>% 
+  clean_names()
+
+
+ipcc <- ipcc_sectors %>% 
+  filter(state %in% c("MN", "WI"),
+         category == "Transportation") %>% 
+  pivot_longer(starts_with("y"),
+               names_to = "year",
+               values_to = "value") %>% 
+  mutate(emissions_metric_tons_co2e = value*1000000)
+
+
+# economic sectors -----
+econ_sectors <- readxl::read_xlsx("_transportation/data-raw/epa/state_ghg/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx",
+                                  sheet = 3) %>% 
+  clean_names()
+
+econ <- econ_sectors %>% 
+  filter(state %in% c("MN", "WI")) %>% 
+  pivot_longer(starts_with("y"),
+               names_to = "year",
+               values_to = "value") %>% 
+  filter(econ_sector == "Transportation") %>% 
+  mutate(emissions_metric_tons_co2e = value*1000000)
+
