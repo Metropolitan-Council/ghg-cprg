83c84c51ea3bcb30bf88a648a420cfbbef76ee6c
Laine McNamara <70707631+mcnamx@users.noreply.github.com>
2024-01-25 13:49:34 -0600

    update csvs

--- _meta/data-raw/ghg_inv_2018_processing.R
+++ _meta/data-raw/ghg_inv_2018_processing.R
@@ -1,7 +1,12 @@
 source("R/_load_pkgs.R")
 
+# load in data from GHG Inventory 2018 tableau workbooks
 ctus_summary_2018 <- readr::read_csv("_meta/data-raw/ctus_summary_2018.csv")
-Fields_Formulas_GHGInv1 <- readxl::read_excel("_meta/data-raw/Fields_Formulas_GHGInv1.xlsx")
+# load in field names, metadata etc.
+Fields_Formulas_GHGInv1 <- readr::read_csv("_meta/data-raw/Fields_Formulas_GHGInv1.csv")
+sector_breakdown <- readr::read_csv("_meta/data-raw/sector_breakdown.csv")
+
+# manipulate metadata 
 select_fields_v1 <- Fields_Formulas_GHGInv1 %>% filter(Keep_YN == 'Y') %>%
   mutate(units = case_when(str_detect(ctus_summary_field, fixed('Co2')) ~ 'Tonnes CO2e',
                            str_detect(ctus_summary_field, fixed('CO2e')) ~ 'Tonnes CO2e',
@@ -20,6 +25,7 @@ select_fields_v1 <- Fields_Formulas_GHGInv1 %>% filter(Keep_YN == 'Y') %>%
                                    .default = 'Transportation'
                                 )
          )
+
 
 data_2018 <- select(ctus_summary_2018, all_of(select_fields_v1[[1]])) %>%
   pivot_longer(cols = !(filter(select_fields_v1, Activity_Emissions %in% c('Metadata', 'Demographics'))[[1]]),
