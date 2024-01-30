7592df399d5e9af6c4adc014b4c9886debee45c4
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-30 12:33:05 -0600

    process save as metc_wastewater

--- _waste/data-raw/wastewater/metc_wastewater.R
+++ _waste/data-raw/wastewater/metc_wastewater.R
@@ -5,7 +5,7 @@ cprg_ctu <- readRDS("_meta/data/cprg_ctu.RDS")
 ww_raw <- readxl::read_xlsx("_waste/data-raw/wastewater/GHGEmissions_2019-2021_CorrectedWSCA.xlsx") %>% 
   clean_names()
 
-metro_ww <- ww_raw %>% 
+metc_ww <- ww_raw %>% 
   select(everything(),
          CTU_NAME = city_or_township,
          scope = emissions_scope,
@@ -16,8 +16,16 @@ metro_ww <- ww_raw %>%
                values_to = "emissions_metric_tons_co2e") %>% 
   mutate(CTU_NAME = stringr::str_replace(CTU_NAME, "St. ", "Saint "),
           CTU_NAME =  case_when(CTU_NAME == "White Bear Twp." ~ "White Bear",
+                                CTU_NAME == "Laketown Twp." ~ "Laketown",
                      TRUE ~ CTU_NAME)
-         )
+         ) %>% 
+  left_join(cprg_ctu %>% 
+              sf::st_drop_geometry() %>% 
+              select(CTU_NAME, CTU_CLASS,
+                     COUNTY_NAM, STATE,
+                     STATEFP), 
+            relationship = "many-to-many",
+            by = c("CTU_NAME"),
+            multiple = "first")
 
-
-saveRDS(metro_ww, "_waste/data-raw/wastewater/metro_ww.RDS")
+saveRDS(metc_ww, "_waste/data-raw/wastewater/metc_wastewater.RDS")
