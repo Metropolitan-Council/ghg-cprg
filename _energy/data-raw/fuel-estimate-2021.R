17ae92e80e20fb0f9e5fa90abccdf1e718f6092e
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-31 12:21:41 -0600

    harmonize NAME column to match cprg_county

--- _energy/data-raw/fuel-estimate-2021.R
+++ _energy/data-raw/fuel-estimate-2021.R
@@ -1,17 +1,24 @@
 source("R/_load_pkgs.R")
-
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 # source propane and kerosene data
 propane_use <- readRDS(file.path(here::here(), "_energy/data-raw/propane_use_county.RDS")) %>%
   mutate(fuel_type = "Propane")
 kerosene_use <- readRDS(file.path(here::here(), "_energy/data-raw/kerosene_use_county.RDS")) %>%
   mutate(fuel_type = "Kerosene")
 
-#row bind for output data
+# row bind for output data
 fuel_use <- bind_rows(
   propane_use,
   kerosene_use
-) %>% select(NAME,mmBtu,CO2e,fuel_type) %>% 
-  mutate(year = 2021)
+) %>%
+  # select(NAME, mmBtu, CO2e, fuel_type) %>%
+  mutate(year = 2021) %>% 
+  left_join(cprg_county, by = "GEOID") %>% 
+  select(NAME = NAME.y,
+         year,
+         fuel_type,
+         mmBtu,
+         emissions_metric_tons_co2e = CO2e)
 
 names(fuel_use)
 
@@ -19,10 +26,10 @@ fuel_use_meta <-
   tibble::tribble(
     ~"Column", ~"Class", ~"Description",
     "NAME", class(fuel_use$NAME), "County name",
-    "mmBtu", class(fuel_use$mmBtu), "Millions of BTUs generated in county from household fuel use",
-    "CO2e", class(fuel_use$CO2e), "Metric tonnes of CO2 equivalency generated from household fuel use",
+    "year", class(fuel_use$year), "Year",
     "fuel_type", class(fuel_use$fuel_type), "Type of fuel combusted",
-    "year", class(fuel_use$year), "Year"
+    "mmBtu", class(fuel_use$mmBtu), "Millions of BTUs generated in county from household fuel use",
+    "emissions_metric_tons_co2e", class(fuel_use$emissions_metric_tons_co2e), "Metric tonnes of CO2 equivalency generated from household fuel use"
   )
 
 saveRDS(fuel_use, "_energy/data/fuel_use.RDS")
