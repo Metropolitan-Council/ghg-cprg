829a7a3705586a515909d354bde7802d45adc998
Liz Roten <liz.roten@metc.state.mn.us>
2024-01-31 12:21:11 -0600

    changed GWP values from those provided in GHG hub to AR5
    ch4 changed from 25 to 28
    n20 changed from 298 to 265
    this is important for consistency across the modules

--- _energy/data-raw/kerosene-estimate-2021.R
+++ _energy/data-raw/kerosene-estimate-2021.R
@@ -1,49 +1,73 @@
 source("R/_load_pkgs.R")
-source("R/cprg_geography.R")
+source("_meta/data-raw/global_warming_potential.R")
 
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 # read in efficiency factors
-eff_fac <- readxl::read_excel('_energy/data-raw/ghg-emission-factors-hub-2021.xlsx')
+eff_fac <- readxl::read_excel("_energy/data-raw/ghg-emission-factors-hub-2021.xlsx")
 
 ### poor formatting but the co2e for kerosene is:
-kero_ef <- as.numeric(eff_fac %>% filter(...2 == 'Kerosene') %>% select(...4)) + #CO2 emissions per mmBtu of kerosene used
-  as.numeric(eff_fac %>% filter(...2 == 'Kerosene') %>% select(...5)) * as.numeric(eff_fac[6,4]) + #methane emissions per mmBtu kerosene scale to CO2 equivalency
-  as.numeric(eff_fac %>% filter(...2 == 'Kerosene') %>% select(...6)) * as.numeric(eff_fac[7,4]) #n20 emissions per mmBtu kerosene scale to CO2 equivalency
+kero_ef <- 
+  # CO2 emissions per mmBtu of kerosene used PLUS
+  as.numeric(eff_fac %>% filter(...2 == "Kerosene") %>% select(...4)) + 
+  # methane emissions per mmBtu kerosene scale to CO2 equivalency PLUS
+  as.numeric(eff_fac %>% filter(...2 == "Kerosene") %>% select(...5)) * gwp$ch4 + 
+  # n20 emissions per mmBtu kerosene scale to CO2 equivalency
+  as.numeric(eff_fac %>% filter(...2 == "Kerosene") %>% select(...6)) * gwp$n2o 
 
-# kerosene mmBtu generation estimates are not provided at the state level for MN and WI due to inadequate sample size. Best approximation I can see is regional usage rates, which may skew high
-eia2020 <-  readxl::read_excel('_energy/data-raw/eia-recs-region-2020.xlsx')
+# kerosene mmBtu generation estimates are not provided at the state level for MN and WI due to inadequate sample size. 
+# Best approximation I can see is regional usage rates, which may skew high
+# source: https://www.eia.gov/consumption/residential/data/2020/state/pdf/ce2.1.st.pdf
+eia2020 <- read.csv("_energy/data-raw/eia-recs-region-2020.csv")
 
 
 # these are the estimated per household values of million btu generation for households that use kerosene or fuel oil.
 # WI and MN are in different subregions of Midwest, so using different estimates for each of those
-mn_kero_use <- as.numeric(eia2020[7,12])
-wi_kero_use <- as.numeric(eia2020[6,12])
+mn_kero_use <- as.numeric(eia2020[7, 12])
+wi_kero_use <- as.numeric(eia2020[6, 12])
 
 ### look up codes in ACS
-load_variables(year = 2020, dataset = "acs5") %>% mutate(concept_short = substr(concept,1,10)) %>% distinct(concept_short) %>% print(n=10000)
-#### house heating fuel
-v_heat<- load_variables(year = 2021, dataset = "acs5") %>% mutate(concept_short = substr(concept,1,10)) %>% 
-  filter(concept_short == "HOUSE HEAT") %>% print(n=10000)
+load_variables(year = 2020, dataset = "acs5") %>%
+  mutate(concept_short = substr(concept, 1, 10)) %>%
+  distinct(concept_short) %>%
+  print(n = 10000)
 
-#get number of households in each county using propane
-mn_kero_hh <- get_acs(geography = 'county',
-                      variables = 'B25040_005',
-                      state = 'MN',
-                      year = 2021) %>% 
-  filter(GEOID %in% cprg_county$GEOID) %>% 
-  mutate(mmBtu = estimate * as.numeric(mn_kero_use), #multiply average propane use by household be estimated number of households
-         CO2e = mmBtu * kero_ef * 0.001) # multiply mmBtu per county by emissions factor
+#### house heating fuel
+v_heat <- load_variables(year = 2021, dataset = "acs5") %>%
+  mutate(concept_short = substr(concept, 1, 10)) %>%
+  filter(concept_short == "HOUSE HEAT") %>%
+  print(n = 10000)
+
+# get number of households in each county using propane
+mn_kero_hh <- get_acs(
+  geography = "county",
+  variables = "B25040_005",
+  state = "MN",
+  year = 2021
+) %>%
+  filter(GEOID %in% cprg_county$GEOID) %>%
+  mutate(
+    # multiply average propane use by household be estimated number of households
+    mmBtu = estimate * as.numeric(mn_kero_use),
+    # multiply mmBtu per county by emissions factor
+    CO2e = mmBtu * kero_ef * 0.001
+  ) 
 
 # repeat for WI
-wi_kero_hh <- get_acs(geography = 'county',
-                      variables = 'B25040_005',
-                      state = 'WI',
-                      year = 2021) %>% 
-  filter(GEOID %in% cprg_county$GEOID) %>% 
-  mutate(mmBtu = estimate * as.numeric(wi_kero_use), #multiply average propane use by household be estimated number of households
-         CO2e = mmBtu * kero_ef * 0.001) # multiply mmBtu per county by emissions factor and then convert to metric tonnes
-
-#bind data
-kero_county <- rows_append(mn_kero_hh,wi_kero_hh)
+wi_kero_hh <- get_acs(
+  geography = "county",
+  variables = "B25040_005",
+  state = "WI",
+  year = 2021
+) %>%
+  filter(GEOID %in% cprg_county$GEOID) %>%
+  mutate(
+    # multiply average propane use by household be estimated number of households
+    mmBtu = estimate * as.numeric(wi_kero_use), 
+    # multiply mmBtu per county by emissions factor and then convert to metric tonnes
+    CO2e = mmBtu * kero_ef * 0.001
+  ) 
+# bind data
+kero_county <- rows_append(mn_kero_hh, wi_kero_hh)
 kero_county
 
 total_regional_emissions <- sum(kero_county$CO2e) # total regional emissions of the 11 county area
--- _energy/data-raw/propane-estimate-2021.R
+++ _energy/data-raw/propane-estimate-2021.R
@@ -1,50 +1,71 @@
 source("R/_load_pkgs.R")
-source("R/cprg_geography.R")
+source("_meta/data-raw/global_warming_potential.R")
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 
 
 # read in efficiency factors
-eff_fac <- readxl::read_excel('_energy/data-raw/ghg-emission-factors-hub-2021.xlsx')
+eff_fac <- readxl::read_excel("_energy/data-raw/ghg-emission-factors-hub-2021.xlsx")
 # source: https://www.epa.gov/climateleadership/ghg-emission-factors-hub
 ### poor formatting but the co2e for propane is:
-prop_ef <- as.numeric(eff_fac %>% filter(...2 == 'Propane') %>% select(...4)) + #CO2 emissions per mmBtu of propane used
-  as.numeric(eff_fac %>% filter(...2 == 'Propane') %>% select(...5)) * as.numeric(eff_fac[6,4]) + #methane emissions per mmBtu propane scale to CO2 equivalency
-  as.numeric(eff_fac %>% filter(...2 == 'Propane') %>% select(...6)) * as.numeric(eff_fac[7,4]) #n20 emissions per mmBtu propane scale to CO2 equivalency
+prop_ef <- 
+  # CO2 emissions per mmBtu of propane used
+  as.numeric(eff_fac %>% filter(...2 == "Propane") %>% select(...4)) + 
+  # methane emissions per mmBtu propane scale to CO2 equivalency
+  as.numeric(eff_fac %>% filter(...2 == "Propane") %>% select(...5)) * gwp$ch4 +
+  # n20 emissions per mmBtu propane scale to CO2 equivalency
+  as.numeric(eff_fac %>% filter(...2 == "Propane") %>% select(...6)) * gwp$n2o
 
-eia2020 <- read.csv('_energy/data-raw/eia-recs-2020.csv')
+eia2020 <- read.csv("_energy/data-raw/eia-recs-2020.csv")
 # source: https://www.eia.gov/consumption/residential/data/2020/state/pdf/ce2.1.st.pdf
 
 
 # these are the estimated per household values of million btu generation for households that use propane
-mn_prop_use <- eia2020[33,15]
-wi_prop_use <- eia2020[64,15]
+mn_prop_use <- eia2020[33, 15]
+wi_prop_use <- eia2020[64, 15]
 
 
 ### look up codes in ACS
-load_variables(year = 2020, dataset = "acs5") %>% mutate(concept_short = substr(concept,1,10)) %>% distinct(concept_short) %>% print(n=10000)
+load_variables(year = 2020, dataset = "acs5") %>%
+  mutate(concept_short = substr(concept, 1, 10)) %>%
+  distinct(concept_short) %>%
+  print(n = 10000)
 #### house heating fuel
-v_heat<- load_variables(year = 2021, dataset = "acs5") %>% mutate(concept_short = substr(concept,1,10)) %>% 
-  filter(concept_short == "HOUSE HEAT") %>% print(n=10000)
+v_heat <- load_variables(year = 2021, dataset = "acs5") %>%
+  mutate(concept_short = substr(concept, 1, 10)) %>%
+  filter(concept_short == "HOUSE HEAT") %>%
+  print(n = 10000)
 
-#get number of households in each county using propane
-mn_prop_hh <- get_acs(geography = 'county',
-        variables = 'B25040_003',
-        state = 'MN',
-        year = 2021) %>% 
-  filter(GEOID %in% cprg_county$GEOID) %>% 
-  mutate(mmBtu = estimate * as.numeric(mn_prop_use), #multiply average propane use by household be estimated number of households
-         CO2e = mmBtu * prop_ef * 0.001) # multiply mmBtu per county by emissions factor
-
+# get number of households in each county using propane
+mn_prop_hh <- get_acs(
+  geography = "county",
+  variables = "B25040_003",
+  state = "MN",
+  year = 2021
+) %>%
+  filter(GEOID %in% cprg_county$GEOID) %>%
+  mutate(
+    # multiply average propane use by household be estimated number of households
+    mmBtu = estimate * as.numeric(mn_prop_use),
+    # multiply mmBtu per county by emissions factor
+    CO2e = mmBtu * prop_ef * 0.001
+)
 # repeat for WI
-wi_prop_hh <- get_acs(geography = 'county',
-                      variables = 'B25040_003',
-                      state = 'WI',
-                      year = 2021) %>% 
-  filter(GEOID %in% cprg_county$GEOID) %>% 
-  mutate(mmBtu = estimate * as.numeric(wi_prop_use), #multiply average propane use by household be estimated number of households
-         CO2e = mmBtu * prop_ef * 0.001) # multiply mmBtu per county by emissions factor and then convert to metric tonnes
+wi_prop_hh <- get_acs(
+  geography = "county",
+  variables = "B25040_003",
+  state = "WI",
+  year = 2021
+) %>%
+  filter(GEOID %in% cprg_county$GEOID) %>%
+  mutate(
+    # multiply average propane use by household be estimated number of households
+    mmBtu = estimate * as.numeric(wi_prop_use), 
+    # multiply mmBtu per county by emissions factor, conver to metric tons
+    CO2e = mmBtu * prop_ef * 0.001
+  ) 
 
-#bind data
-prop_county <- rows_append(mn_prop_hh,wi_prop_hh)
+# bind data
+prop_county <- rows_append(mn_prop_hh, wi_prop_hh)
 prop_county
 
 total_regional_emissions <- sum(prop_county$CO2e) # total regional emissions of the 11 county area
