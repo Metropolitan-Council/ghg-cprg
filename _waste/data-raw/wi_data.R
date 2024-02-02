e7a8b88df257a4999186aa95098ae604671cb6b4
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-02 17:59:43 -0600

    get population of WI and then scale county population
    cprg county only has the 11 counties

--- _waste/data-raw/wi_data.R
+++ _waste/data-raw/wi_data.R
@@ -1,10 +1,6 @@
 # allocate WI state emissions by county population
 
 source("R/_load_pkgs.R")
-# library(rvest)
-# library(janitor)
-# library(tibble)
-library(dplyr)
 
 # from https://widnr.widen.net/view/pdf/o9xmpot5x7/AM610.pdf?t.download=true
 # WI GHG Emissions Inventory from the DNR, 2018 data
@@ -13,25 +9,29 @@ wi_pop_2021 <- 5893718 # NOTE this is not retrieved from same place as cprg_pop
 # check with Liz
 
 # population values from cprg_pop
-cprg_pop <- readRDS(file.path(here::here(), "R/data/cprg_population.RDS"))
-wi_pop <- cprg_pop %>%
-  filter(STATE == "Wisconsin") %>%
-  select(
-    County = NAME,
-    population
-  )
+cprg_pop <- readRDS(file.path(here::here(), "_meta/data/cprg_population.RDS"))
+wi_pop <- tidycensus::get_decennial("county",
+                                    state = "WI",
+                                    variables = "P1_001N",
+                                    year = 2020
+) %>%
+  mutate(population = sum(value),
+    pop_percent = value / sum(value)) %>%
+  filter(GEOID %in% cprg_county$GEOID)
+
 
 wi_emissions <- wi_pop %>%
   dplyr::mutate(
-    percent_pop = population / wi_pop_2021,
-    emissions_metric_tons_co2e = percent_pop * wi_total_emissions
-  )
+    emissions_metric_tons_co2e = pop_percent * wi_total_emissions
+  ) %>% 
+  left_join(cprg_county) %>% 
+  select(NAME, population, pop_percent, emissions_metric_tons_co2e)
 
 wi_emissions_meta <- tribble(
   ~Column, ~Class, ~Description,
-  "County", class(wi_emissions$County), "WI county of waste origin, including state total",
+  "NAME", class(wi_emissions$NAME), "WI county of waste origin, including state total",
   "population", class(wi_emissions$population), "Population of WI county (2020)",
-  "percent_pop", class(wi_emissions$percent_pop), "Percent of WI population in county (2020)",
+  "pop_percent", class(wi_emissions$pop_percent), "Percent of WI population in county (2020)",
   "emissions_metric_tons_co2e", class(wi_emissions$emissions_metric_tons_co2e),
   "Total waste emissions allocated to county based on 2018 totals"
 )
