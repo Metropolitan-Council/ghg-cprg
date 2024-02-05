351e8927c5859993e127cc91d1f4c4f965e316b1
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-05 15:43:48 -0600

    re-run waste and wastewater data
    no real change in values

--- _waste/data-raw/wi_data.R
+++ _waste/data-raw/wi_data.R
@@ -8,34 +8,26 @@ wi_total_emissions <- 2.2 * 10^6 # in mtco2e
 wi_pop_2021 <- 5893718 # NOTE this is not retrieved from same place as cprg_pop
 # check with Liz
 
-# population values from cprg_pop
 cprg_pop <- readRDS(file.path(here::here(), "_meta/data/cprg_population.RDS"))
+cprg_county_proportions <- readRDS("_meta/data/cprg_county_proportions.RDS")
 
-wi_pop <- tidycensus::get_decennial("county",
-  state = "WI",
-  variables = "P1_001N",
-  year = 2020
-) %>%
-  mutate(
-    population = sum(value),
-    pop_percent = value / sum(value)
-  ) %>%
-  filter(GEOID %in% cprg_county$GEOID)
-
-
+wi_pop <- cprg_county_proportions %>% 
+  filter(STATE == "Wisconsin",
+         year == "2020")
+  
 wi_emissions <- wi_pop %>%
   rowwise() %>%
   dplyr::mutate(
-    emissions_metric_tons_co2e = pop_percent * wi_total_emissions
+    emissions_metric_tons_co2e = county_proportion_of_state_pop * wi_total_emissions
   ) %>%
   left_join(cprg_county, by = "GEOID") %>%
-  select(NAME = NAME.y, population, pop_percent, emissions_metric_tons_co2e)
+  select(NAME = NAME.y, county_population, county_proportion_of_state_pop, emissions_metric_tons_co2e)
 
 wi_emissions_meta <- tribble(
   ~Column, ~Class, ~Description,
   "NAME", class(wi_emissions$NAME), "WI county of waste origin, including state total",
-  "population", class(wi_emissions$population), "Population of WI county (2020)",
-  "pop_percent", class(wi_emissions$pop_percent), "Percent of WI population in county (2020)",
+  "county_population", class(wi_emissions$county_population), "Population of WI county (2020)",
+  "county_proportion_of_state_pop", class(wi_emissions$county_proportion_of_state_pop), "Percent of WI population in county (2020)",
   "emissions_metric_tons_co2e", class(wi_emissions$emissions_metric_tons_co2e),
   "Total waste emissions allocated to county based on 2018 totals"
 )
