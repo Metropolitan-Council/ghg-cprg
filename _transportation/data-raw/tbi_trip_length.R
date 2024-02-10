ae3ff8215413ef9ba382d6b0dd5919dd66f39d5a
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-09 23:19:44 -0600

    style

--- _transportation/data-raw/tbi_trip_length.R
+++ _transportation/data-raw/tbi_trip_length.R
@@ -107,7 +107,8 @@ tbi_mean_trip_length <- trip21 %>%
     var = survey_var(distance)
   ) %>%
   left_join(od_pair_index,
-            by = join_by(od_pair))
+    by = join_by(od_pair)
+  )
 
 
 
@@ -124,13 +125,15 @@ tbi_od_ordered_trip_length <- trip21 %>%
     estimate_n = survey_total(),
     var = survey_var(distance)
   ) %>%
-  left_join(od_pair_index %>%
-    select(
-      trip_o_county, trip_d_county,
-      origin_dest_county_pair
-    ) %>%
-    unique(),
-    by = join_by(origin_dest_county_pair))
+  left_join(
+    od_pair_index %>%
+      select(
+        trip_o_county, trip_d_county,
+        origin_dest_county_pair
+      ) %>%
+      unique(),
+    by = join_by(origin_dest_county_pair)
+  )
 
 saveRDS(tbi_mean_trip_length, "_transportation/data-raw/tbi/tbi_mean_trip_length.RDS")
 saveRDS(tbi_od_ordered_trip_length, "_transportation/data-raw/tbi/tbi_od_ordered_trip_length.RDS")
