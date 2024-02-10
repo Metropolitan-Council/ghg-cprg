d84ae7bb85456cd91f12a1c0575663b880e6132e
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-09 23:12:45 -0600

    write tests for gwp and epa_mvoes

---
+++ tests/test-gwp.R
@@ -0,0 +1,11 @@
+testthat::test_that("GWP values", {
+
+  source(file.path(here::here(), "R/global_warming_potential.R"))
+
+  testthat::expect_equal(length(gwp), 5)
+
+  testthat::expect_equal(gwp$co2, 1)
+  testthat::expect_equal(gwp$ch4, 28)
+  testthat::expect_equal(gwp$n2o, 265)
+
+  })
