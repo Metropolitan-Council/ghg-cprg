308362f741b943fe7f1bc9ba83ae26db79a025e0
ksmiff33 <Kenneth.smith@metc.state.mn.us>
2024-02-13 14:39:04 -0600

    Create lidac_block_groups.R

---
+++ _meta/data-raw/lidac_block_groups.R
@@ -0,0 +1,10 @@
+## Some header info
+
+## Start here:
+source(file.path(here::here(), "R/_load_pkgs.R"))
+source(file.path(here::here(), "R/_quarto_helpers.R"))
+source(file.path(here::here(), "R/plot_county_emissions.R"))
+
+
+## KS pick up here...
+
