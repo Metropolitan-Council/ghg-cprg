27b90a594421b3370e9b231c73382a178ddfd46d
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-14 12:38:27 -0600

    update text, style

--- _waste/data-raw/wastewater/metc_service_area.R
+++ _waste/data-raw/wastewater/metc_service_area.R
@@ -6,21 +6,23 @@ ww_raw <- councilR::import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/
 cprg_county <- readRDS(file.path(here::here(), "_meta/data/cprg_county.RDS"))
 source(file.path(here::here(), "R/_leaflet_helpers.R"))
 
-ww_service_areas <- ww_raw %>% 
-  filter(ServiceSta == "Served",
-         !is.na(WWTP)) %>% 
-  sf::st_make_valid() %>% 
+ww_service_areas <- ww_raw %>%
+  filter(
+    ServiceSta == "Served",
+    !is.na(WWTP)
+  ) %>%
+  sf::st_make_valid() %>%
   group_by(ServiceSta) %>%
-  summarize(geom = st_union(geom)) %>% 
-  sf::st_make_valid() %>% 
+  summarize(geom = st_union(geom)) %>%
+  sf::st_make_valid() %>%
   sf::st_simplify(dTolerance = 2)
 
 saveRDS(ww_service_areas, "_waste/data-raw/wastewater/metc_service_area.RDS")
-# 
-# council_leaflet() %>% 
+#
+# council_leaflet() %>%
 #   addPolygons(
 #     data = cprg_county %>%
-#       sf::st_transform(4326) %>% 
+#       sf::st_transform(4326) %>%
 #       st_union(),
 #     fill = FALSE,
 #     color = "gray",
@@ -31,11 +33,11 @@ saveRDS(ww_service_areas, "_waste/data-raw/wastewater/metc_service_area.RDS")
 #     data = ww_service_areas,
 #     group = "MetC wastewater service area",
 #     weight = 1,
-#     popup =  "MetC wastewater service area") %>% 
+#     popup =  "MetC wastewater service area") %>%
 #   leaflet::addLayersControl(
 #     overlayGroups = c(
 #       "CPRG area",
 #       "MetC wastewater service area"
 #     )
 #   )
-# 
+#
