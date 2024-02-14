daf71d3ef062a145d8896a2c777bd53027138cfe
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-13 22:10:50 -0600

    move MPCA section

---
+++ _waste/data-raw/wastewater/mpca_wastewater.R
@@ -0,0 +1,14 @@
+source("R/_load_pkgs.R")
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
+
+### MPCA provides waste water emission estimates
+mn_mpca <- readxl::read_xlsx("_waste/data-raw/wastewater/mpca-mn-wastewater.xlsx")
+
+mpca_state_ghg <- mn_mpca %>%
+  pivot_longer(7:37,
+               names_to = "year"
+  ) %>%
+  filter(year == 2020) %>%
+  select(GHGs, year, value)
+
+mn_state_est <- as.numeric(mn_mpca[mn_mpca$Sector == "Grand Total", "2020"])
