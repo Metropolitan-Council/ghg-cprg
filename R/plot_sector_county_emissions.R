7a50311bab8708f04061cf32bdd31d3c27bd0e56
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-23 11:17:14 -0600

    start plot for sector emissions, simple

---
+++ R/plot_sector_county_emissions.R
@@ -0,0 +1,54 @@
+#' Plot county level emissions for any given sector, no colors, ordered by total
+#'
+#' @param county_emissions data frame, compiled emissions. Must have columns
+#'   "sector", "geog_name", "emissions_metric_tons_co2e", "source", "category",
+#'   and "geog_level"
+#' @param sector character, one of "Transportation", ....
+#' @param plotly_source character, passed to source paramter in `plotly::plot_ly()`
+#'
+#' @return Plotly object
+#' @export
+#'
+#' @importFrom plotly plot_ly layout
+#' @importFrom dplyr filter
+#' @importFrom stringr str_to_title
+#' @importFrom councilR plotly_layout
+plot_county_emissions <- function(county_emissions,
+                                  .sector,
+                                  .plotly_source) {
+  plot_data <- county_emissions %>%
+    dplyr::filter(sector == .sector)
+
+  if (nrow(plot_data) == 0) {
+    return(plot_ly(
+      source = .plotly_source,
+      type = "bar"
+    ))
+  }
+
+  plot_ly(
+    data = plot_data,
+    type = "bar",
+    source = .plotly_source,
+    y = ~geog_name,
+    x = ~emissions_metric_tons_co2e,
+    # color = ~source,
+    # split = ~source,
+    # colors = unlist(source_colors),
+    marker = list()
+    hovertemplate = ~ paste0(
+      geog_name, " County", "<br>",
+      sector, " - ", category, ", ", source, "<br>",
+      round(emissions_metric_tons_co2e * 1e-6, digits = 2), " million metric tons CO<sub>2</sub>e", "<br>",
+      "<extra></extra>"
+    )
+  ) %>%
+    councilR::plotly_layout(
+      main_title = paste0(.sector, " emissions"),
+      subtitle = "",
+      y_title = stringr::str_to_title(unique(plot_data$geog_level)),
+      x_title = "Metric tons CO<sub>2</sub>e",
+      legend_title = "Category"
+    ) %>%
+    plotly::layout(barmode = "stack")
+}
