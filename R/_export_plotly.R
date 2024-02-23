e0b57dbd8b21dad742150cf07da0fb28ca67b622
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-22 18:30:27 -0600

    update save plotly protocol

--- R/_export_plotly.R
+++ R/_export_plotly.R
@@ -4,10 +4,10 @@ source("R/_quarto_helpers.R")
 if (!reticulate::virtualenv_exists("save-plotly")) {
   # if virtual environment with needed python packages does not exist
   # create and use it, then install libraries
-
+  
   reticulate::virtualenv_create("save-plotly")
   reticulate::use_virtualenv("save-plotly")
-
+  
   reticulate::py_install("kaleido")
   reticulate::py_install("plotly")
 }
@@ -17,22 +17,31 @@ reticulate::use_virtualenv("save-plotly")
 reticulate::py_run_string("import sys")
 
 fs::dir_create("purl")
+fs::dir_create("purl/_energy")
+fs::dir_create("purl/_meta")
+fs::dir_create("purl/_transportation")
+fs::dir_create("purl/_waste")
+
+
 # purl all quarto docs
-list.files(path = ".", pattern = "*.qmd") %>%
+list.files(path = ".", pattern = "*.qmd", recursive = TRUE) %>%
   purrr::map(
     function(x) {
       knitr::purl(x,
-        output = paste0(
-          "purl/",
-          stringr::str_replace_all(x, ".qmd", ".R")
-        ),
-        documentation = 2
+                  output = paste0(
+                    "purl/",
+                    stringr::str_replace_all(x, ".qmd", ".R")
+                  ),
+                  documentation = 2
       )
     }
   )
 
 # evaluate all quarto
-list.files("purl/", full.names = TRUE) %>%
+list.files("purl",
+           full.names = TRUE,
+           pattern = ".R", recursive = TRUE
+) %>%
   purrr::map(source)
 fs::file_delete("purl/")
 
@@ -64,45 +73,85 @@ complete_figure_index <-
   left_join(
     printCap(book = TRUE) %>%
       dplyr::filter(stringr::str_detect(Figure, "fig")) %>%
-      unique()
+      unique(),
+    by = "Figure"
   )
 
-saveRDS(complete_figure_index, "data/_figure_index.RDS")
+saveRDS(complete_figure_index, "assets/_figure_index.RDS")
 
 # export plots -----
 cli::cli_alert(paste0("Writing ", length(pls), " plots"))
 
 # export to pdf (highest resolution)
+# purrr::map(
+#   names(pls),
+#   function(x) {
+#     save_plotly(get(x),
+#                 fmt = "pdf",
+#                 file_title = "source"
+#     )
+#   }
+# )
+# purrr::map(
+#   names(pls),
+#   function(x) {
+#     save_plotly(get(x),
+#                 fmt = "svg",
+#                 file_title = "source"
+#     )
+#   }
+# )
+
+
+purrr::map(names(pls),
+           function(x){
+             save_plotly(get(x), 
+                         fmt = "png",
+                         file_title = "source",
+                         width = 900,
+                         height = 450,
+                         scale = 2)
+           })
+
+
+
+# create plots without titles ----
 purrr::map(
   names(pls),
-  function(x) {
-    save_plotly(get(x),
-      fmt = "pdf",
-      file_title = "source"
-    )
-  }
-)
+  function(x){
+    this_pl <- get(x)
+
+    # navigate to text
+    # and change to empty character
+    this_pl$x$layoutAttrs[[1]]$title$text <- ""
+    
+    save_plotly(this_pl, 
+                fmt = "png",
+                file_title = "source",
+                file_location = "assets/plots/plotly_no_title_png",
+                width = 900,
+                height = 450,
+                scale = 2)
+  })
+
 
 purrr::map(
   names(pls),
-  function(x) {
-    save_plotly(get(x),
-      fmt = "svg",
-      file_title = "source"
-    )
-  }
-)
-
-
-# export to png (lower resolution, smaller, still readable)
-# purrr::map(names(pls),
-#            function(x){
-#              save_plotly(get(x), fmt = "png",
-#                          file_title = "source")
-#            })
-
-
-
+  function(x){
+    this_pl <- get(x)
+    
+    # navigate to text
+    # and change to empty character
+    this_pl$x$layoutAttrs[[1]]$title$text <- ""
+    
+    save_plotly(this_pl, 
+                fmt = "svg",
+                file_title = "source",
+                file_location = "assets/plots/plotly_no_title_svg",
+                width = 900,
+                height = 450,
+                scale = 2)
+  })
 
 # export leaflet -----
 #
