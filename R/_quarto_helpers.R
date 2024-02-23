815557a3feb82739c099f1254fa6b218fa66990d
Liz Roten <liz.roten@metc.state.mn.us>
2024-02-22 18:30:54 -0600

    add arguments for width, height, scale, and file location

--- R/_quarto_helpers.R
+++ R/_quarto_helpers.R
@@ -14,10 +14,10 @@ addCap <- function(label = NULL, cap = NULL, short_cap = NULL) {
     if (!length(r)) {
       r <- knitr::opts_current$get(tag3)
     }
-
+    
     r
   }
-
+  
   alter_label <- function() {
     lab <- get_chunk_opt("label")
     # if chunk label exists and it does NOT start with "fig"
@@ -27,62 +27,62 @@ addCap <- function(label = NULL, cap = NULL, short_cap = NULL) {
     }
     lab
   }
-
+  
   # if no label, alter label
   if (!length(label)) {
     label <- alter_label()
   }
-
+  
   deb <- .Options$debugaddCap
   deb <- length(deb) && deb
-
+  
   if (deb) {
     cat("label:", label, "\n", file = "/tmp/z", append = TRUE)
   }
-
+  
   # if there is no label, return invisible list
   if (!length(label)) {
     return(invisible(list(NULL, NULL, NULL)))
   }
-
+  
   # if label is logical and is false, return invisible list
   if (is.logical(label) && !label) {
     return(invisible(list(NULL, NULL, NULL)))
   }
-
+  
   # if there is no cap , get cap from chunk
   if (!length(cap)) {
     cap <- get_chunk_opt("fig.cap", "tbl.cap", "cap", "tbl-cap")
   }
-
+  
   # if there is no short_cap, get short_cap from chunk
   if (!length(short_cap)) {
     short_cap <- get_chunk_opt("fig.short_cap", "short_cap")
   }
-
+  
   # if there is no cap but there is a short cap, re-assign cap
   if (!length(cap) && length(short_cap)) {
     cap <- short_cap
   }
-
+  
   # if there is no short_cap but there is a cap, re-assign short_cap
   if (!length(short_cap) && length(cap)) {
     short_cap <- cap
   }
-
+  
   # if global global_caption_index does NOT exist, create it
   if (!exists("global_caption_index")) {
     global_caption_index <<- NULL
   }
-
+  
   # create data frame with label, caption, and short caption
   info <- data.frame(label = label, cap = cap, short_cap = short_cap)
-
-
+  
+  
   if (deb) {
     prn(info, fi = "/tmp/z")
   }
-
+  
   # if there is nothing in global_caption_index OR
   # label is NOT in global_caption_index$label
   # if (!length(global_caption_index) || !label %in% global_caption_index$label) {
@@ -108,7 +108,7 @@ printCap <- function(book = FALSE) {
     for (f in files) global_caption_index <- rbind(global_caption_index, readRDS(f))
   }
   cap <- global_caption_index[c("label", "short_cap")]
-
+  
   # use '@' to cross reference exact figure
   cap$label <- paste0("@", cap$label)
   names(cap) <- c("Figure", "Short Caption")
@@ -130,24 +130,24 @@ hookaddcap <- function(loc = NULL) {
     # create/find label and caption ----
     label <- knitr::opts_current$get("label")
     cap <- options$fig.cap
-
-
+    
+    
     if (!length(cap)) {
       cap <- options$tbl.cap
     }
-
-
+    
+    
     if (!length(cap)) {
       cap <- options$`tbl-cap`
     }
-
+    
     # if 'cap' is empty, try 'tbl.cap'
-
+    
     # if  still empty, use 'cap'
     if (!length(cap)) {
       cap <- options$cap
     }
-
+    
     # if cap is a function, eval
     if (length(cap) && is.call(cap)) {
       cap <- eval(cap)
@@ -156,10 +156,10 @@ hookaddcap <- function(loc = NULL) {
     if (!length(cap) || cap == "" || (!grepl("^fig-", label) && !grepl("^tbl-", label))) {
       return()
     }
-
+    
     # create/find short caption----
     short_cap <- options$fig.short_cap
-
+    
     # if 'fig.short_cap' is empty, try looking for 'short_cap'
     if (!length(short_cap)) {
       short_cap <- options$short_cap
@@ -168,7 +168,7 @@ hookaddcap <- function(loc = NULL) {
     if (length(short_cap) && is.call(short_cap)) {
       short_cap <- eval(short_cap)
     }
-
+    
     # if short_cap is empty, reassign cap
     if (!length(short_cap) || short_cap == "") {
       short_cap <- cap
@@ -186,8 +186,13 @@ hookaddcap <- function(loc = NULL) {
 }
 
 
-save_plotly <- function(a_plotly, fmt = c("png", "pdf"),
-                        file_title = c("source", "title")) {
+save_plotly <- function(a_plotly, fmt = c("png", "pdf", "svg"),
+                        file_title = c("source", "title"),
+                        file_location = paste0("assets/plots/plotly_", fmt),
+                        width = 900,
+                        height = 450,
+                        scale = 1) {
+  # browser()
   pl_titles <-
     purrr::map(
       1:length(a_plotly$x$layoutAttrs),
@@ -195,13 +200,13 @@ save_plotly <- function(a_plotly, fmt = c("png", "pdf"),
         a_plotly$x$layoutAttrs[[x]]$title$text
       }
     )
-
+  
   pl_title <- pl_titles %>%
     rlist::list.clean() %>%
     rlist::list.filter(. != "") %>%
     unlist() %>%
     stringr::str_replace_all("[^[:alnum:]]", " ")
-
+  
   # browser()
   pl_source <-
     purrr::map(
@@ -212,26 +217,33 @@ save_plotly <- function(a_plotly, fmt = c("png", "pdf"),
     ) %>%
     unique() %>%
     unlist()
-
+  
+  if(!fs::dir_exists(file_location)){
+    fs::dir_create(file_location, recurse = TRUE)
+  }
+  
   file_name <- if (file_title == "source") {
     paste0(
-      "plotly_", fmt, "/",
+      file_location, "/",
       pl_source, ".", fmt
     )
   } else if (file_title == "title") {
     paste0(
-      "plotly_", fmt, "/",
+      file_location, "/",
       pl_title, ".", fmt
     )
   }
-
+  
   scope <- kaleido()
-
+  
   scope$transform(
     p = a_plotly,
-    file = file_name
+    file = file_name,
+    width = width,
+    height = height,
+    scale = scale
   )
-
+  
   if (fmt == "pdf") {
     Sys.sleep(1)
     scope$transform(
