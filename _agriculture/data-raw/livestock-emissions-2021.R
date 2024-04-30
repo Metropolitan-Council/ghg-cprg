f642b88fa4b0b4077a795311a514e8084387f90b
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-04-30 16:07:30 -0500

    start livestock data-raw processing
    I think these four csvs can be used to get MN livestock emissions. EFs derived from SIT data (manure, enteric) and activity data, livestock numbers, from mn-feedlots

---
+++ _agriculture/data-raw/livestock-emissions-2021.R
@@ -0,0 +1,8 @@
+source("R/_load_pkgs.R")
+source("R/global_warming_potential.R")
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
+
+enteric <- read_csv('_agriculture/data-raw/enteric-fermentation.csv')
+manure_n2o <- read_csv('_agriculture/data-raw/manure-n2o.csv')
+manure_ch4 <- read_csv('_agriculture/data-raw/manure-ch4.csv')
+mn_feedlots <- read_csv('_agriculture/data-raw/mn-feedlots.csv')
