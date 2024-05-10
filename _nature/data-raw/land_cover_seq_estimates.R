cfb42a99474fd49a386ffb33d5dbcc4cb1e24102
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-05-10 16:29:09 -0500

    finalize sequestration rate data

--- _nature/data-raw/land_cover_seq_estimates.R
+++ _nature/data-raw/land_cover_seq_estimates.R
@@ -6,20 +6,24 @@ seq_rates <- read_csv('./_nature/data-raw/land_cover_seq_rates.csv')
 
 #sequestration rates require some recalculation given varying units. 
 # Further, urban grasslands (usually turf grass) have a C sequestration rate but also are expected emitters of N20
-seq_rates<- mutate(seq_rates, mtco2e = if_else(units == 'metric ton C', activity * 3.67,  #scale units carbon to CO2e (atomic weight of a carbon atom compared to CO2 molecule)
-                                            if_else(units == 'Tg C', activity * 3.67 * 1e+6, # teragrams carbon to metric ton CO2e
+seq_rates_sum <- mutate(seq_rates, mtco2e = if_else(units == 'metric ton C', activity * 3.67,  #scale units carbon to CO2e (atomic weight of a carbon atom compared to CO2 molecule)
+                                            if_else(units == 'US ton C', activity * 3.67 * 0.907185, # us ton to metric ton CO2e
                                                   if_else(units == 'kg N', activity * 0.01 * gwp$n2o * 0.001, # IPCC 2019 assumes 1% of nitrogen fertilizer volatilizes to N20
                                     activity)))) %>% 
   ##### convert all seq rates to Mt per 1 sq km
-          mutate(mtco2e_sqkm = if_else(area_unit == 'ha', (mtco2e / area) * 100, 
-                                       if_else(area_unit == 'sqkm', mtco2e / area, NA)))
+          mutate(sqkm = if_else(area_unit == 'ha', area / 100, 
+                                       if_else(area_unit == 'acre', area / 247.1  , NA))) %>% 
+  mutate(mtco2e_sqkm = mtco2e / sqkm) %>% 
+  filter(!activity == 0.25) %>% # remove hidden carbon cost calculation as it seems to underestimate emissions
+  group_by(land_cover_type) %>% 
+  summarize(mtco2e_sqkm = sum(mtco2e_sqkm))
 
-wc_county_seq <- left_join(wc_county,seq_rates) %>% 
-  filter(activity < 0) %>% 
-  mutate(area_ha = area * 100, sequestration_potential = area_ha * activity * 3.67) # 3.67 is conversion of unit carbon to unit co2e
+wc_county_seq <- left_join(wc_county,seq_rates_sum) %>% 
+  filter(mtco2e_sqkm < 0) %>% 
+  mutate(sequestration_potential = area  * mtco2e_sqkm)
   
 county_seq_total <- summarize(wc_county_seq, seq_total = sum(sequestration_potential))
 
 county_seq_total
 
-sum(county_seq_total$seq_total) #601099.1
+sum(county_seq_total$seq_total) #-2180841
