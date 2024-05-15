1d4a81e2df43f527e192d3b20a2cf8c0df3c5a85
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-05-15 14:57:43 -0500

    Split out carbon stock and seq standardization from regional estimate

--- _nature/data-raw/land_cover_seq_estimates.R
+++ _nature/data-raw/land_cover_seq_estimates.R
@@ -1,8 +1,8 @@
 source("R/_load_pkgs.R")
 source("R/global_warming_potential.R")
 
-wc_county <- readRDS('./_nature/data/county_landcover_2021.rds')
 seq_rates <- read_csv('./_nature/data-raw/land_cover_seq_rates.csv')
+stock <- read_csv('./_nature/data-raw/land_cover_stock.csv')
 
 #sequestration rates require some recalculation given varying units. 
 # Further, urban grasslands (usually turf grass) have a C sequestration rate but also are expected emitters of N20
@@ -16,28 +16,27 @@ seq_rates_sum <- mutate(seq_rates, mtco2e = if_else(units == 'metric ton C', act
   mutate(mtco2e_sqkm = mtco2e / sqkm) %>% 
   filter(!activity == 0.25) %>% # remove hidden carbon cost calculation as it seems to underestimate emissions
   group_by(land_cover_type) %>% 
-  summarize(mtco2e_sqkm = sum(mtco2e_sqkm))
+  summarize(seq_mtco2e_sqkm = sum(mtco2e_sqkm))
 
-wc_county_seq <- left_join(wc_county,seq_rates_sum) %>% 
-  filter(mtco2e_sqkm < 0) %>% 
-  mutate(sequestration_potential = area  * mtco2e_sqkm)
+# find the mean stock estimates with standardized units
+stock_mean <- mutate(stock, mtco2e = if_else(units == 'metric tons C', activity * 3.67,  #scale units carbon to CO2e (atomic weight of a carbon atom compared to CO2 molecule)
+                                                    if_else(units == 'kg C', activity * 3.67 * 0.001, # kg carbon to metric ton CO2e
+                                                                    activity))) %>% 
+  ##### convert all seq rates to Mt per 1 sq km
+  mutate(sqkm = if_else(area_unit == 'ha', area / 100, 
+                        if_else(area_unit == 'm', area * 1e-6  , NA))) %>% 
+  mutate(mtco2e_sqkm = mtco2e / sqkm) %>% 
+  group_by(land_cover_type) %>% 
+  summarize(stock_mtco2e_sqkm = -mean(mtco2e_sqkm)) # take the mean values for cover types with multiple, call it negative for messaging
 
-wc_county_seq_meta <-
+
+land_cover_c <- left_join(seq_rates_sum, stock_mean)
+
+land_cover_c_meta <-
   tibble::tribble(
     ~"Column", ~"Class", ~"Description",
-    "county ", class(wc_county_seq$county ), "County name",
-    "land_cover_type", class(wc_county_seq$land_cover_type), "Land cover type from World Cover. 'Urban_' indicates a natural area within NLCD designated developed land cover",
-    "area", class(wc_county_seq$area), "Area of land cover in square kilometers. 'Urban_Tree' is scaled down by NLCD percent impervious",
-    "CO2e per square km", class(wc_county_seq$mtco2e_sqkm), "Average regional sequestration rate in CO2 equivalency per square kilometer",
-    "land cover sequestration potential", class(wc_county_seq$sequestration_potential), "Annual sequestration potential of land cover type in the county"
+    "land_cover_type", class(land_cover_c$land_cover_type), "Land cover type from World Cover. 'Urban_' indicates a natural area within NLCD designated developed land cover",
+    "Sequestration rate", class(land_cover_c$seq_mtco2e_sqkm),  "Carbon sequestration potential of land cover type in metric tons CO2 per square kilometer per year",
+    "Stock potential", class(land_cover_c$stock_mtco2e_sqkm), "Total carbon stock potential of land cover type in metric tons CO2 equivalency per square kilometer"
   )
-  
 
-saveRDS(wc_county_seq, './_nature/data/county_landcover_sequestration_2021.rds')
-saveRDS(wc_county_seq_meta, './_nature/data/county_landcover_sequestration_2021_meta.rds')
-
-county_seq_total <- summarize(wc_county_seq, seq_total = sum(sequestration_potential))
-
-county_seq_total
-
-sum(county_seq_total$seq_total) #-2180841
