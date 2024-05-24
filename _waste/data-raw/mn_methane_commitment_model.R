a21f01000c8fc3d490b682db44f5b38d4c221065
Zoey Yandell <zeyandell@gmail.com>
2024-05-24 17:00:02 -0500

    flaring data read-in

--- _waste/data-raw/mn_methane_commitment_model.R
+++ _waste/data-raw/mn_methane_commitment_model.R
@@ -89,6 +89,18 @@ calc_landfill_emissions <- function(County, Year, Tons, f_rec){
 
 # pull in and calculate f_rec
 
+# Source: EPA State Inventory Solid Waste tool, methane flaring data 
+
+# flaring_natl_data <- read_csv(file.path(here::here(), "_waste/data-raw/solid-waste-module-flaring.csv"))%>%  
+#   row_to_names(row_number = 1) 
+# flaring_natl_data <- data.frame(t(flaring_natl_data)) %>%  
+#   row_to_names(row_number = 1) 
+
+
+flaring_data <- readxl::read_xlsx("_waste/data-raw/solid_waste_flaring.xlsx",
+                                  range = "A2:AG54") %>% 
+  rename(State = 1)
+
 landfill_data <- score_filtered %>% 
   filter(Method == "Landfill") %>% 
   select(County,
