5d6f4352d054a4ab7d0758a57f27411e2010027a
Liz Roten <liz.roten@metc.state.mn.us>
2024-05-28 18:11:06 -0500

    update fix_style with additional sections

--- R/fix_style.R
+++ R/fix_style.R
@@ -3,6 +3,8 @@ if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
   fs::dir_copy("style", "docs/_energy/style/", overwrite = TRUE)
   fs::dir_copy("style", "docs/_waste/style/", overwrite = TRUE)
   fs::dir_copy("style", "docs/_meta/style/", overwrite = TRUE)
+  fs::dir_copy("style", "docs/_nature/style/", overwrite = TRUE)
+  fs::dir_copy("style", "docs/_agriculture/style/", overwrite = TRUE)
   fs::dir_copy("assets/maps", "docs/_meta/assets/maps", overwrite = TRUE)
   quit()
 }
