d3a4413da088239aa11c2f1defa5a3df49803f86
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-06-06 17:29:52 -0500

    Update livestock-emissions
    generate animal count. First glance seems low

--- _agriculture/data-raw/livestock-emissions-2021.R
+++ _agriculture/data-raw/livestock-emissions-2021.R
@@ -5,4 +5,31 @@ cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 enteric <- read_csv('_agriculture/data-raw/enteric-fermentation.csv')
 manure_n2o <- read_csv('_agriculture/data-raw/manure-n2o.csv')
 manure_ch4 <- read_csv('_agriculture/data-raw/manure-ch4.csv')
-mn_feedlots <- read_csv('_agriculture/data-raw/mn-feedlots.csv')
+mn_feedlots <- read_csv('_agriculture/data-raw/mn-feedlots.csv') %>% 
+  filter(county_name %in% cprg_county$NAME)
+
+### m
+mn_feedlots_long <- mn_feedlots %>% 
+  mutate(start_year = year(as.Date(start_d_reg)),
+         end_year = year(as.Date(end_d_reg))) %>%
+  filter(!is.na(start_year)) %>% 
+  rowwise() %>%
+  mutate(years = list(seq(start_year, end_year))) %>%
+  unnest(years) %>% 
+  pivot_longer(cols = c(
+    "cattle_dl", "heifer_d", "calf_d", "cattle_db", "steer_b", "heifer_b", "cow_calf_b", "calf_b", 
+    "bull_mature", "veal_calf", "swine_big", "swine_medium", "swine_little", "horse", "sheep", 
+    "chx_lm", "chx_b_big", "chx_b_little", "chx_l_big", "chx_l_little", "turkey_big", "turkey_little", "duck", "duck_lm", 
+    "alpacas", "bison", "bison_calf", "camels", "deer", "donkey_mule", "elk", "emus_ostriches", "peacocks", "fowl", 
+    "foxes", "geese", "goats", "goats_small", "ponies", "llamas", "mink", "rabbits", "reindeer_caribou", "unknown"
+  ),
+  names_to = "animal",
+  values_to = "count",
+  values_drop_na = TRUE
+  ) %>%
+  filter(count >0) %>% 
+  group_by(county_name, years, animal) %>% 
+  summarize(county_count = sum(count)) 
+  
+
+
