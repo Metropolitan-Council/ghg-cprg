9b253bc976cdf70af720f21dc0517f0c89087f1d
Zoey Yandell <zeyandell@gmail.com>
2024-06-07 11:34:01 -0500

    rewrite without purrr

--- _waste/data-raw/mn_methane_flaring.R
+++ _waste/data-raw/mn_methane_flaring.R
@@ -1,6 +1,8 @@
+source("R/_load_pkgs.R")
+
 # pull in and calculate f_rec ----
 
-# Source: EPA State Inventory Solid Waste tool, methane flaring data 
+# Source: EPA State Inventory Solid Waste tool, methane flaring and LFGTE data 
 
 # flaring_natl_data <- read_csv(file.path(here::here(), "_waste/data-raw/solid-waste-module-flaring.csv"))%>%  
 #   row_to_names(row_number = 1) 
@@ -16,9 +18,12 @@ flaring_data <- readxl::read_xlsx("_waste/data-raw/solid_waste_flaring.xlsx",
 
 flaring_data <- data.frame(t(flaring_data)) %>% 
   tibble::rownames_to_column("Year") %>% 
-  rename(flared_mmt_ch4_mn = t.flaring_data.)
+  rename(flared_metric_tons_ch4_mn = t.flaring_data.) # this is currently mmt. it will be converted
 
-flaring_data <- flaring_data[-1,]
+flaring_data <- flaring_data[-1,] %>% 
+  mutate(
+    flared_metric_tons_ch4_mn = as.numeric(flared_metric_tons_ch4_mn) * 10^6 # converting mmt to mt
+  )
 
 # need to:
 # multiply by population percentages to scale down
@@ -31,6 +36,17 @@ lfgte_data <- readxl::read_xlsx("_waste/data-raw/solid_waste_lfgte.xlsx",
 
 lfgte_data <- data.frame(t(lfgte_data)) %>% 
   tibble::rownames_to_column("Year") %>% 
-  rename(lfgte_mmt_ch4_mn = t.lfgte_data.)
+  rename(lfgte_metric_tons_ch4_mn = t.lfgte_data.) 
 
-lfgte_data <- lfgte_data[-1,]
+lfgte_data <- lfgte_data[-1,] %>% 
+  mutate(
+    lfgte_metric_tons_ch4_mn = as.numeric(lfgte_metric_tons_ch4_mn) * 10^6
+  )
+
+# join dfs
+
+methane_recovery_mn <- flaring_data %>% 
+  left_join(lfgte_data, by = join_by(Year))
+# left to do: allocate by county
+# write metadata
+# save as rds
