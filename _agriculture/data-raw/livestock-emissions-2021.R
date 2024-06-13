692c3a7325917f67b4b6f7c99f477f4cec4d290c
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-06-13 14:12:09 -0500

    homogenize livestock name

--- _agriculture/data-raw/livestock-emissions-2021.R
+++ _agriculture/data-raw/livestock-emissions-2021.R
@@ -15,6 +15,9 @@ key <- keyring::key_get('usda_key')
 
 counties <- toupper(cprg_county$NAME)
 
+### this is an API to get livestock data (mammals) from the USDA.
+### USDA has year survey data that provides heads of animals
+### USDA also has 5 year census data with more detailed information (2007, 2012, 2017, 2022). This should be examined for validation later
 usda_livestock <- tidyUSDA::getQuickstat(
   sector="ANIMALS & PRODUCTS",
   group="LIVESTOCK",
@@ -23,7 +26,7 @@ usda_livestock <- tidyUSDA::getQuickstat(
   domain= NULL,
   county= counties,
   key = key,
-  program = NULL,
+  program = "SURVEY",
   data_item = NULL,
   geographic_level = 'COUNTY',
   year = as.character(2005:2022),
@@ -33,24 +36,19 @@ usda_livestock <- tidyUSDA::getQuickstat(
   weighted_by_area = T)
 
 
-usda_livestock %>% filter(commodity_desc == "CATTLE", unit_desc == "HEAD") %>% distinct(year)
-#### Cattle have head estimates in all years from 2005-2021
-usda_livestock %>% filter(commodity_desc != "CATTLE", unit_desc == "OPERATIONS") %>% distinct(year)
-
 usda_livestock_use <- usda_livestock %>% 
   filter(!is.na(Value), unit_desc == "HEAD")  # exclude missing values, take only head count surveys now. May be useful to validate by operations census later.
 
-
 usda_livestock_use %>% 
-  filter(commodity_desc == "CATTLE", year == 2021, county_name == "DAKOTA")
+  filter(commodity_desc != "CATTLE") %>% 
+  distinct(year)
+## there is no non-cattle survey data past 2013. Will need to use inventory data to in-fill other livestock
 
 unique(usda_livestock_use$short_desc)
-unique(usda_livestock_use$domain_desc)
-
 
 ### other live stock have head estimates from 2005:2013, and 2017
 
-### operations censuses are benchmarkes at 2007,2012,2017, 2022
+### operations censuses are benchmarks at 2007,2012,2017, 2022
 
 ### format enteric fermentation emissions factors
 
@@ -68,7 +66,45 @@ unique(enteric_formatted$Livestock)
 
 enteric_agg <- enteric_formatted %>% 
   filter(!grepl("mos.",Livestock)) %>% 
-  mutate(livestock_type = if_else(grepl("Replacement", "C")))
+  mutate(livestock_type = if_else(grepl("Replacement",Livestock), "Calves",
+                                  if_else(grepl("Feedlot",Livestock),"Feedlot cattle",
+                                          Livestock))) %>% 
+  group_by(Year, livestock_type) %>% 
+  summarize(kg_ch4_per_head = mean(Emission_factor_kg_ch4_per_head ))
+
+unique(enteric_agg$livestock_type)
+unique(usda_livestock_use$short_desc)
+
+usda_livestock_agg <- usda_livestock_use %>%
+  mutate(livestock_type = if_else(grepl("MILK", short_desc), "Dairy Cows", 
+                                  if_else(grepl("BEEF", short_desc), "Beef Cows",
+                                          if_else(grepl("FEED", short_desc), "Feedlot cattle",
+                                                  if_else(grepl("CALVES",short_desc), "Calves",
+                                                  if_else(grepl("HOGS", short_desc),"Swine",
+                                                          if_else(grepl("SHEEP",short_desc),"Sheep",
+                                                                  short_desc))))))) %>% 
+  group_by(year, county_name) %>% 
+  summarise(head_count = sum(Value))
+
+### match livestock labels to enteric
+
+#is TOTAL overlapping with non-TOTAL fields?
+usda_livestock_use %>% filter(domain_desc == "TOTAL", grepl("CATTLE", short_desc)) %>% 
+  group_by(year) %>% 
+  summarize(cattle_count = sum(Value)) %>% print(n=50)
+
+usda_livestock_use %>% filter(domain_desc != "TOTAL", grepl("CATTLE", short_desc)) %>% 
+  group_by(year) %>% 
+  summarize(cattle_count = sum(Value))
+
+## non-TOTAL fields are from 5 year inventories, omitting inventory for now. TOTAL fields roughly triple in inventory years, digging down further
+
+
+usda_livestock_use
+
+
+usda_livestock_enteric <- usda_livestock_use %>% 
+  
 
 ### make the feedlot data long form
 mn_feedlots_long <- mn_feedlots %>% 
