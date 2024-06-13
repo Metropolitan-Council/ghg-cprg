ee3a10aad92e2ca42d39e0179eb55a5f1cd103bd
Liz Roten <liz.roten@metc.state.mn.us>
2024-06-13 16:11:25 -0500

    create tests for land cover and parks carbon data

---
+++ tests/test-land_cover_seq.R
@@ -0,0 +1,33 @@
+testthat::test_that("land cover", {
+  
+  land_cover_c <- readRDS(file.path(here::here(), "_nature/data/land_cover_carbon.rds"))
+
+  testthat::expect_equal(nrow(land_cover_c), 5)
+
+  testthat::expect_equal(
+    names(land_cover_c),
+    c("land_cover_type", "seq_mtco2e_sqkm", "stock_mtco2e_sqkm")  )
+
+  land_cover_c %>%
+    filter(land_cover_type == "Tree") %>%
+    magrittr::extract2("seq_mtco2e_sqkm") %>%
+    testthat::expect_equal(-329.0748, tolerance = 0.001)
+
+})
+
+
+testthat::test_that("sequestration totals", {
+  
+  wc_county_c <- readRDS(file.path(here::here(), "_nature/data/county_landcover_sequestration_2021.rds"))
+  
+  
+  county_seq_total <- summarize(wc_county_c, seq_total = sum(sequestration_potential))
+  county_stock_total <- summarize(wc_county_c, stock_total = sum(stock_potential))
+  
+  testthat::expect_equal(sum(county_stock_total$stock_total), -257462245)
+  
+  sum(county_seq_total$seq_total) #-2180841
+  sum(county_stock_total$stock_total) #-257,462,245
+  
+  
+})
--- tests/test-park-carbon-data.R
+++ tests/test-park-carbon-data.R
@@ -1,10 +1,27 @@
 testthat::test_that("Park carbon data values are as expected",{
   
+  cprg_county <- readRDS(file.path(here::here(),"_meta/data/cprg_county.RDS"))
+  cprg_area <- cprg_county %>%
+    mutate(area_sq_km = sf::st_area(cprg_county) %>% units::set_units("km^2") %>%
+             as.numeric())
+  
+  
   park_carbon <- readRDS(file.path(here::here(), "_nature/data/park_landcover_sequestration_2021.RDS"))
   parks <- readRDS(file.path(here::here(), "_nature/data/regional_parks_shape.RDS"))
+  carbon_stock <- readRDS(file.path(here::here(), "_meta/data/cprg_county_carbon_stock.RDS"))
+  county_emissions <- readRDS(file.path(here::here(), "_meta/data/cprg_county_emissions.RDS"))
+  
+  carbon_stock_area <- left_join(
+    carbon_stock %>%
+      group_by(geog_name) %>%
+      summarize(emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e)),
+    cprg_area,
+    by = c("geog_name" = "NAME")
+  ) %>%
+    mutate(stock_per_area = emissions_metric_tons_co2e / area_sq_km)
   
   
-  cprg_7 <- cprg_county %>% filter(!NAME %in% c("Sherburne", "Chisago", "St. Croix", "Pierce"))
+  cprg_7 <- vect(cprg_county %>% filter(!NAME %in% c("Sherburne", "Chisago", "St. Croix", "Pierce")))
   county_seq_7 <- county_emissions %>% filter(geog_name %in% cprg_7$NAME, sector == "Nature")
   county_stock_7 <- carbon_stock_area %>% filter(geog_name %in% cprg_7$NAME)
   
