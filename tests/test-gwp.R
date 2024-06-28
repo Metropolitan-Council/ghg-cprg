e40e6429b6d36d618d4feb4c080952b594422e11
Liz Roten <liz.roten@metc.state.mn.us>
2024-06-28 15:01:19 -0500

    styling

--- tests/test-gwp.R
+++ tests/test-gwp.R
@@ -6,8 +6,8 @@ testthat::test_that("GWP values", {
   testthat::expect_equal(gwp$co2, 1)
   testthat::expect_equal(gwp$ch4, 27.9)
   testthat::expect_equal(gwp$n2o, 273)
-  
-  
+
+
   # GWP AR5 values
   # testthat::expect_equal(gwp$co2, 1)
   # testthat::expect_equal(gwp$ch4, 28)
