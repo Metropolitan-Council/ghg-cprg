77047364fe3cd1b7d157d0cdf58ef0444b0a8b86
Liz Roten <liz.roten@metc.state.mn.us>
2024-06-28 12:17:05 -0500

    add commented AR5 values for quick reference

--- R/global_warming_potential.R
+++ R/global_warming_potential.R
@@ -11,3 +11,19 @@ gwp <-
     "cf4" = 7380,
     "HFC-152a" = 64
   )
+
+
+# OLD VALUES do not use ------
+# 100-year, accurate to AR5
+# * Following  revised reporting requirements under the UNFCCC, this tool presents CO2 equivalent values based on the IPCC Fifth Assessment Report (AR5) GWP values.
+# see Box 3.2, Table 1
+# Zotero: ipccClimateChange2014
+# gwp <-
+#   list(
+#     "co2" = 1,
+#     "ch4" = 28,
+#     "n2o" = 265,
+#     "cf4" = 6630,
+#     "HFC-152a" = 138
+#   )
+
