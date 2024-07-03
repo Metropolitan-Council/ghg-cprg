b607b76b0db09154eb223bbda48c5ad3b37761c7
ksmiff33 <kenneth.smith13@gmail.com>
2024-07-03 12:50:09 -0500

    Run styler::style_dir(".", recursive = TRUE, filetype = c("R", "qmd"))
    Run styler::style_dir(".", recursive = TRUE, filetype = c("R", "qmd"))

--- _nature/data-raw/nlcd_county_land_area_cover_calc.R
+++ _nature/data-raw/nlcd_county_land_area_cover_calc.R
@@ -13,11 +13,13 @@ cprg_county <- terra::vect(cprg_county)
 crs_use <- terra::crs(cprg_county)
 
 # Create an empty dataframe to store the results
-nlcd_county <- data.frame(year = numeric(),
-                         county = character(),
-                         land_cover_type = character(),
-                         area = numeric(),
-                         stringsAsFactors = FALSE)
+nlcd_county <- data.frame(
+  year = numeric(),
+  county = character(),
+  land_cover_type = character(),
+  area = numeric(),
+  stringsAsFactors = FALSE
+)
 
 start_year <- 2001
 end_year <- 2021
@@ -25,26 +27,25 @@ end_year <- 2021
 
 # Loop through all years of NLCD data and extract area estimates by land cover type and by county
 lapply(start_year:end_year, function(year) {
-  
   # browser()
-  message(paste0('\nBeginning year: ', year, '\n'))
-  message(paste0(' 0%  |____________________|'))
-  
-  
+  message(paste0("\nBeginning year: ", year, "\n"))
+  message(paste0(" 0%  |____________________|"))
+
+
   # Try to get the tree canopy cover layer
   nlcd_tcc <- try(FedData::get_nlcd( # use FedData::get_nlcd() to load nlcd data
-    cprg_county,  # area to sample over
+    cprg_county, # area to sample over
     "county", # label for current selection
     year = year, # the current year
     dataset = "canopy" # downloads tree canopy cover data only
   ) %>%
     terra::project(., crs_use), silent = TRUE)
-  
+
   # Check if the tree canopy cover layer was successfully retrieved
   tcc_available <- !inherits(nlcd_tcc, "try-error")
-  
-  message(paste0('10%  |==__________________|'))
-  
+
+  message(paste0("10%  |==__________________|"))
+
   # # Get the impervious surfaces layer
   # nlcd_is <- try(FedData::get_nlcd(
   #   cprg_county,
@@ -53,14 +54,14 @@ lapply(start_year:end_year, function(year) {
   #   dataset = "impervious" # downloads impervious surfaces data only
   # ) %>%
   #   terra::project(., crs_use), silent = TRUE)
-  # 
+  #
   # # Check if the impervious surfaces layer was successfully retrieved
   # if (inherits(nlcd_is, "try-error")) {
   #   # Move to the next year if the impervious surfaces layer is missing
   #   return(NULL)
   # }
   # message(paste0('20%  |====________________|'))
-  
+
   # Get the land cover type layer
   nlcd_lc <- try(FedData::get_nlcd(
     cprg_county,
@@ -69,46 +70,46 @@ lapply(start_year:end_year, function(year) {
     dataset = "landcover" # downloads land cover classification data only
   ) %>%
     terra::project(., crs_use), silent = TRUE)
-  
+
   # Check if the land cover type layer was successfully retrieved
   if (inherits(nlcd_lc, "try-error")) {
     # Move to the next year if the land cover type layer is missing
-    message(paste0('\nSTOP! No land cover layers for ', year, '.\nMoving to next year...'))
+    message(paste0("\nSTOP! No land cover layers for ", year, ".\nMoving to next year..."))
     return(NULL)
   }
-  
-  
-  
+
+
+
   if (tcc_available) {
-    message(paste0('30%  |======______________| - Tree canopy data available for ', year))
+    message(paste0("30%  |======______________| - Tree canopy data available for ", year))
     # Perform the necessary calculations for the current year
     nlcd_lc_mask <- terra::mask(nlcd_lc, cprg_county)
     nlcd_lc_area <- terra::mask(cellSize(nlcd_lc_mask, unit = "km"), cprg_county)
     county_raster <- terra::rasterize(cprg_county, nlcd_lc_mask, field = "NAME")
-    
-    message(paste0('40%  |========____________|'))
-    
+
+    message(paste0("40%  |========____________|"))
+
     nlcd_lc_values <- terra::extract(nlcd_lc, cprg_county)
     # nlcd_is_values <- terra::extract(nlcd_is, cprg_county)
     nlcd_tcc_values <- terra::extract(nlcd_tcc, cprg_county)
-    
-    message(paste0('50%  |==========__________|'))
-    
+
+    message(paste0("50%  |==========__________|"))
+
     # nlcd_is_values <- nlcd_is_values %>%
     #   modify_if(is.numeric, ~replace_na(., 0))
-    
+
     nlcd_tcc_values <- nlcd_tcc_values %>%
-      modify_if(is.numeric, ~replace_na(., 0))
-    
-    message(paste0('60%  |============________|'))
-    
+      modify_if(is.numeric, ~ replace_na(., 0))
+
+    message(paste0("60%  |============________|"))
+
     area_values <- terra::extract(nlcd_lc_area, cprg_county)
     lc_values <- terra::extract(nlcd_lc_mask, cprg_county)
     cty_values <- terra::extract(county_raster, cprg_county)
-    
-    message(paste0('70%  |==============______|'))
-    
-    
+
+    message(paste0("70%  |==============______|"))
+
+
     lc_df <- as_tibble(data.frame(
       county = cty_values[, 2],
       nlcd_cover = nlcd_lc_values[, 2],
@@ -116,10 +117,10 @@ lapply(start_year:end_year, function(year) {
       tree_canopy_cover = as.numeric(as.character(nlcd_tcc_values[, 2])),
       area = area_values[, 2]
     ))
-    
-    message(paste0('80%  |================____|'))
-    
-    
+
+    message(paste0("80%  |================____|"))
+
+
     lc_rc <- lc_df %>%
       mutate(
         land_cover_type = case_when(
@@ -142,60 +143,60 @@ lapply(start_year:end_year, function(year) {
           grepl("Emergent Herbaceous Wetlands", nlcd_cover) ~ "Wetland"
         )
       ) %>%
-      
       mutate(
         area_corrected = if_else(land_cover_type == "Urban_Tree", area * (tree_canopy_cover / 100), area)
       )
-    
-    
-    message(paste0('90%  |==================__|'))
-    
+
+
+    message(paste0("90%  |==================__|"))
+
     lc_county <- lc_rc %>%
       group_by(county, land_cover_type) %>%
       summarize(area = sum(area_corrected), .groups = "keep")
-    
-    
+
+
     # Add the results for the current year to the results dataframe
-    nlcd_county <<- rbind(nlcd_county, data.frame(year = year,
-                                                county = lc_county$county,
-                                                land_cover_type = lc_county$land_cover_type,
-                                                area = lc_county$area,
-                                                stringsAsFactors = FALSE))
-    
-    message(paste0('100% |====================|'))
+    nlcd_county <<- rbind(nlcd_county, data.frame(
+      year = year,
+      county = lc_county$county,
+      land_cover_type = lc_county$land_cover_type,
+      area = lc_county$area,
+      stringsAsFactors = FALSE
+    ))
+
+    message(paste0("100% |====================|"))
   } else {
-    
-    message(paste0('30%  |======______________| - Tree canopy data NOT available for ', year))
-    
+    message(paste0("30%  |======______________| - Tree canopy data NOT available for ", year))
+
     # Perform the necessary calculations for the current year
     nlcd_lc_mask <- terra::mask(nlcd_lc, cprg_county)
     nlcd_lc_area <- terra::mask(cellSize(nlcd_lc_mask, unit = "km"), cprg_county)
     county_raster <- terra::rasterize(cprg_county, nlcd_lc_mask, field = "NAME")
-    
-    message(paste0('40%  |========____________|'))
-    
+
+    message(paste0("40%  |========____________|"))
+
     nlcd_lc_values <- terra::extract(nlcd_lc, cprg_county)
     # nlcd_is_values <- terra::extract(nlcd_is, cprg_county)
     # nlcd_tcc_values <- terra::extract(nlcd_tcc, cprg_county)
-    
-    message(paste0('50%  |==========__________|'))
-    
+
+    message(paste0("50%  |==========__________|"))
+
     # nlcd_is_values <- nlcd_is_values %>%
     #   modify_if(is.numeric, ~replace_na(., 0))
-    
+
     # nlcd_tcc_values <- nlcd_tcc_values %>%
     #   modify_if(is.numeric, ~replace_na(., 0))
-    
-    message(paste0('60%  |============________|'))
-    
+
+    message(paste0("60%  |============________|"))
+
     area_values <- terra::extract(nlcd_lc_area, cprg_county)
     lc_values <- terra::extract(nlcd_lc_mask, cprg_county)
     cty_values <- terra::extract(county_raster, cprg_county)
-    
-    message(paste0('70%  |==============______|'))
-    
+
+    message(paste0("70%  |==============______|"))
+
     # browser()
-    
+
     lc_df <- as_tibble(data.frame(
       county = cty_values[, 2],
       nlcd_cover = nlcd_lc_values[, 2],
@@ -203,20 +204,18 @@ lapply(start_year:end_year, function(year) {
       tree_canopy_cover = as.numeric(NA),
       area = area_values[, 2]
     ))
-    
-    message(paste0('80%  |================____|'))
-    
-    
+
+    message(paste0("80%  |================____|"))
+
+
     lc_rc <- lc_df %>%
-      
       mutate(
         land_cover_type = case_when(
           # grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Urban_Grassland",
           # grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover > 0 ~ "Urban_Tree",
           # grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Built-up",
-          
+
           grepl("Developed", nlcd_cover) ~ "Built-up",
-          
           grepl("Deciduous Forest", nlcd_cover) ~ "Tree",
           grepl("Evergreen Forest", nlcd_cover) ~ "Tree",
           grepl("Mixed Forest", nlcd_cover) ~ "Tree",
@@ -233,36 +232,32 @@ lapply(start_year:end_year, function(year) {
           grepl("Emergent Herbaceous Wetlands", nlcd_cover) ~ "Wetland"
         )
       ) # %>%
-    # 
+    #
     # mutate(
     #   area_corrected = if_else(land_cover_type == "Urban_Tree", area * (tree_canopy_cover / 100), area)
     # )
-    
-    
-    message(paste0('90%  |==================__|'))
-    
+
+
+    message(paste0("90%  |==================__|"))
+
     lc_county <- lc_rc %>%
       group_by(county, land_cover_type) %>%
       summarize(area = sum(area), .groups = "keep")
-    
+
     # browser()
-    
-    
+
+
     # Add the results for the current year to the results dataframe
-    nlcd_county <<- rbind(nlcd_county, data.frame(year = year,
-                                                county = lc_county$county,
-                                                land_cover_type = lc_county$land_cover_type,
-                                                area = lc_county$area,
-                                                stringsAsFactors = FALSE))
-    
-    message(paste0('100% |====================|'))
-    
-    
-    
+    nlcd_county <<- rbind(nlcd_county, data.frame(
+      year = year,
+      county = lc_county$county,
+      land_cover_type = lc_county$land_cover_type,
+      area = lc_county$area,
+      stringsAsFactors = FALSE
+    ))
+
+    message(paste0("100% |====================|"))
   } # end else
-  
-  
-  
 })
 
 
@@ -278,8 +273,8 @@ nlcd_county_meta <-
     "area", class(nlcd_county$area), "Area of land cover in square kilometers. 'Urban_Tree' is scaled based on the percentage of tree canopy cover within 'Developed' areas"
   )
 
-saveRDS(nlcd_county, paste0("./_nature/data/nlcd_county_landcover_",head(sort(unique(nlcd_county$year)),1),"_",tail(sort(unique(nlcd_county$year)),1),".rds"))
-saveRDS(nlcd_county_meta, paste0("./_nature/data/nlcd_county_landcover_",head(sort(unique(nlcd_county$year)),1),"_",tail(sort(unique(nlcd_county$year)),1),"_meta.rds"))
+saveRDS(nlcd_county, paste0("./_nature/data/nlcd_county_landcover_", head(sort(unique(nlcd_county$year)), 1), "_", tail(sort(unique(nlcd_county$year)), 1), ".rds"))
+saveRDS(nlcd_county_meta, paste0("./_nature/data/nlcd_county_landcover_", head(sort(unique(nlcd_county$year)), 1), "_", tail(sort(unique(nlcd_county$year)), 1), "_meta.rds"))
 
 
 
@@ -289,8 +284,3 @@ saveRDS(nlcd_county_meta, paste0("./_nature/data/nlcd_county_landcover_",head(so
 
 
 ### LOOK INTO LAKE CARBON SEQUESTRATION
-
-
-
-
-
--- _nature/nlcd_county_land_cover_sequestration_ns.R
+++ _nature/nlcd_county_land_cover_sequestration_ns.R
@@ -5,7 +5,8 @@ nlcd_county <- readRDS("./_nature/data/nlcd_county_landcover_2001_2021.rds")
 land_cover_c <- readRDS("./_nature/data/land_cover_carbon.rds")
 
 # Compute C sequestration and stock potential for natural systems sectors
-nlcd_county_c <- nlcd_county %>% as_tibble() %>%
+nlcd_county_c <- nlcd_county %>%
+  as_tibble() %>%
   left_join(., land_cover_c, by = join_by(land_cover_type)) %>%
   filter(seq_mtco2e_sqkm < 0) %>%
   mutate(
@@ -13,40 +14,40 @@ nlcd_county_c <- nlcd_county %>% as_tibble() %>%
     stock_potential = area * stock_mtco2e_sqkm
   ) %>%
   dplyr::select(-c(seq_mtco2e_sqkm, stock_mtco2e_sqkm)) %>%
-  
   left_join( # this creates an empty grid of all desired year,livestock combinations
     x = expand.grid(
       year = seq(2001, 2021, by = 1),
       county = unique(nlcd_county$county) %>% sort(),
       land_cover_type = unique(nlcd_county$land_cover_type)
     ),
-    y = ., by = join_by(year, county, land_cover_type)) %>%
+    y = ., by = join_by(year, county, land_cover_type)
+  ) %>%
   mutate(source = ifelse(is.na(area), "est", "nlcd")) %>%
   group_by(county, land_cover_type) %>%
   arrange(year) %>%
   mutate(
     sequestration_potential2 = zoo::na.approx(sequestration_potential, na.rm = FALSE),
     stock_potential2 = zoo::na.approx(stock_potential, na.rm = FALSE),
-    sequestration_potential3 = zoo::na.approx(sequestration_potential, na.rm = FALSE, maxgap=Inf, rule=2),
-    stock_potential3 = zoo::na.approx(stock_potential, na.rm = FALSE, maxgap=Inf, rule=2),
+    sequestration_potential3 = zoo::na.approx(sequestration_potential, na.rm = FALSE, maxgap = Inf, rule = 2),
+    stock_potential3 = zoo::na.approx(stock_potential, na.rm = FALSE, maxgap = Inf, rule = 2),
     source = case_when(
       source == "est" & !is.na(sequestration_potential2) ~ "interpolated",
       source == "est" & !is.na(sequestration_potential3) ~ "extrapolated",
       source == "nlcd" ~ "nlcd"
     ),
     sequestration_potential = sequestration_potential3,
-    stock_potential = stock_potential3) %>%
+    stock_potential = stock_potential3
+  ) %>%
   dplyr::select(year, county, land_cover_type, area, sequestration_potential, stock_potential, source) %>%
   filter(land_cover_type %in% c("Grassland", "Tree", "Wetland", "Urban_Grassland", "Urban_Tree")) %>%
   arrange(year, county, land_cover_type) %>%
-  
   # Now that we've extrapolated sequestration/stock values despite having no area estimates,
   # we need to calculate area based on the extrapolated sequestration.
   left_join(., land_cover_c, by = join_by(land_cover_type)) %>% # grab your emissions factors
   # when area estimates are unavailable, but sequestration totals are available, you can estimate area
-  mutate(area = ifelse(source != "nlcd", sequestration_potential/seq_mtco2e_sqkm, area)) %>% 
+  mutate(area = ifelse(source != "nlcd", sequestration_potential / seq_mtco2e_sqkm, area)) %>%
   dplyr::select(-c(seq_mtco2e_sqkm, stock_mtco2e_sqkm))
-  
+
 
 
 nlcd_county_c_meta <-
@@ -61,9 +62,5 @@ nlcd_county_c_meta <-
     "source", class(nlcd_county_c$source), "Data source indicates whether area estimates came directly from NLCD, or whether they were interpolated/extrapolated"
   )
 
-saveRDS(nlcd_county_c, paste0("./_nature/data/nlcd_county_landcover_sequestration_",head(sort(unique(nlcd_county_c$year)),1),"_",tail(sort(unique(nlcd_county_c$year)),1),".rds"))
-saveRDS(nlcd_county_c_meta, paste0("./_nature/data/nlcd_county_landcover_sequestration_",head(sort(unique(nlcd_county_c$year)),1),"_",tail(sort(unique(nlcd_county_c$year)),1),"_meta.rds"))
-
-
-
-
+saveRDS(nlcd_county_c, paste0("./_nature/data/nlcd_county_landcover_sequestration_", head(sort(unique(nlcd_county_c$year)), 1), "_", tail(sort(unique(nlcd_county_c$year)), 1), ".rds"))
+saveRDS(nlcd_county_c_meta, paste0("./_nature/data/nlcd_county_landcover_sequestration_", head(sort(unique(nlcd_county_c$year)), 1), "_", tail(sort(unique(nlcd_county_c$year)), 1), "_meta.rds"))
