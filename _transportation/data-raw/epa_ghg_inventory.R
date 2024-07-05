cfb634f061da3a9487556943a299b3f344ebe1a5
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-05 18:23:05 -0500

    finish cleaning and importing IPCC and economic sector versions

--- _transportation/data-raw/epa_ghg_inventory.R
+++ _transportation/data-raw/epa_ghg_inventory.R
@@ -1,7 +1,8 @@
 # uses AR5 GWP values
 # https://www.epa.gov/ghgemissions/inventory-us-greenhouse-gas-emissions-and-sinks-1990-2022
+# IPCC categories -----
 source("R/_load_pkgs.R")
-clean_raw <- function(state, path){
+clean_ipcc <- function(state, path){
   read_xlsx(path,
             skip = 2) %>% 
     mutate(sector_group = c(
@@ -30,11 +31,13 @@ clean_raw <- function(state, path){
 
 state_ipcc <- 
   bind_rows(
-    clean_raw(path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Minnesota.xlsx",
-              state = "Minnesota"),
-    clean_raw(state = "Wisconsin",
-              path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Wisconsin.xlsx"))
-    
+    clean_ipcc(
+      path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Minnesota.xlsx",
+      state = "Minnesota"),
+    clean_ipcc(
+      state = "Wisconsin",
+      path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Wisconsin.xlsx"))
+
 
 state_ipcc %>% 
   group_by(sector_group, inventory_year, state) %>% 
@@ -45,3 +48,45 @@ state_ipcc %>%
     x  = ~emissions_metric_tons_co2e,
     color = ~sector_group
   )
+
+
+# Economic sectors -----
+
+clean_economic <- function(state, path){
+  read_xlsx(path,
+            skip = 2) %>% 
+    mutate(sector_group = c(
+      "remove",
+      rep("Transportation", 4),
+      "remove",
+      rep("Electric Power Industry", 5),
+      "remove",
+      rep("Industry", 36),
+      "remove",
+      rep("Agriculture", 10),
+      "remove",
+      rep("Commercial",  8),
+      "remove",
+      rep("Residential", 3),
+      "remove",
+      "LULUCF Sector Net Total",
+      rep("remove", 6)
+    )) %>% 
+    filter(sector_group != "remove") %>% 
+    select(-...3, -...19) %>% 
+    pivot_longer(cols = 2:33,
+                 names_to = "inventory_year",
+                 values_to = "emissions") %>% 
+    mutate(emissions_metric_tons_co2e = emissions * 1000000,
+           state = state)
+}
+
+
+state_economic <- bind_rows(
+  clean_economic(
+    path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Minnesota-Economic.xlsx",
+    state = "Minnesota"),
+  clean_economic(
+    state = "Wisconsin",
+    path = "_transportation/data-raw/epa/state_ghg/State-Level-GHG-data/Wisconsin-Economic.xlsx"))
+
