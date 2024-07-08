00f19f04cee745782982108ccf905cc5f7c47e42
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-08 16:07:26 -0500

    update VMT tests

--- tests/test-dot_vmt.R
+++ tests/test-dot_vmt.R
@@ -1,12 +1,14 @@
 testthat::test_that("state DOT VMT", {
   dot_vmt <- readRDS(file.path(here::here(), "_transportation/data/dot_vmt.RDS"))
 
-  testthat::expect_equal(nrow(dot_vmt), 46)
+  testthat::expect_equal(nrow(dot_vmt), 217)
 
 
   # check match with PDFs
   dot_vmt %>%
-    filter(county == "Pierce") %>%
+    filter(county == "Pierce",
+           year >= 2018) %>%
+    arrange(desc(year)) %>% 
     magrittr::extract2("daily_vmt") %>%
     testthat::expect_equal(c(1067675, 1074726, 949687, 1099233, 1068300))
 
