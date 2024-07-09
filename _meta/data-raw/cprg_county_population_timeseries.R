b7d9f563159aadcdcf7b9db9fa9d09d1139d8a66
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-09 14:20:03 -0500

    better commenting; create all metadata

--- _meta/data-raw/cprg_county_population_timeseries.R
+++ _meta/data-raw/cprg_county_population_timeseries.R
@@ -1,18 +1,19 @@
 source("R/_load_pkgs.R")
 cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 cprg_county_meta <- readRDS("_meta/data/cprg_county_meta.RDS")
+
 library(tidycensus)
 options(tidycensus.cache = TRUE)
 # To get started working with tidycensus, users should set their Census API key.
 # A key can be obtained from http://api.census.gov/data/key_signup.html.
 # census_api_key("YOUR API KEY GOES HERE")
 
-# we will pull 2021 ACS 5-Year estimates for total population
-# county level
+# we will pull ACS 1-Year and decennial estimates for total population
+# county level. 
 
-vars <- load_variables(2005, "acs1")
+vars <- load_variables("pl", year = "2020")
 
-pop_time_series <- purrr::map_dfr(
+pop_acs <- purrr::map_dfr(
   c(2005:2019, 2021:2022),
   function(x){
     tidycensus::get_acs(
@@ -20,7 +21,7 @@ pop_time_series <- purrr::map_dfr(
       year = x,
       state = "MN",
       geography = "county",
-      variables = c(total_pop = "B01001_001")
+      variables = c(total_pop = "B01003_001")
     ) %>% 
       mutate(acs_year = x)
   }
@@ -34,69 +35,91 @@ pop_time_series <- purrr::map_dfr(
           year = x,
           state = "WI",
           geography = "county",
-          variables = c(total_pop = "B01001_001")
+          variables = c(total_pop = "B01003_001")
         ) %>% 
           mutate(acs_year = x)
       }
     )) %>% 
-  filter(GEOID %in% cprg_county$GEOID)
-
-tst <- ipumsr::get_metadata_nhgis("time_series_tables")
-
-tidycensus::get_acs(
-  survey = "acs1",
-  year = ,
-  state = "MN",
-  geography = "county",
-  variables = c(total_pop = "DP05_0001E")
-)
-
-
-cprg_population <- tidycensus::get_acs(
-  survey = "acs5",
-  year = 2021,
-  state = "MN",
-  geography = "county",
-  variables = c(total_pop = "DP05_0001E")
-) %>%
-  bind_rows(
-    tidycensus::get_acs(
-      survey = "acs5",
-      year = 2021,
-      state = "WI",
-      geography = "county",
-      variables = c(total_pop = "DP05_0001E")
-    )
-  ) %>%
-  select(-NAME, -moe) %>%
-  right_join(
-    cprg_county,
-    by = c("GEOID")
-  ) %>%
-  sf::st_drop_geometry() %>%
+  filter(GEOID %in% cprg_county$GEOID) %>% 
   mutate(
-    population = estimate,
-    population_data_source = "ACS 5-Year Estimates 2021, Table DP05"
-  ) %>%
-  select(
-    names(cprg_county),
-    population,
-    population_data_source,
-    -geometry
-  ) %>%
-  arrange(STATE, NAME)
+    population_data_source = "ACS 1-Year Estimates, Table B01003",
+    population = estimate
+  )
 
-cprg_population
 
-cprg_population_meta <- bind_rows(
+pop_decennial <- 
+  bind_rows(
+    purrr::map_dfr(
+      c(2010, 2020),
+      function(x){
+        tidycensus::get_decennial(
+          geography = "county",
+          state = "MN",
+          variables = c(total_pop = "P1_001N")
+        ) %>% 
+          mutate(decennial_year = x)
+      }
+    ),
+    purrr::map_dfr(
+      c(2010, 2020),
+      function(x){
+        tidycensus::get_decennial(
+          geography = "county",
+          state = "WI",
+          variables = c(total_pop = "P1_001N")
+        ) %>% 
+          mutate(decennial_year = x)
+      }
+    )) %>% 
+  filter(GEOID %in% cprg_county$GEOID) %>% 
+  mutate(
+    population = value,
+    population_data_source = "Decennial Census, Table P1"
+  )
+
+
+# confirm that the 2010 and 2020 population estimates
+# to ensure they are the same across tables
+pop_decennial %>% 
+  left_join(pop_acs, by = c("decennial_year" = "acs_year",
+                                    "GEOID",
+                                    "NAME"))
+  
+  
+
+# combine all 
+cprg_population_timeseries <- 
+  pop_acs %>% 
+  select(GEOID, population_year = acs_year, population, population_data_source) %>% 
+  # remove 2010 and 2020 from acs 1 year
+  filter(!population_year %in% c(2010, 2020)) %>% 
+  # bind pop decennial
+  bind_rows(pop_decennial %>% 
+              select(GEOID, 
+                     population_year = decennial_year,
+                     population, population_data_source)) %>% 
+  # join with cprg_county to get metadata
+  right_join(
+    cprg_county %>% 
+      sf::st_drop_geometry(),
+    by = c("GEOID")
+  ) %>% 
+  arrange(STATE, NAME) %>% 
+  select(names(cprg_county)[1:7],
+         population_year, population, population_data_source)
+  
+
+# create metadata
+cprg_population_timeseries_meta <- bind_rows(
   cprg_county_meta,
   tribble(
     ~Column, ~Class, ~Description,
-    "population", class(cprg_population$population), "Total county population estimate",
-    "population_data_source", class(cprg_population$population_data_source), "Population estimate data source"
+    "population_year", class(cprg_population_timeseries$population_year), "Population estimate year",
+    "population", class(cprg_population_timeseries$population), "Total county population estimate",
+    "population_data_source", class(cprg_population_timeseries$population_data_source), "Population estimate data source"
   )
 ) %>%
-  filter(Column %in% names(cprg_population))
+  filter(Column %in% names(cprg_population_timeseries))
 
-saveRDS(cprg_population, "_meta/data/cprg_population.RDS")
-saveRDS(cprg_population_meta, "_meta/data/cprg_population_meta.RDS")
+saveRDS(cprg_population_timeseries, "_meta/data/cprg_population_timeseries.RDS")
+saveRDS(cprg_population_timeseries_meta, "_meta/data/cprg_population_timeseries_meta.RDS")
