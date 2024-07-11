e76b9f008ce07ae1c7d1da0a28589c414562ec2e
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-11 14:40:50 -0500

    lots of work calculating various emissions

--- _transportation/data-raw/epa_ghg_state.R
+++ _transportation/data-raw/epa_ghg_state.R
@@ -6,9 +6,6 @@ cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 source("R/global_warming_potential.R")
 
 
-# TODO read in and convert gas emissions to CO2e, finish cleaning
-# use these tables to get county/regional emissions!
-# 
 # ipcc sectors -----
 ipcc_sectors <- readxl::read_xlsx("_transportation/data-raw/epa/state_ghg/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx",
   sheet = 2
@@ -25,19 +22,54 @@ ipcc_sector_category <- ipcc_sectors %>%
 ipcc <- ipcc_sectors %>%
   filter(
     state %in% c("MN", "WI")
-    # category == "Transportation"
   ) %>%
   pivot_longer(starts_with("y"),
-    names_to = "year",
-    values_to = "value"
+    names_to = "inventory_year",
+    values_to = "emission_grams"
   ) %>%
-  # reported in millions of metric tons
-  mutate(ghg_equiv = value * 1000000) %>% # this value is CO2e according to documentation
-  # select(-subcategory2, -subcategory3, -subcategory4) %>%
-  mutate(
-    inventory_year = stringr::str_remove(year, "y")
+  mutate(inventory_year = str_remove(inventory_year, "y"),
+         # reported in millions of metric tons
+         # so convert to metric tons then to grams
+         emission_grams = as.numeric(emission_grams) * 1000000 %>% 
+           units::as_units("metric_ton") %>% 
+           units::set_units("gram") %>% 
+           as.numeric()) 
+  
+
+ipcc_transportation <-
+  
+  ipcc %>% 
+  filter(category == "Transportation",
+         # these are things like asphalt, paints, 
+         # natural gas is used to move fuel through pipelines and in LNG vehicles
+         # subsector != "Non-Energy Uses of Fossil Fuels",
+         # inventory_year == 2021,
+         !is.na(subsector)
+         # state == "MN"
+         ) %>% 
+  select(-subcategory2, -subcategory3, -subcategory4, -rownumber) %>% 
+  pivot_wider(names_from = "ghg",
+              values_from = "emission_grams",
+              values_fill = 0) %>% 
+  clean_names() %>% 
+  group_by(sector, subsector,  category,  fuel, subcategory1, inventory_year, state) %>% 
+  summarize(co2 = sum(co2),
+            ch4 = sum(ch4),
+            n2o = sum(n2o),
+            .groups = "keep") %>%
+  group_by(sector, subsector, category, subcategory1, inventory_year, state) %>% 
+  summarize(
+    co2_co2_equivalent =
+      sum(co2, (ch4 * gwp$ch4), (n2o * gwp$n2o)),
+    emissions_metric_tons_co2e = co2_co2_equivalent / 1000000,
+    million_metric_tons_co2e = emissions_metric_tons_co2e / 1000000
   )
 
+# for cross-reference
+# MN 2021 as shown in GHG Data Explorer is the sum of CO2 for fossil fuel combustion
+# fuel types Petroleum and Natural Gas
+
+unique(ipcc_transportation$fuel)
 
 ipcc %>%
   group_by(
@@ -55,11 +87,11 @@ econ_sectors <- readxl::read_xlsx("_transportation/data-raw/epa/state_ghg/allsta
 
 econ <- econ_sectors %>%
   filter(state %in% c("MN", "WI")) %>%
-  select(-subcategory4) %>% 
+  select(-subcategory4, -rownumber) %>% 
   pivot_longer(starts_with("y"),
     names_to = "inventory_year",
     values_to = "emission_grams"
-  ) %>%
+  ) %>% 
   mutate(inventory_year = str_remove(inventory_year, "y"),
          # reported in millions of metric tons
          # so convert to metric tons then to grams
@@ -67,8 +99,26 @@ econ <- econ_sectors %>%
            units::as_units("metric_ton") %>% 
            units::set_units("gram") %>% 
            as.numeric()) %>% 
+  unique()
+
+
+
+econ_transportation <- econ %>% 
+  filter(econ_sector == "Transportation") %>% 
+  group_by(econ_sector, econ_source, subsector, subcategory1, 
+           state, ghg, inventory_year) %>% 
+  summarize(emission_grams = sum(emission_grams),
+            .groups = "keep") %>% 
   pivot_wider(names_from = "ghg",
-              values_from = "emission_grams")
+              values_from = "emission_grams",
+              values_fill = 0) %>% 
+  clean_names() 
+  mutate(
+    co2_co2_equivalent =
+      sum(co2, (ch4 * gwp$ch4), (n2o * gwp$n2o)),
+    emissions_metric_tons_co2e = co2_co2_equivalent / 1000000
+  )
+
 
 # compare line numbers, sectors -----
 
