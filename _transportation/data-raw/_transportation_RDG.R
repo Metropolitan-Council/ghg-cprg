f23c62e4c7380c89da7dab00b95b809c4a0a702e
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-12 12:56:14 -0500

    add subsector data

--- _transportation/data-raw/_transportation_RDG.R
+++ _transportation/data-raw/_transportation_RDG.R
@@ -2,7 +2,7 @@
 source("R/_load_pkgs.R")
 # read in pre-processed proportions
 epa_nei_county_proportions <- readRDS("_transportation/data/epa_nei_county_proportions.RDS")
-
+epa_nei <- readRDS("_transportation/data/epa_nei.RDS")
 cprg_county_emission <- readRDS("_meta/data/cprg_county_emissions.RDS")
 
 cprg_county_emission %>% 
@@ -23,3 +23,20 @@ transport_baseline_current <- epa_nei_county_proportions %>%
 
 saveRDS(transport_baseline_current, "_transportation/data/county_emissions_RDG.RDS")
 
+
+# with subsectors
+
+transport_baseline_subsectors <- epa_nei %>% 
+  ungroup() %>% 
+  filter(
+    nei_inventory_year %in% c(2008, 2020)) %>% 
+  select(nei_inventory_year,vehicle_weight_label,
+         county_name, emissions_metric_tons_co2e) %>% 
+  group_by(nei_inventory_year, 
+           county_name,
+           vehicle_weight_label) %>% 
+  summarize(emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e),
+            .groups = "keep") %>%
+  mutate(year = ifelse(nei_inventory_year == 2008, 2005, 2021))
+
+saveRDS(transport_baseline_subsectors, "_transportation/data/county_emissions_subsectors.RDG.RDS")
