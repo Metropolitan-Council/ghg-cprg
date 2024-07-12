436c99c0285c993684bf5e0024dd537c8669792d
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-12 12:49:18 -0500

    save transportation emissions
    using 2008 and 2020 as proxies for 2005 and 2021 (for now)

---
+++ _transportation/data-raw/_transportation_RDG.R
@@ -0,0 +1,25 @@
+# Quick and dirty compilation for the RDG 90% draft
+source("R/_load_pkgs.R")
+# read in pre-processed proportions
+epa_nei_county_proportions <- readRDS("_transportation/data/epa_nei_county_proportions.RDS")
+
+cprg_county_emission <- readRDS("_meta/data/cprg_county_emissions.RDS")
+
+cprg_county_emission %>% 
+  group_by(year, sector) %>% 
+  summarize(emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e))
+
+transport_baseline_current <- epa_nei_county_proportions %>% 
+  ungroup() %>% 
+  filter(
+    nei_inventory_year %in% c(2008, 2020)) %>% 
+  select(nei_inventory_year, county_name, county_emissions_metric_tons_co2e) %>% 
+  group_by(nei_inventory_year, 
+           county_name) %>% 
+  summarize(emissions_metric_tons_co2e = sum(county_emissions_metric_tons_co2e),
+            .groups = "keep") %>%
+  mutate(year = ifelse(nei_inventory_year == 2008, 2005, 2021))
+
+
+saveRDS(transport_baseline_current, "_transportation/data/county_emissions_RDG.RDS")
+
