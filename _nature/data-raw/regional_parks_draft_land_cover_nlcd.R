cd3b986ccd89d22fff501e5c0d74d4cb5732ed31
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-07-17 15:30:13 -0500

    regional park nlcd analysis for RDG

--- _nature/data-raw/regional_parks_draft_land_cover_nlcd.R
+++ _nature/data-raw/regional_parks_draft_land_cover_nlcd.R
@@ -1,12 +1,22 @@
 source("R/_load_pkgs.R")
 source("R/cprg_colors.R")
 
+## stock and sequestration values
+land_cover_c <- readRDS("./_nature/data/land_cover_carbon.rds")
+
+
+### seven county shapefile
 cprg_county <- readRDS("_meta/data/cprg_county.RDS") %>% 
   filter(!NAME %in% c("Chisago","Sherburne","St. Croix","Pierce"))
 
+### seven county land cover (NLCD)
 county_lc <- readRDS("_nature/data/nlcd_county_landcover_2001_2021.rds") %>% 
   filter(!county %in% c("Chisago","Sherburne","St. Croix","Pierce"),
          year == 2021)
+
+### parks land cover (ESA)
+# parks_esa <- readRDS("_nature/data/worldcover_parks.rds") #raster
+parks_esa_lc <- readRDS("_nature/data/park_landcover_2021.rds")
 
 ### get Mpls, St Paul, and BLoomington shapefiles
 
@@ -40,9 +50,10 @@ cities <- terra::vect(cities) %>%
 
 ## load regional park shapefile
 
-parks <- vect("./_nature/data-raw/DBO_RPTAdministrativeBoundary.shp")
+parks <- vect("./_nature/data-raw/DBO_RPTAdministrativeBoundary.shp")%>% 
+  project(., crs_use)
 
-parks_use <- parks %>% filter(Implementa == "Operational", !Category %in% c("Conservation Area", "County Park", "Natural Area")) 
+parks_use <- parks %>% filter(Implementa == "Operational", !Category %in% c("Conservation Area", "County Park", "Natural Area"))
 
 ### start with area calculations
 
@@ -59,135 +70,207 @@ areas <- bind_rows(
     data.frame(geog_name = cities$CTU_NAME, geog_area = expanse(cities, unit="km"))
 ) %>% ### bring in park areas and merge to common geography
 left_join(., 
-    data.frame(agency = parks_use$AgencyMana, acres = parks_use$AcresCalcu / 247.1) %>% 
+    data.frame(agency = parks_use$AgencyMana, sqkm = parks_use$AcresCalcu / 247.1) %>% 
     group_by(agency) %>% 
-    summarize(park_area = sum(acres))%>%
+    summarize(park_area_sqkm = sum(sqkm))%>%
       mutate(geog_name = str_extract(agency, "^[^ ]+"),
              geog_name = if_else(geog_name == "Three", "Hennepin", geog_name),
              geog_name = if_else(geog_name == "Saint", "Saint Paul", geog_name)) %>% 
-      select(geog_name, park_area)
+      select(geog_name, park_area_sqkm)
            ) %>% 
-  mutate(nonpark_area = geog_area - park_area)
+  mutate(nonpark_area_sqkm = geog_area - park_area_sqkm) %>% 
+  select(-geog_area)
 
-nlcd_lc <- get_nlcd(
-  parks,
+
+nlcd_lc_parks <- get_nlcd(
+  parks_use,
   "county",
   year = 2021,
   dataset = "landcover"
 ) %>%
-  project(., crs_use) %>% # reproject
-  resample(., wc_parks) # resample nlcd raster to match more granular ESA World Cover raster
+  terra::project(., crs_use) # reproject
+ 
 
-nlcd_is <- get_nlcd(
-  parks,
-  "county",
-  year = 2021,
-  dataset = "impervious"
-) %>%
-  project(., crs_use) %>%
-  resample(., wc_parks)
-
-nlcd_tcc <- FedData::get_nlcd( # use FedData::get_nlcd() to load nlcd data
-  cprg_county, # area to sample over
+nlcd_tcc_parks <- FedData::get_nlcd( # use FedData::get_nlcd() to load nlcd data
+  parks_use, # area to sample over
   "county", # label for current selection
-  year = year, # the current year
+  year = 2021, # the current year
   dataset = "canopy" # downloads tree canopy cover data only
 ) %>%
-  terra::project(., crs_use), silent = TRUE)
+  terra::project(., crs_use)
+
+### extract needed data and repackage into data frame
+nlcd_lc_mask_parks <- terra::mask(nlcd_lc_parks, parks_use)
+nlcd_tcc_mask_parks <- terra::mask(nlcd_tcc_parks, parks_use)
+nlcd_lc_area_parks <- terra::mask(cellSize(nlcd_lc_mask_parks, unit = "km"), parks_use)
+parks_raster <- terra::rasterize(parks_use, nlcd_lc_mask_parks, field = "AgencyMana")
+
+nlcd_lc_values_parks <- terra::extract(nlcd_lc_parks, parks_use)
+nlcd_tcc_values_parks <- terra::extract(nlcd_tcc_parks, parks_use)
+nlcd_tcc_values_parks <- nlcd_tcc_values_parks %>%
+  modify_if(is.numeric, ~ replace_na(., 0))
+area_values_parks <- terra::extract(nlcd_lc_area_parks, parks_use)
+parks_values <- terra::extract(parks_raster, parks_use)
 
 
-## reduce raster size before merging
-wc_red_list <- lapply(paste0("wc", 1:4), function(x) terra::crop(get(x), cprg_county))
-# merge raster to get full area
-wc_combined <- do.call(mosaic, c(wc_red_list, fun = "min"))
-
-# if you want to take a peek
-# plot(wc_combined)
-
-# define CRS for using with other layers
-crs_use <- terra::crs(wc_combined)
-
-# convert tigris data to SpatVector (terra) and reproject
-cprg_county <- vect(cprg_county)
-cprg_county <- project(cprg_county, crs_use)
-parks <- project(parks, crs_use)
-
-# remove non-county pixels
-wc_parks <- mask(wc_combined, parks)
-# check
-# terra::plot(wc_outline)
-
-#saveRDS(wc_parks, "./_nature/data/worldcover_parks.rds")
-
-# calculate the area of each pixel (raster cells are skewed at higher latitudes)
-area_wc <- mask(cellSize(wc_parks, unit = "km"), parks)
-
-# rasterize cprg_county to assign pixels to counties
-parks_raster <- rasterize(parks, wc_parks, field = "AgencyMana")
-parks_implementa_raster <- rasterize(parks, wc_parks, field = "Implementa")
-county_raster <- rasterize(cprg_county, parks_raster, field = "NAME")
-
-
-
-# nicer plot - move to qmd later
-# ggplot() + geom_spatraster(data= wc_factor) +
-#   scale_fill_manual(breaks = code_class_tab$old_lab,
-#                     labels = code_class_tab$new_lab,
-#                     values=esa_color,
-#                     na.value="white",
-#                     name = 'World Cover Class') + theme_void()
-
-### load nlcd land cover and impervious surface data and match to WC data
-
-
-
-# convert all rasters to data.frame
-
-#### convert all rasters to data.frame. These are memory intensive steps for full county area.
-nlcdlc_values <- extract(nlcd_lc, parks)
-nlcdis_values <- extract(nlcd_is, parks)
-# NAs to 0s
-nlcdis_values[is.na(nlcdis_values$county_NLCD_Impervious_2021), ]$county_NLCD_Impervious_2021 <- 0
-
-area_values <- extract(area_wc, parks)
-wc_values <- extract(wc_parks, parks)
-cty_values <- extract(county_raster, parks)
-parks_values <- extract(parks_raster, parks)
-parks_implementa_values <- extract(parks_implementa_raster, parks)
-
-#### combine raster values into one dataframe
-parks_df <- data.frame(
-  county = cty_values[, 2],
+lc_df <- as_tibble(data.frame(
   park = parks_values[, 2],
-  park_implementa = parks_implementa_values[, 2],
-  nlcd_cover = nlcdlc_values[, 2],
-  impervious_cover = as.numeric(as.character(nlcdis_values[, 2])),
-  area = area_values[, 2],
-  wc_lab = wc_values[, 2]
-) %>%
-  left_join(., code_class_tab, by = c("wc_lab" = "old_lab")) %>% # join in ESA WC land class labels
+  nlcd_cover = nlcd_lc_values_parks[, 2],
+  # impervious_cover = as.numeric(as.character(nlcd_is_values[, 2])),
+  tree_canopy_cover = as.numeric(as.character(nlcd_tcc_values_parks[, 2])),
+  area = area_values_parks[, 2]
+))
+
+# reduce to categories of interest
+lc_rc <- lc_df %>%
   mutate(
-    area_corrected = if_else(esa_label == "Tree", area * (1 - (impervious_cover / 100)), area), # downscale trees by inverse of % impervious
-    land_cover_type = if_else(grepl("Developed", nlcd_cover) & esa_label %in% c("Grassland", "Cropland", "Tree"),
-      paste0("Urban_", esa_label),
-      esa_label
+    land_cover_type = case_when(
+      grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Urban_Grassland",
+      grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover > 0 ~ "Urban_Tree",
+      grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Built-up",
+      grepl("Deciduous Forest", nlcd_cover) ~ "Tree",
+      grepl("Evergreen Forest", nlcd_cover) ~ "Tree",
+      grepl("Mixed Forest", nlcd_cover) ~ "Tree",
+      grepl("Dwarf Scrub", nlcd_cover) ~ "Shrubland",
+      grepl("Shrub/Scrub", nlcd_cover) ~ "Shrubland",
+      grepl("Grassland/Herbaceous", nlcd_cover) ~ "Grassland",
+      grepl("Sedge/Herbaceous", nlcd_cover) ~ "Grassland",
+      grepl("Cultivated Crops", nlcd_cover) ~ "Cropland",
+      grepl("Pasture/Hay", nlcd_cover) ~ "Cropland",
+      grepl("Barren Land", nlcd_cover) ~ "Bare",
+      grepl("Perennial Ice/Snow", nlcd_cover) ~ "Snow",
+      grepl("Open Water", nlcd_cover) ~ "Water",
+      grepl("Woody Wetlands", nlcd_cover) ~ "Tree", ## Changed from "Wetland"
+      grepl("Emergent Herbaceous Wetlands", nlcd_cover) ~ "Wetland"
     )
-  ) # relabel natural areas in developed zones as urban_ as these likely have diminished C sequestration potential
-
-# reduce data frame to sum of all pixels (i.e. area) of designated land class system within each county
-wc_parks <- parks_df %>%
-  group_by(park, land_cover_type,park_implementa) %>%
-  summarize(area = sum(area_corrected))
-
-# create metadata
-wc_parks_meta <-
-  tibble::tribble(
-    ~"Column", ~"Class", ~"Description",
-    "park ", class(wc_parks$park), "Park Implementing Agency",
-    "land_cover_type", class(wc_parks$land_cover_type), "Land cover type from World Cover. 'Urban_' indicates a natural area within NLCD designated developed land cover",
-    "park_implementa", class(wc_parks$park_implementa), "Park land acquisition status - not all categories finalized",
-    "area", class(wc_parks$area), "Area of land cover in square kilometers. 'Urban_Tree' is scaled down by NLCD percent impervious"
+  ) %>%
+  mutate(
+    area_corrected = if_else(land_cover_type == "Urban_Tree", area * (tree_canopy_cover / 100), area)
   )
 
-saveRDS(wc_parks, "./_nature/data/park_landcover_draft_2021.rds")
-saveRDS(wc_parks_meta, "./_nature/data/park_landcover_draft_2021_meta.rds")
+# summarize and bring in sequestration and stock values
+lc_park <- lc_rc %>%
+  group_by(park, land_cover_type) %>%
+  summarize(area = sum(area_corrected), .groups = "keep") %>%  
+  mutate(geog_name = str_extract(park, "^[^ ]+"),
+         geog_name = if_else(geog_name == "Three", "Hennepin", geog_name),
+         geog_name = if_else(geog_name == "Saint", "Saint Paul", geog_name)) %>% 
+  ungroup() %>% 
+  select(-park)
+
+park_c <- left_join(lc_park, land_cover_c) %>% 
+  filter(!is.na(stock_mtco2e_sqkm)) %>% 
+  mutate(mtco2e_seq_yr = seq_mtco2e_sqkm * area,
+         mtco2e_stock = stock_mtco2e_sqkm * area) %>% 
+  group_by(geog_name) %>% 
+  summarize(mtco2e_seq_yr_park = sum(mtco2e_seq_yr),
+            mtco2e_stock_park = sum(mtco2e_stock)) %>% 
+  filter(!is.na(geog_name))  %>% 
+  select(geog_name, mtco2e_seq_yr_park, mtco2e_stock_park)
+
+### get cities land cover type 
+  
+nlcd_lc_cities <- get_nlcd(
+  cities,
+  "county",
+  year = 2021,
+  dataset = "landcover"
+) %>%
+  terra::project(., crs_use) # reproject
+
+
+nlcd_tcc_cities <- FedData::get_nlcd( # use FedData::get_nlcd() to load nlcd data
+  cities, # area to sample over
+  "county", # label for current selection
+  year = 2021, # the current year
+  dataset = "canopy" # downloads tree canopy cover data only
+) %>%
+  terra::project(., crs_use)
+
+### extract needed data and repackage into data frame
+nlcd_lc_mask_cities <- terra::mask(nlcd_lc_cities, cities)
+nlcd_tcc_mask_cities <- terra::mask(nlcd_tcc_cities, cities)
+nlcd_lc_area_cities <- terra::mask(cellSize(nlcd_lc_mask_cities, unit = "km"), cities)
+cities_raster <- terra::rasterize(cities, nlcd_lc_mask_cities, field = "CTU_NAME")
+
+nlcd_lc_values_cities <- terra::extract(nlcd_lc_cities, cities)
+nlcd_tcc_values_cities <- terra::extract(nlcd_tcc_cities, cities)
+nlcd_tcc_values_cities <- nlcd_tcc_values_cities %>%
+  modify_if(is.numeric, ~ replace_na(., 0))
+area_values_cities <- terra::extract(nlcd_lc_area_cities, cities)
+cities_values <- terra::extract(cities_raster, cities)
+
+
+lc_df <- as_tibble(data.frame(
+  city = cities_values[, 2],
+  nlcd_cover = nlcd_lc_values_cities[, 2],
+  # impervious_cover = as.numeric(as.character(nlcd_is_values[, 2])),
+  tree_canopy_cover = as.numeric(as.character(nlcd_tcc_values_cities[, 2])),
+  area = area_values_cities[, 2]
+))
+
+# reduce to categories of interest
+lc_rc <- lc_df %>%
+  mutate(
+    land_cover_type = case_when(
+      grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Urban_Grassland",
+      grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover > 0 ~ "Urban_Tree",
+      grepl("Developed", nlcd_cover) & !grepl("Developed, Open Space", nlcd_cover) & tree_canopy_cover == 0 ~ "Built-up",
+      grepl("Deciduous Forest", nlcd_cover) ~ "Tree",
+      grepl("Evergreen Forest", nlcd_cover) ~ "Tree",
+      grepl("Mixed Forest", nlcd_cover) ~ "Tree",
+      grepl("Dwarf Scrub", nlcd_cover) ~ "Shrubland",
+      grepl("Shrub/Scrub", nlcd_cover) ~ "Shrubland",
+      grepl("Grassland/Herbaceous", nlcd_cover) ~ "Grassland",
+      grepl("Sedge/Herbaceous", nlcd_cover) ~ "Grassland",
+      grepl("Cultivated Crops", nlcd_cover) ~ "Cropland",
+      grepl("Pasture/Hay", nlcd_cover) ~ "Cropland",
+      grepl("Barren Land", nlcd_cover) ~ "Bare",
+      grepl("Perennial Ice/Snow", nlcd_cover) ~ "Snow",
+      grepl("Open Water", nlcd_cover) ~ "Water",
+      grepl("Woody Wetlands", nlcd_cover) ~ "Tree", ## Changed from "Wetland"
+      grepl("Emergent Herbaceous Wetlands", nlcd_cover) ~ "Wetland"
+    )
+  ) %>%
+  mutate(
+    area_corrected = if_else(land_cover_type == "Urban_Tree", area * (tree_canopy_cover / 100), area)
+  )
+
+# summarize and bring in sequestration and stock values
+lc_city <- lc_rc %>%
+  group_by(city, land_cover_type) %>%
+  summarize(area = sum(area_corrected), .groups = "keep") %>% 
+  rename(geog_name = city)
+
+lc_city_county <- lc_rc %>% 
+  mutate(county = if_else(city == "Saint Paul","Ramsey","Hennepin")) %>% 
+  group_by(county, land_cover_type) %>%
+  summarize(area = sum(area_corrected), .groups = "keep")
+  
+### need to first subtract city lc areas from counties, then park areas from relevant geogs
+lc_county <- left_join(county_lc %>% filter(!is.na(land_cover_type)),
+                     lc_city_county,
+                     by = c("county","land_cover_type")) %>% 
+  mutate(area.y = replace_na(area.y, 0),
+         area = area.x - area.y) %>% 
+  select(geog_name = county, land_cover_type,area)
+
+lc_geog <- bind_rows(lc_city,lc_county) %>% 
+  left_join(., lc_park, by = c("geog_name", "land_cover_type"), suffix = c("_geog","_park")) %>% 
+  mutate(area_nonpark = area_geog - area_park)
+
+geog_c <- left_join(lc_geog, land_cover_c) %>% 
+  filter(!is.na(stock_mtco2e_sqkm)) %>% 
+  mutate(mtco2e_seq_yr = seq_mtco2e_sqkm * area_nonpark ,
+         mtco2e_stock = stock_mtco2e_sqkm * area_nonpark ) %>% 
+  group_by(geog_name) %>% 
+  summarize(mtco2e_seq_yr_nonpark = sum(mtco2e_seq_yr),
+            mtco2e_stock_nonpark = sum(mtco2e_stock)) %>% 
+  filter(!is.na(geog_name))  %>% 
+  select(geog_name, mtco2e_seq_yr_nonpark, mtco2e_stock_nonpark)
+  
+out <- left_join(areas, geog_c) %>% 
+  left_join(., park_c)
+
+write_csv(out,
+          "_nature/data/implementing_agency_park_c_nlcd_2021.csv")
