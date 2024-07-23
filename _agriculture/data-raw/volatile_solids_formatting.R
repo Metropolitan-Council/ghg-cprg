6f792f090596d05a71b60a8c462847bde541ea75
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-07-23 16:05:03 -0500

    volatile solid formatting

--- _agriculture/data-raw/volatile_solids_formatting.R
+++ _agriculture/data-raw/volatile_solids_formatting.R
@@ -22,85 +22,109 @@ vs_dairy_cows <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
          livestock_type = "Dairy Cows",
          year = as.numeric(year))
 
-enteric_dairy_calves <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                   sheet = "enteric EFsNEW",
-                                   range = "A159:AK209") %>% 
-  rename(state = `Dairy Replacements Total`) %>% 
+vs_dairy_calves <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                                   sheet = "VS-CattleNEW",
+                                   range = "A55:AK105") %>% 
+  rename(state = `Dairy Replacements`) %>% 
   filter(state %in% c("Minnesota", "Wisconsin")) %>% 
-  pivot_longer(cols = -1, names_to = "year", values_to = "kgch4_head_yr") %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr  / 1000,
+  pivot_longer(cols = -1, names_to = "year", values_to = "kg_vs_head_yr") %>% 
+  mutate(mt_vs_head_yr = kg_vs_head_yr  / 1000,
          livestock_type = "Calves",
          year = as.numeric(year))
 
-enteric_beef_cows <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                         sheet = "enteric EFsNEW",
-                                         range = "A211:AK261") %>% 
+vs_beef_cows <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                                         sheet = "VS-CattleNEW",
+                                         range = "A107:AK157") %>% 
   rename(state = `Beef Cows`) %>% 
   filter(state %in% c("Minnesota", "Wisconsin")) %>% 
-  pivot_longer(cols = -1, names_to = "year", values_to = "kgch4_head_yr") %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr  / 1000,
+  pivot_longer(cols = -1, names_to = "year", values_to = "kg_vs_head_yr") %>% 
+  mutate(mt_vs_head_yr = kg_vs_head_yr  / 1000,
          livestock_type = "Beef Cows",
          year = as.numeric(year))
 
-enteric_beef_calves <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                          sheet = "enteric EFsNEW",
-                                          range = "A367:AK417") %>% 
-  rename(state = `Beef Replacements Total`) %>% 
+vs_beef_calves <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                                          sheet = "VS-CattleNEW",
+                                          range = "A159:AK209") %>% 
+  rename(state = `Beef Replacements`) %>% 
   filter(state %in% c("Minnesota", "Wisconsin")) %>% 
-  pivot_longer(cols = -1, names_to = "year", values_to = "kgch4_head_yr") %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr  / 1000,
+  pivot_longer(cols = -1, names_to = "year", values_to = "kg_vs_head_yr") %>% 
+  mutate(mt_vs_head_yr = kg_vs_head_yr  / 1000,
          livestock_type = "Calves",
          year = as.numeric(year))
 
-enteric_feedlot_steer <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                         sheet = "enteric EFsNEW",
-                                         range = "A523:AK573") %>% 
+vs_feedlot_steer <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                                         sheet = "VS-CattleNEW",
+                                         range = "A315:AK365") %>% 
   rename(state = `Steer Feedlot`) %>% 
   filter(state %in% c("Minnesota", "Wisconsin")) %>% 
-  pivot_longer(cols = -1, names_to = "year", values_to = "kgch4_head_yr") %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr / 1000,
+  pivot_longer(cols = -1, names_to = "year", values_to = "kg_vs_head_yr") %>% 
+  mutate(mt_vs_head_yr = kg_vs_head_yr / 1000,
          livestock_type = "Feedlot Cattle",
          year = as.numeric(year))
 
-enteric_feedlot_heifer <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                           sheet = "enteric EFsNEW",
-                                           range = "A575:AK625") %>% 
+vs_feedlot_heifer <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                                           sheet = "VS-CattleNEW",
+                                           range = "A367:AK417") %>% 
   rename(state = `Heifer Feedlot`) %>% 
   filter(state %in% c("Minnesota", "Wisconsin")) %>% 
-  pivot_longer(cols = -1, names_to = "year", values_to = "kgch4_head_yr") %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr / 1000,
+  pivot_longer(cols = -1, names_to = "year", values_to = "kg_vs_head_yr") %>% 
+  mutate(mt_vs_head_yr = kg_vs_head_yr / 1000,
          livestock_type = "Feedlot Cattle",
          year = as.numeric(year))
 
 # constants for sheep, goats, swine, horses in control data sheet
-enteric_other <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
-                                                             sheet = "Control",
-                                                             range = "A26:B30") %>% 
-  rename(livestock_type = `Other`,
-         kgch4_head_yr = `...2`) %>% 
-  mutate(mt_ch4_head_yr = kgch4_head_yr / 1000) %>% 
-  crossing(year = 2005:2021,
-           state = c("Minnesota", "Wisconsin"))
+vs_other <- readxl::read_xlsx("_agriculture/data-raw/ag-module.xlsx",
+                              "VS-CattleNEW",
+                              range = "AO3:BE39") %>% 
+  pivot_longer(cols = -1, names_to = "livestock_type_temp", values_to = "kg_vs_1000kg_animal") %>% 
+  mutate(mt_vs_1000kg_animal = kg_vs_1000kg_animal / 1000,
+         year = as.numeric(Year),
+         livestock_type = case_when(
+           grepl("calf_nof", livestock_type_temp) ~ "Calves",
+           grepl("goats", livestock_type_temp) ~ "Goats",
+           grepl("horses", livestock_type_temp) ~ "Horses",
+           grepl("poultry_broilers", livestock_type_temp) ~ "Broilers",
+           grepl("poultry_layers", livestock_type_temp) ~ "Layers",
+           grepl("poultry_pullets", livestock_type_temp) ~ "Pullets",
+           grepl("poultry_turkeys", livestock_type_temp) ~ "Turkey",
+           grepl("swine", livestock_type_temp) ~ "Swine",
+           grepl("sheep", livestock_type_temp) ~ "Sheep",
+           TRUE ~ NA
+         )) %>% 
+  filter(!is.na(livestock_type)) %>% 
+  crossing(state = c("Minnesota", "Wisconsin")) %>% 
+  select(-c(kg_vs_1000kg_animal , livestock_type_temp, Year))
 
-enteric_out <-  bind_rows(enteric_dairy_cows,
-                          enteric_dairy_calves,
-                          enteric_beef_cows,
-                          enteric_beef_calves,
-                          enteric_feedlot_steer,
-                          enteric_feedlot_heifer,
-                          enteric_other) %>% 
+vs_cows <-  bind_rows(vs_dairy_cows,
+                          vs_dairy_calves,
+                          vs_beef_cows,
+                          vs_beef_calves,
+                          vs_feedlot_steer,
+                          vs_feedlot_heifer) %>% 
   group_by(state, year, livestock_type) %>% 
-  summarize(mt_ch4_head_yr = mean(mt_ch4_head_yr))
+  summarize(mt_vs_head_yr = mean(mt_vs_head_yr))
 
 
-enteric_out_meta <-
+vs_cows_meta <-
   tibble::tribble(
     ~"Column", ~"Class", ~"Description",
-    "state", class(enteric_out$state), "State",
-    "year", class(enteric_out$year), "Year",
-    "livestock_type", class(enteric_out$livestock_type), "Formatted livestock classification - matches USDA census labels",
-    "mt_ch4_head_yr", class(enteric_out$mt_ch4_head_yr), "Metric tons of CH4 emitted per animal per year"
+    "state", class(vs_cows$state), "State",
+    "year", class(vs_cows$year), "Year",
+    "livestock_type", class(vs_cows$livestock_type), "Formatted livestock classification - matches USDA census labels",
+    "mt_vs_head_yr", class(vs_cows$mt_vs_head_yr), "Metric tons of volatile solids produced per animal per year"
   )
 
-saveRDS(enteric_out, "./_agriculture/data/enteric_fermentation_emission_factors.rds")
-saveRDS(enteric_out_meta, "./_agriculture/data/enteric_fermentation_emission_factors_meta.rds")
+saveRDS(vs_cows, "./_agriculture/data/volatile_solids_cows.rds")
+saveRDS(vs_cows_meta, "./_agriculture/data/vs_fermentation_emission_factors_meta.rds")
+
+vs_other_meta <-
+  tibble::tribble(
+    ~"Column", ~"Class", ~"Description",
+    "state", class(vs_other$state), "State",
+    "year", class(vs_other$year), "Year",
+    "livestock_type", class(vs_other$livestock_type), "Formatted livestock classification - matches USDA census labels",
+    "mt_vs_1000kg_animal ", class(vs_other$mt_vs_1000kg_animal ), "Metric tons of volatile solids produced per 1000kg of animal per year"
+  )
+
+saveRDS(vs_other, "./_agriculture/data/volatile_solids_by_animal_weight.rds")
+saveRDS(vs_other_meta, "./_agriculture/data/volatile_solids_by_animal_weight.rds_meta.rds")
