7d8bc938b9aa7acef87af7ad66a18c844f76ce06
Liz Roten <liz.roten@metc.state.mn.us>
2024-07-25 12:03:57 -0500

    add comments; reorganize and use nei_inventory_year

--- _meta/data-raw/epa_nei.R
+++ _meta/data-raw/epa_nei.R
@@ -1,7 +1,13 @@
-# get data from the 2020 national emissions inventory
+# Fetch data from the 2020 national emissions inventory
 # NEI data prior to 2008 are unavailable
 # 
-# graphic model here: https://www.epa.gov/enviro/nei-model
+# This script does not save any outputs, but is intended to be 
+# called through source() within another modules and resulting datasets
+# (nei_county, nei_state, nei_state_multi_year, nei_county_multi_year) 
+# can filtered as needed. 
+
+
+# API graphic model here: https://www.epa.gov/enviro/nei-model
 source("R/_load_pkgs.R")
 library(httr2)
 cprg_county <- readRDS("_meta/data/cprg_county.RDS")
@@ -18,6 +24,7 @@ sectors <- req_base %>%
   httr2::resp_body_string(encoding = "UTF-8") %>%
   readr::read_delim(
     delim = ",",
+    col_types =  "c",
     show_col_types = FALSE
   )
 
@@ -41,6 +48,7 @@ fetch_nei <- function(year, state) {
     )
 }
 
+# pull count-level data for all available years
 multi_year <-
   bind_rows(
     purrr::map_dfr(
@@ -67,29 +75,36 @@ multi_year <-
     )
   )
 
+# county level aggregations ----------
 nei_county_multi_year <- multi_year %>%
-  mutate(GEOID = paste0(state_fips, county_fips)) %>%
-  filter(
-    GEOID %in% cprg_county$GEOID
-  ) %>%
+  mutate(GEOID = paste0(state_fips, county_fips),
+         cprg_area = ifelse(GEOID %in% cprg_county$GEOID, TRUE, FALSE),
+         nei_inventory_year = inventory_year) %>%
   group_by(
-    state_name, inventory_year,
+    state_name, nei_inventory_year,
     state_fips, county_fips, GEOID, pollutant_type, uom, emissions,
-    sector_code, pollutant_code, st_abbrv
+    sector_code, pollutant_code, st_abbrv, cprg_area
   ) %>%
   summarise(emissions = sum(emissions), .groups = "keep") %>%
   filter(pollutant_type == "GHG") %>%
   left_join(sectors, by = c("sector_code")) %>%
   mutate(emissions_grams = emissions %>%
-    units::as_units("ton") %>% # short tons/US tons
-    units::set_units("metric_ton") %>% # convert to grams
-    as.numeric()) %>%
- 
+           units::as_units("ton") %>% # short tons/US tons
+           units::set_units("metric_ton") %>% 
+           units::set_units("gram") %>% # convert to grams
+           as.numeric()) %>%
   rowwise()
 
+nei_county <- nei_county_multi_year %>% 
+  filter(nei_inventory_year == max(nei_inventory_year),
+         cprg_area == TRUE)
+
+
+# state level aggregations -------------
 nei_state_multi_year <- multi_year %>%
+  mutate(nei_inventory_year = inventory_year) %>% 
   group_by(
-    state_name, inventory_year,
+    state_name, nei_inventory_year,
     state_fips, pollutant_type, uom, emissions,
     sector_code, pollutant_code, st_abbrv
   ) %>%
@@ -97,43 +112,14 @@ nei_state_multi_year <- multi_year %>%
   filter(pollutant_type == "GHG") %>%
   left_join(sectors, by = c("sector_code")) %>%
   mutate(emissions_grams = emissions %>%
-    units::as_units("ton") %>% # short tons/US tons
-    units::set_units("metric_ton") %>% # convert to grams
-    as.numeric()) %>%
+           units::as_units("ton") %>% # short tons/US tons
+           units::set_units("metric_ton") %>% 
+           units::set_units("gram") %>% # convert to grams
+           as.numeric()) %>%
   rowwise()
 
 
-# state aggregation, most recent year
-nei_state <- bind_rows(
-  fetch_nei(2020, "Minnesota"),
-  fetch_nei(2020, "Wisconsin")
-) %>%
-  group_by(
-    state_name, inventory_year,
-    state_fips, pollutant_type, uom, emissions,
-    sector_code, pollutant_code, st_abbrv
-  ) %>%
-  summarise(emissions = sum(emissions), .groups = "keep") %>%
-  filter(pollutant_type == "GHG") %>%
-  left_join(sectors, by = c("sector_code")) %>%
-  mutate(emissions_grams = emissions %>%
-    units::as_units("ton") %>% # short tons/US tons
-    units::set_units("metric_ton") %>% # convert to grams
-    as.numeric())
-
-
-
-
-# combine MN and WI, counties
-# filter to only CPRG counties
-nei_county <- bind_rows(
-  fetch_nei(2020, "Minnesota"),
-  fetch_nei(2020, "Wisconsin")
-) %>%
-  mutate(GEOID = paste0(state_fips, county_fips)) %>%
-  filter(
-    GEOID %in% cprg_county$GEOID,
-    pollutant_type == "GHG"
-  ) %>%
-  left_join(sectors, by = c("sector_code")) %>%
-  rowwise()
+# state aggregation, most recent year only
+nei_state <- nei_state_multi_year %>%
+  ungroup() %>% 
+  filter(nei_inventory_year == max(nei_inventory_year))
