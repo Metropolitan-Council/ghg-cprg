39204b7ab09895bb93a53a89d48cae5ae57eb1c7
Liz Roten <liz.roten@metc.state.mn.us>
2024-08-20 11:39:18 -0500

    troubleshoot EQUATES processing

--- _transportation/data-raw/epa_equates.R
+++ _transportation/data-raw/epa_equates.R
@@ -56,6 +56,7 @@ if(!file.exists("_transportation/data-raw/epa/air_emissions_modeling/EQUATES/EQU
 }
 
 read_equates <- function(equates_path){
+  
   data.table::fread(equates_path,
                     skip = 19,
                     header = FALSE,
@@ -74,14 +75,35 @@ read_equates <- function(equates_path){
                                    "jun_pctred","jul_pctred","aug_pctred","sep_pctred",
                                    "oct_pctred","nov_pctred",'dec_pctred','comment')
   ) %>% 
-    dplyr::filter(region_cd %in% county_geography$geoid,
+    dplyr::filter(region_cd %in% c("27095", "27045", "27073", "27085", "27153", "27105", "27001", 
+                                  "27057", "27063", "27039", "27047", "27121", "27143", "27109", 
+                                  "27067", "27133", "27161", "27033", "27071", "27165", "27171", 
+                                  "27027", "27139", "27005", "27107", "27025", "27089", "27169", 
+                                  "27007", "27083", "27049", "27061", "27093", "27065", "27077", 
+                                  "27081", "27111", "27021", "27037", "27075", "27003", "27099", 
+                                  "27101", "27123", "27167", "27059", "27009", "27159", "27113", 
+                                  "27157", "27163", "27035", "27155", "27173", "27131", "27145", 
+                                  "27141", "27079", "27137", "27031", "27051", "27023", "27043", 
+                                  "27017", "27115", "27149", "27069", "27091", "27097", "27117", 
+                                  "27129", "27087", "27053", "27015", "27103", "27041", "27151", 
+                                  "27147", "27125", "27029", "27011", "27013", "27119", "27127", 
+                                  "27055", "27019", "27135", "55111", "55093", "55063", "55033", 
+                                  "55053", "55047", "55127", "55123", "55059", "55079", "55003", 
+                                  "55085", "55137", "55129", "55065", "55135", "55125", "55089", 
+                                  "55117", "55131", "55007", "55097", "55039", "55061", "55067", 
+                                  "55105", "55023", "55035", "55083", "55041", "55113", "55121", 
+                                  "55095", "55045", "55087", "55001", "55119", "55073", "55037", 
+                                  "55005", "55051", "55081", "55101", "55115", "55027", "55025", 
+                                  "55015", "55055", "55013", "55017", "55031", "55077", "55009", 
+                                  "55103", "55141", "55139", "55069", "55091", "55049", "55075", 
+                                  "55099", "55043", "55021", "55019", "55109", "55057", "55107", 
+                                  "55133", "55011", "55078", "55071", "55029"), 
                          poll %in% c("CH4", "N2O", "CO2",
                                      "PM10-PRI", "PM25-PRI")
     ) %>% 
-    dplyr::left_join(counties_light, 
-                     by = c("region_cd" = "geoid")) %>% 
-    dplyr::mutate(across(ends_with("value"), as.numeric),
-                  scc6 = str_sub(scc, 1, 6)) %>% 
+    dplyr::mutate(dplyr::across(tidyr::ends_with("value"), as.numeric),
+                  scc6 = stringr::str_sub(scc, 1, 6),
+                  equates_path = equates_path) %>% 
     dplyr::select(-tribal_code, -census_tract_cd,
                   -shape_id, -country_cd,
                   -date_updated, -data_set_id,
@@ -89,12 +111,12 @@ read_equates <- function(equates_path){
                   -ann_pct_red,
                   -projection_factor, -calc_method,
                   -control_measures, -control_ids,
-                  -starts_with(tolower(month.abb))
+                  -tidyr::starts_with(tolower(month.abb))
     )
 }
 
 library(furrr)
-plan(strategy = future::cluster)
+plan(strategy = future::multisession, workers = 12)
 
 equates <-
   furrr::future_map(c(
@@ -121,9 +143,21 @@ equates <-
 
 
 
-equates_cprg <- equates %>% 
+equates_cprg <- equates %>%
+  mutate(geoid = region_cd) %>% 
+  left_join(counties_light) %>% 
   filter(poll %in% c("CH4", "N2O", "CO2", "PM10-PRI", "PM25-PRI"),
-         emis_type %in% c("RPD", "RPV"))
+         emis_type %in% c("RPD", "RPV")) %>% 
+  tidyr::separate_wider_position(
+    scc, widths = c(
+      "mobile_source" = 2,
+      "fuel_type" = 2,
+      "vehicle_type" = 2,
+      "road_type" = 2,
+      "process_type" = 2
+    ),
+    cols_remove = FALSE
+  )
 
 saveRDS(equates, "_transportation/data-raw/epa/air_emissions_modeling/EQUATES/equates_mn_wi.RDS")
 saveRDS(equates_cprg, "_transportation/data-raw/epa/air_emissions_modeling/EQUATES/equates_cprg.RDS")
