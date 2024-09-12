f7d79e0c55c8824cb91dbea39da4e87ec2a82bfb
Zoey Yandell <zeyandell@gmail.com>
2024-09-12 11:29:27 -0500

    begin crf translation

--- cdp/3.1.3_gcom_inventory_2021.R
+++ cdp/3.1.3_gcom_inventory_2021.R
@@ -8,10 +8,66 @@ county_emissions <- readRDS(file.path(here::here(), "_meta/data/cprg_county_emis
 
 met_counties <- c("Anoka", "Carver", "Dakota", "Hennepin", "Ramsey", "Scott", "Washington")
 
-county_emissions <- county_emissions %>% 
+emissions_council_region <- county_emissions %>% 
   filter(geog_name %in% met_counties,
-         year == 2021)
-# once naming conventions/what's included is finalized: 
-# make sure activity data and factors are included
+         year == 2021) %>% 
+  group_by(sector, category, source) %>% 
+  mutate(
+    emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e)
+  ) %>% 
+  select(
+    sector,
+    category,
+    source,
+    data_source,
+    factor_source,
+    emissions_metric_tons_co2e
+  ) %>% 
+  distinct() %>% 
+  ungroup()
+ 
+
 # modify categories eurgh
+crf_emissions <- emissions_council_region %>% 
+  unite(category_source, c("category", "source")) %>% 
+  select(
+    category_source,
+    emissions_metric_tons_co2e
+  ) %>% 
+  pivot_wider(
+    names_from = category_source,
+    values_from = emissions_metric_tons_co2e
+  ) %>% 
+  mutate(
+    "Stationary energy > Residential buildings^^" = 
+      `Residential energy_Electricity` + `Residential energy_Natural gas`,
+    "Stationary energy > Commercial buildings & facilities^^" = 
+      `Commercial energy_Electricity` + `Commercial energy_Natural gas`,
+    "Stationary energy > Institutional buildings & facilities^^" = "IE",
+    "Stationary energy > Industrial buildings & facilities^^" =
+      `Industrial energy_Electricity` + `Industrial energy_Natural gas`,
+    "Stationary energy > Agriculture" = "IE",
+    "Stationary energy > Fugitive emissions^^" = "NO",
+    "Total Stationary Energy^" = `Total energy_Electricity` + `Total energy_Natural gas`,
+    "Transportation > On-road^^" = `Passenger vehicles_Light-duty vehicles` +
+      `Commercial vehicles_Medium-duty vehicles` + `Commercial vehicles_Heavy-duty vehicles`,
+    "Transportation > Rail^^" = "",
+    "Transportation > Waterborne navigation^^" = "",
+    "Transprotation > Aviation^^" = "",
+    "Transportation > Off-road^^" = "NO",
+    "Total Transport^" = `Passenger vehicles_Light-duty vehicles` +
+      `Commercial vehicles_Medium-duty vehicles` + `Commercial vehicles_Heavy-duty vehicles`,
+    "Waste > Solid waste disposal^^" = `Solid waste_Landfill`,
+    "Waste > Biological treatment^^" = `Solid waste_Organics`,
+    "Waste > Incineration and open burning^^" = `Solid waste_Waste to energy`,
+    "Waste > Wastewater^^" = `Wastewater_Wastewater`,
+    "Total Waste" = `Solid waste_Landfill` + `Solid waste_Organics` +
+      `Solid waste_Waste to energy` + `Wastewater_Wastewater`,
+    "IPPU > Industrial process" = "NE",
+    "IPPU > Product use" = "NE",
+    "Total IPPU" = "NE"
+  ) %>% 
+  select(
+    !contains("_")
+  )
 # sum to total region
--- cdp/4.7_waste.R
+++ cdp/4.7_waste.R
@@ -9,7 +9,38 @@
 # Total annual amount of food waste produced in the jurisdiction (tonnes/year),
 # Volume of wastewater produced within the jurisdiction boundary (megalitres/year),
 # Percentage of wastewater safely treated to at least secondary level (%)
+source("R/_load_pkgs.R")
 
 # Solid waste ----
-# (wait until names are updated)
+## read in score data
+mpca_score <- readRDS(file.path(here::here(), "_waste/data/mpca_score_2021.RDS"))
+met_counties <- c("Anoka", "Carver", "Dakota", "Hennepin", "Ramsey", "Scott", "Washington")
+
+cdp_waste <- mpca_score %>%
+  filter(County %in% met_counties) %>% 
+  select(
+    !`Management Category`
+  ) %>% 
+  group_by(`Method`) %>% 
+  dplyr::summarize(regional_total = sum(Tons)*0.90718474) %>% #converted to metric tons here
+  pivot_wider(
+    names_from = `Method`,
+    values_from = regional_total
+  ) %>% 
+  mutate(
+    total_waste = rowSums(across(Landfill:WTE)), 
+    percentage_wte = WTE/total_waste,
+    total_diverted = `MSW Compost` + Organics + Recycling,
+    percentage_diverted = total_diverted/total_waste,
+    percentage_recycled = Recycling/total_diverted,
+    percentage_reused = "Unknown",
+    percentage_separation_at_source = "Unknown",
+    food_waste = "Unknown",
+    wastewater_volume = "Unknown",
+    wastewater_treated = "Unknown",
+    year = 2021
+  ) %>% 
+  select(
+    !c("Landfill", "MSW Compost", "Onsite", "Organics", "Recycling", "WTE", "total_diverted")
+  )
 
