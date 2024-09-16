da25b6ef1bce3ab9f8d0e80172461e55fe00fb4b
Liz Roten <liz.roten@metc.state.mn.us>
2024-09-16 16:40:55 -0500

    replace with better warning

--- _transportation/data-raw/epa_air_emissions_modeling_activity.R
+++ _transportation/data-raw/epa_air_emissions_modeling_activity.R
@@ -3,81 +3,17 @@
 source("R/_load_pkgs.R")
 source("_meta/data-raw/county_geography.R")
 
-if (file.exists("_transportation/data-raw/epa/air_emissions_modeling/2022v1/2022hc_cb6_22m/inputs/onroad/VMT_2022v1_full_annual_20240226_monthly_18jun2024_nf_v4.csv")) {
-  # 2022
-  cli::cli_alert_warning("These files are hefty")
-  # emissions
-  download.file(
-    "https://gaftp.epa.gov/Air/emismod/2022/v1/2022emissions/onroad_emissions_SMOKE-MOVES_FF10_2022hc_17jul2024.zip",
-    "_transportation/data-raw/epa/air_emissions_modeling/2022v1/onroad_emissions_SMOKE-MOVES_FF10_2022hc_17jul2024.zip"
-  )
-
-  unzip("_transportation/data-raw/epa/air_emissions_modeling/2022v1/onroad_emissions_SMOKE-MOVES_FF10_2022hc_17jul2024.zip", exdir = "_transportation/data-raw/epa/air_emissions_modeling/2022v1/")
-
-  # activity
-  download.file(
-    "https://gaftp.epa.gov/Air/emismod/2022/v1/2022emissions/onroad_activity_SMOKE-MOVES_FF10_2022hc_17jul2024.zip",
-    "_transportation/data-raw/epa/air_emissions_modeling/2022v1/onroad_activity_SMOKE-MOVES_FF10_2022hc_17jul2024.zip"
-  )
-  unzip("_transportation/data-raw/epa/air_emissions_modeling/2022v1/onroad_activity_SMOKE-MOVES_FF10_2022hc_17jul2024.zip",
-    exdir = "_transportation/data-raw/epa/air_emissions_modeling/2022v1/"
-  )
-
-  # onroad comparisons
-  download.file("https://gaftp.epa.gov/Air/emismod/2022/v1/reports/mobile/onroad/2022v1%20onroad%20comparisons%2022-26-32-38%2010aug2024.xlsx",
-    destfile = "_transportation/data-raw/epa/air_emissions_modeling/2022v1/2022v1 onroad comparisons 22-26-32-38 10aug2024.xlsx"
-  )
-
-
-  # 2021
-  ## activity
-  download.file(
-    "https://gaftp.epa.gov/Air/emismod/2021/2021emissions/onroad_emissions_SMOKE-MOVES_FF10_2021hb_26mar2024.zip",
-    "_transportation/data-raw/epa/air_emissions_modeling/2021/onroad_activity_2021hb_24may2024.zip"
-  )
-
-  unzip("_transportation/data-raw/epa/air_emissions_modeling/2021/onroad_activity_2021hb_24may2024.zip",
-    exdir = "_transportation/data-raw/epa/air_emissions_modeling/2021/"
-  )
-
-  ## emissions
-  download.file("https://gaftp.epa.gov/Air/emismod/2021/2021emissions/onroad_emissions_SMOKE-MOVES_FF10_2021hb_26mar2024.zip",
-    destfile = "_transportation/data-raw/epa/air_emissions_modeling/2021/onroad_emissions_SMOKE-MOVES_FF10_2021hb_26mar2024.zip"
-  )
-
-  unzip("_transportation/data-raw/epa/air_emissions_modeling/2021/onroad_emissions_SMOKE-MOVES_FF10_2021hb_26mar2024.zip",
-    exdir = "_transportation/data-raw/epa/air_emissions_modeling/2021/"
-  )
-
-
-  download.file(
-    "https://gaftp.epa.gov/Air/emismod/2020/2020emissions/2020ha2_onroad_SMOKE-MOVES_emissions_FF10_22sep2023.zip",
-    "_transportation/data-raw/epa/air_emissions_modeling/2020/2020ha2_onroad_SMOKE-MOVES_emissions_FF10_22sep2023.zip"
-  )
+if(any(purrr::map(
+  c(("_transportation/data-raw/epa/air_emissions_modeling/2022v1/2022hc_cb6_22m/inputs/onroad/VMT_2022v1_full_annual_20240226_monthly_18jun2024_nf_v4.csv")),
+  file_exists) == FALSE)){
+  cli::cli_abort(c(
+    "Required datasets unavailable",
+    "*" = "Consult documentation for more information",
+    "*" = "{.file _transportation/data-raw/epa/README_epa_downloads.html}"
+  ))
 }
 
-
 vmt22 <- read_nei_vmt("_transportation/data-raw/epa/air_emissions_modeling/2022v1/2022hc_cb6_22m/inputs/onroad/VMT_2022v1_full_annual_20240226_monthly_18jun2024_nf_v4.csv")
-
-mod22 <- fread("_transportation/data-raw/epa/air_emissions_modeling/2022v1/2022hc_cb6_22m/inputs/onroad/2022hc_onroad_SMOKE_MOVES_MOVES4_forAQ_27jun2024_v0.csv",
-  keepLeadingZeros = TRUE
-) %>%
-  .[region_cd %in% county_geography$geoid, ] %>%
-  select(
-    -starts_with(tolower(month.abb)),
-    -tribal_code, -census_tract_cd, -shape_id
-  ) %>%
-  mutate(
-    scc = as.character(scc),
-    scc6 = str_sub(scc, 1, 6),
-    calc_year = as.character(calc_year)
-  ) %>%
-  filter(poll %in% c("CH4", "CO2", "N2O", "CO")) %>%
-  left_join(counties_light,
-    by = c("region_cd" = "geoid")
-  ) %>%
-  left_join(scc6_desc)
-
 
 vmt22 %>%
   group_by(scc6, region_cd, cprg_area, county_name) %>%
