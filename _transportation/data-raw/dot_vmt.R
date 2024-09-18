3d01cc428d3bd136401b377b8d0b06a4417ee8c5
Liz Roten <liz.roten@metc.state.mn.us>
2024-09-18 17:31:03 -0500

    additional VMT related commits

--- _transportation/data-raw/dot_vmt.R
+++ _transportation/data-raw/dot_vmt.R
@@ -1,5 +1,5 @@
 source("R/_load_pkgs.R")
-
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 # load VMT from both MnDOT and WisDOT
 mndot_vmt <- readRDS(file.path(here::here(), "_transportation/data-raw/mndot/mndot_vmt_county.RDS")) %>%
   mutate(data_source = "MnDOT")
@@ -10,7 +10,20 @@ dot_vmt <- bind_rows(
   mndot_vmt,
   wisdot_vmt
 ) %>%
-  ungroup()
+  ungroup() %>%
+  left_join(cprg_county,
+    by = c("county" = "NAME")
+  ) %>%
+  select(
+    year,
+    GEOID,
+    county,
+    cprg_area,
+    daily_vmt,
+    annual_vmt,
+    centerline_miles,
+    data_source
+  )
 
 names(dot_vmt)
 
@@ -18,6 +31,7 @@ dot_vmt_meta <-
   tibble::tribble(
     ~"Column", ~"Class", ~"Description",
     "year", class(dot_vmt$year), "VMT estimation year",
+    "GEOID", class(dot_vmt$GEOID), "5-digit county ID",
     "county", class(dot_vmt$county), "County name",
     "cprg_area", class(dot_vmt$cprg_area), "Whether county is included in the CPRG area",
     "daily_vmt", class(dot_vmt$daily_vmt), "Vehicle miles traveled on an average day",
