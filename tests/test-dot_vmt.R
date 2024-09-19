3da80d24b9071f0296056d5637dc9dac1ceb8e34
Liz Roten <liz.roten@metc.state.mn.us>
2024-09-19 12:13:59 -0500

    update column names
    all tests pass across all sectors

--- tests/test-dot_vmt.R
+++ tests/test-dot_vmt.R
@@ -6,16 +6,16 @@ testthat::test_that("state DOT VMT", {
   # check match with PDFs
   dot_vmt %>%
     filter(
-      county == "Pierce",
-      year >= 2018
+      county_name == "Pierce",
+      vmt_year >= 2018
     ) %>%
-    arrange(desc(year)) %>%
+    arrange(desc(vmt_year)) %>%
     magrittr::extract2("daily_vmt") %>%
     testthat::expect_equal(c(1067675, 1074726, 949687, 1099233, 1068300))
 
   # hennepin has highest annual
   dot_vmt %>%
     filter(annual_vmt == max(annual_vmt)) %>%
-    magrittr::extract2("county") %>%
+    magrittr::extract2("county_name") %>%
     testthat::expect_equal("Hennepin")
 })
