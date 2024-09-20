a8bf346c4c11746529c5a807659dc959f293b27f
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-09-20 10:17:19 -0500

    rename and remove EPA emissions data

--- _transportation/data-raw/transportation_county_proportion_compare.R
+++ _transportation/data-raw/transportation_county_proportion_compare.R
@@ -6,49 +6,33 @@ source("R/_load_pkgs.R")
 # read in pre-processed proportions
 dot_vmt_county_proportions <- readRDS("_transportation/data/dot_vmt_county_proportions.RDS")
 cprg_county_proportions <- readRDS("_meta/data/cprg_county_proportions.RDS")
-epa_nei_county_proportions <- readRDS("_transportation/data/epa_nei_county_proportions.RDS")
-
-
 
 
 county_vmt_pop_emissions <- cprg_county_proportions %>%
   left_join(dot_vmt_county_proportions,
-    by = c("GEOID", "year",
-      "name" = "county",
-      "STATE" = "state"
+    by = c("geoid", "population_year" = "vmt_year",
+      "county_name",
+      "state_name" = "state"
     )
-  ) %>%
-  left_join(
-    epa_nei_county_proportions %>%
-      mutate(nei_inventory_year = as.character(nei_inventory_year)),
-    by = c(
-      "year" = "nei_inventory_year",
-      "GEOID",
-      "name" = "county_name",
-      "STATE" = "state_name"
-    )
-  ) %>%
+  )  %>%
   mutate(
     vmt_per_capita = annual_vmt / county_population,
-    emissions_per_capita = county_emissions_metric_tons_co2e / county_population
   ) %>%
-  filter(year >= 2005)
+  filter(population_year  >= 2005)
 
 
 county_prop_long <- county_vmt_pop_emissions %>%
   select(
-    GEOID, name, year, county_proportion_annual_vmt,
-    county_proportion_of_state_pop,
-    county_proportion_emissions
+    geoid, county_name, population_year, county_proportion_annual_vmt,
+    county_proportion_of_state_pop
   ) %>%
-  pivot_longer(4:6,
+  pivot_longer(4:5,
     names_to = "proportion_base",
     values_to = "value"
   ) %>%
   mutate(proportion_base = case_when(
     proportion_base == "county_proportion_annual_vmt" ~ "VMT",
-    proportion_base == "county_proportion_of_state_pop" ~ "population",
-    proportion_base == "county_proportion_emissions" ~ "emissions"
+    proportion_base == "county_proportion_of_state_pop" ~ "population"
   ))
 
 
@@ -56,23 +40,12 @@ county_prop_long <- county_vmt_pop_emissions %>%
 
 
 county_prop_long %>%
-  group_by(name) %>%
+  group_by(county_name) %>%
   plot_ly(
     type = "scatter",
     mode = "lines+markers",
-    x = ~year,
+    x = ~population_year,
     y = ~value,
     symbol = ~proportion_base,
-    color = ~name
-  )
-
-county_vmt_pop_emissions %>%
-  group_by(name) %>%
-  filter(!is.na(emissions_per_capita)) %>%
-  plot_ly(
-    mode = "lines+markers",
-    type = "scatter",
-    x = ~year,
-    y = ~emissions_per_capita,
-    color = ~name
+    color = ~county_name
   )
