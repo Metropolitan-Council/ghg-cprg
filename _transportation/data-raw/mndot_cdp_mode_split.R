b0217024aa456b4587263409e18d720e949eccc6
Liz Roten <liz.roten@metc.state.mn.us>
2024-09-26 13:51:52 -0500

    use MnDOT stations to estimate freight mode split
    only medium and heavy duty on road

---
+++ _transportation/data-raw/mndot_cdp_mode_split.R
@@ -0,0 +1,14 @@
+source("R/_load_pkgs.R")
+# takes traffic ratios used for calibrating StreetLight and finds the average 
+# proportional breakdown for passenger, medium, and heavy duty traffic. 
+# 
+## read in traffic ratios
+traffic_ratios <- readRDS(paste0("_transportation/data-raw/mndot/most_recent_yearly_volume_percentage_by_class.RDS"))
+
+cdp_freight <- traffic_ratios %>% 
+  summarize(
+    `Medium Goods Vehicles` = mean(medium_duty),
+    `Heavy Goods Vehicles` =  mean(heavy_duty)
+  )
+
+cdp_freight %>% kable(format = "markdown")
