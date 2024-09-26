1cfadd379f428f88777c48b295aa03c892d950ec
Liz Roten <liz.roten@metc.state.mn.us>
2024-09-26 16:30:49 -0500

    process US GHG Emissions and Sinks (US GHGI)

--- _meta/data-raw/epa_us_ghgi.R
+++ _meta/data-raw/epa_us_ghgi.R
@@ -1,135 +1,160 @@
-# uses AR5 GWP values
-# https://www.epa.gov/ghgemissions/inventory-us-greenhouse-gas-emissions-and-sinks-1990-2022
-# already converted to CO2e
-# IPCC categories -----
+# 
+# Inventory of U.S. Greenhouse Gas Emissions and Sinks by State: 1990-2021
+# https://www.epa.gov/ghgemissions/methodology-report-inventory-us-greenhouse-gas-emissions-and-sinks-state-1990-2021
 source("R/_load_pkgs.R")
-clean_ipcc <- function(state, path) {
-  read_xlsx(path,
-    skip = 2
-  ) %>%
-    mutate(sector_group = c(
-      "remove",
-      rep("Energy", 10),
-      "remove",
-      rep("Industrial Processes and Product Use", 26),
-      "remove",
-      rep("Agriculture", 7),
-      "remove",
-      rep("Waste", 4),
-      "remove",
-      "remove",
-      rep("Land Use, Land-Use Change, and Forestry", 5),
-      "remove",
-      rep("remove", 10)
-    )) %>%
-    filter(sector_group != "remove") %>%
-    select(-...3, -...19) %>%
-    pivot_longer(
-      cols = 2:33,
-      names_to = "inventory_year",
-      values_to = "emissions"
-    ) %>%
-    mutate(
-      emissions_metric_tons_co2e = emissions * 1000000,
-      state = state
-    )
+source("R/global_warming_potential.R")
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
+
+if(!file.exists("_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx")){
+  download.file("https://www.epa.gov/system/files/other-files/2023-10/allstateghgdatapy2023readme_100323_0.zip",
+                destfile = "_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0.zip")
+  
+  unzip("_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0.zip",
+        exdir = "_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0")
 }
 
-state_ipcc <-
-  bind_rows(
-    clean_ipcc(
-      path = "_meta/data-raw/epa/state_ghg/State-Level-GHG-data/Minnesota.xlsx",
-      state = "Minnesota"
-    ),
-    clean_ipcc(
-      state = "Wisconsin",
-      path = "_meta/data-raw/epa/state_ghg/State-Level-GHG-data/Wisconsin.xlsx"
-    )
-  )
+# ipcc sectors -----
+ipcc_sectors <- readxl::read_xlsx("_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx",
+                                  sheet = 2
+) %>%
+  clean_names()
 
-
-state_ipcc %>%
-  group_by(sector_group, inventory_year, state) %>%
-  summarize(emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e)) %>%
-  ungroup() %>%
-  plot_ly(
-    y = ~inventory_year,
-    x = ~emissions_metric_tons_co2e,
-    color = ~sector_group
-  )
-
-state_ipcc %>%
-  select(Sector, sector_group) %>%
-  unique() %>%
-  View()
-
-state_ipcc %>%
-  mutate(transportation_related = case_when(
-    Sector %in% c(
-      "Fossil Fuel Combustion",
-      "Mobile Combustion"
-    )
-  ))
-
-# Economic sectors -----
-
-clean_economic <- function(state, path) {
-  read_xlsx(path,
-    skip = 2
+ipcc_sector_category <- ipcc_sectors %>%
+  select(
+    ipcc_sector = sector, subsector, category, subcategory1, subcategory2,
+    subcategory3, subcategory4, rownumber
   ) %>%
-    mutate(sector_group = c(
-      "remove",
-      rep("Transportation", 4),
-      "remove",
-      rep("Electric Power Industry", 5),
-      "remove",
-      rep("Industry", 36),
-      "remove",
-      rep("Agriculture", 10),
-      "remove",
-      rep("Commercial", 8),
-      "remove",
-      rep("Residential", 3),
-      "remove",
-      "LULUCF Sector Net Total",
-      rep("remove", 6)
-    )) %>%
-    filter(sector_group != "remove") %>%
-    select(-...3, -...19) %>%
-    pivot_longer(
-      cols = 2:33,
-      names_to = "inventory_year",
-      values_to = "emissions"
-    ) %>%
-    mutate(
-      emissions_metric_tons_co2e = emissions * 1000000,
-      state = state
-    )
-}
+  unique()
 
-
-state_economic <- bind_rows(
-  clean_economic(
-    path = "_meta/data-raw/epa/state_ghg/State-Level-GHG-data/Minnesota-Economic.xlsx",
-    state = "Minnesota"
-  ),
-  clean_economic(
-    state = "Wisconsin",
-    path = "_meta/data-raw/epa/state_ghg/State-Level-GHG-data/Wisconsin-Economic.xlsx"
+ipcc <- ipcc_sectors %>%
+  filter(
+    state %in% c("MN", "WI")
+  ) %>%
+  pivot_longer(starts_with("y"),
+               names_to = "inventory_year",
+               values_to = "emission_grams"
+  ) %>%
+  mutate(
+    inventory_year = str_remove(inventory_year, "y"),
+    # reported in millions of metric tons
+    # so convert to metric tons then to grams
+    emission_grams = as.numeric(emission_grams) * 1000000 %>%
+      units::as_units("metric_ton") %>%
+      units::set_units("gram") %>%
+      as.numeric()
   )
+
+
+ipcc_transportation <-
+  ipcc %>%
+  filter(
+    category == "Transportation",
+    # these are things like asphalt, paints,
+    # natural gas is used to move fuel through pipelines and in LNG vehicles
+    # subsector != "Non-Energy Uses of Fossil Fuels",
+    # inventory_year == 2021,
+    !is.na(subsector)
+    # state == "MN"
+  ) %>%
+  select(-subcategory2, -subcategory3, -subcategory4, -rownumber) %>%
+  pivot_wider(
+    names_from = "ghg",
+    values_from = "emission_grams",
+    values_fill = 0
+  ) %>%
+  clean_names() %>%
+  group_by(sector, subsector, category, fuel, subcategory1, inventory_year, state) %>%
+  summarize(
+    co2 = sum(co2),
+    ch4 = sum(ch4),
+    n2o = sum(n2o),
+    .groups = "keep"
+  ) %>%
+  group_by(sector, subsector, category, subcategory1, inventory_year, state) %>%
+  summarize(
+    co2_co2_equivalent =
+      sum(co2, (ch4 * gwp$ch4), (n2o * gwp$n2o)),
+    emissions_metric_tons_co2e = co2_co2_equivalent / 1000000,
+    million_metric_tons_co2e = emissions_metric_tons_co2e / 1000000
+  )
+
+# for cross-reference
+# MN 2021 as shown in GHG Data Explorer is the sum of CO2 for fossil fuel combustion
+# fuel types Petroleum and Natural Gas
+
+unique(ipcc_transportation$fuel)
+
+ipcc %>%
+  group_by(
+    inventory_year, state, sector, category, subsector, subcategory1, subcategory2,
+    subcategory3, subcategory4, fuel
+  ) %>%
+  summarize(emissions_metric_tons_co2e = sum(ghg_equiv))
+
+
+# economic sectors -----
+econ_sectors <- readxl::read_xlsx(
+  "_meta/data-raw/epa/us_ghgi/allstateghgdatapy2023readme_100323_0/AllStateGHGDataPY2023_100323.xlsx",
+  sheet = 3
+) %>%
+  clean_names()
+
+econ <- econ_sectors %>%
+  filter(state %in% c("MN", "WI")) %>%
+  select(-subcategory4, -rownumber) %>%
+  pivot_longer(starts_with("y"),
+               names_to = "inventory_year",
+               values_to = "emission_grams"
+  ) %>%
+  mutate(
+    inventory_year = str_remove(inventory_year, "y"),
+    # reported in millions of metric tons
+    # so convert to metric tons then to grams
+    emission_grams = as.numeric(emission_grams) * 1000000 %>%
+      units::as_units("metric_ton") %>%
+      units::set_units("gram") %>%
+      as.numeric()
+  ) %>%
+  unique()
+
+
+
+econ_transportation <- econ %>%
+  filter(econ_sector == "Transportation") %>%
+  group_by(
+    econ_sector, econ_source, subsector, subcategory1,
+    state, ghg, inventory_year
+  ) %>%
+  summarize(
+    emission_grams = sum(emission_grams),
+    .groups = "keep"
+  ) %>%
+  pivot_wider(
+    names_from = "ghg",
+    values_from = "emission_grams",
+    values_fill = 0
+  ) %>%
+  clean_names()
+mutate(
+  co2_co2_equivalent =
+    sum(co2, (ch4 * gwp$ch4), (n2o * gwp$n2o)),
+  emissions_metric_tons_co2e = co2_co2_equivalent / 1000000
 )
 
 
-state_economic %>%
-  filter(
-    sector_group == "Transportation",
-    `Sector/Source` == "CO2 from Fossil Fuel Combustion",
-    inventory_year > 2004
+# compare line numbers, sectors -----
+
+ipcc_econ_mapping <- econ_sectors %>%
+  select(
+    econ_sector, subsector,
+    subcategory1, subcategory2,
+    subcategory3, subcategory4,
+    rownumber
   ) %>%
-  plot_ly(
-    type = "scatter",
-    mode = "markers+lines",
-    x = ~inventory_year,
-    y = ~emissions_metric_tons_co2e,
-    color = ~state
-  )
+  unique() %>%
+  left_join(ipcc_sector_category,
+            by = "rownumber",
+            suffix = c(".econ", ".ipcc")
+  ) %>%
+  select(-rownumber) %>%
+  unique()
