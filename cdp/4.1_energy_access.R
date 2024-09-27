a8ff8f0bffd2747ef3aa97e84e3d142eef2495d9
Zoey Yandell <zeyandell@gmail.com>
2024-09-27 12:54:47 -0500

    calculate cdp energy use

---
+++ cdp/4.1_energy_access.R
@@ -0,0 +1,19 @@
+
+source("R/_load_pkgs.R")
+minnesota_elecUtils_ActivityAndEmissions <- readRDS(file.path(here::here(),
+                                                              "_energy/data/minnesota_elecUtils_ActivityAndEmissions.RDS"))
+
+countyActivity <- minnesota_elecUtils_ActivityAndEmissions %>%
+  group_by(county) %>%
+  summarize(
+    mWh_delivered_county = sum(mWh_delivered, na.rm = TRUE)
+  ) %>%
+  ungroup()%>% 
+  summarize(mWh_delivered_region = sum(mWh_delivered_county))
+
+mwh_total <- countyActivity$mWh_delivered_region
+
+# Renewable energy can be defined as energy derived from sunlight, wind, 
+# geothermal processes, biomass, and water (including hydropower, oceanic/tidal energy)
+renewable_gridshare <- (0.9 + 34.6 + 0.0 + 0.8 + 4.4) /100
+mwh_renewable <- mwh_total * renewable_gridshare
