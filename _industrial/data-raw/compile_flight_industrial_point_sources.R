741baa361ff9d8c7ab6bc2930ef6683aa12d2bb7
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-09-27 12:19:39 -0500

    output flight ctu data

--- _industrial/data-raw/compile_flight_industrial_point_sources.R
+++ _industrial/data-raw/compile_flight_industrial_point_sources.R
@@ -32,18 +32,68 @@ cprg_flight %>% distinct(subparts)
 
 cprg_flight <- cprg_flight %>% 
 ## DD is electric transmission and would be filtered if it were included here (it is not)
-  filter(!grepl("D", subparts),
-         !grepl("HH", subparts))
+  mutate(doublecount = if_else((grepl("D", subparts) | grepl("HH", subparts)),
+                               "Yes", "No"))
 
-county_summary <- cprg_flight %>% 
-  group_by(reporting_year, county_name) %>% 
-  summarize(co2e = sum(ghg_quantity_metric_tons_co2e))
+cprg_flight_out <- cprg_flight %>% 
+  select(inventory_year = reporting_year,
+         facility_name,
+         latitude,
+         longitude,
+         city_name,
+         county_name,
+         state,
+         value_emissions = ghg_quantity_metric_tons_co2e,
+         doublecount) %>% 
+  mutate(units_emissions = "Metric tons CO2e",
+         sector = "Industrial",
+         data_source = "EPA FLIGHT",
+         factor_source = "EPA FLIGHT (no direct gas data provied)")
+### convert subparts to category/source at later date
 
-ggplot(county_summary, aes(x = reporting_year, y = co2e, col = county_name)) + 
+
+cprg_flight_meta <-
+  tibble::tribble(
+    ~"Column", ~"Class", ~"Description",
+    "inventory_year", class(cprg_flight_out$inventory_year), "Year of survey",
+    "city_name", class(cprg_flight_out$city_name), "City name",
+    "county_name", class(cprg_flight_out$city_name), "County name",
+    "state", class(cprg_flight_out$state), "State name",
+    "facility_name", class(cprg_flight_out$facility_name), "Facility name",
+    "latitude", class(cprg_flight_out$latitude), "Latitude of industrial source",
+    "longitude", class(cprg_flight_out$longitude), "Longitude of industrial source",
+    "sector", class(cprg_flight_out$sector), "Emissions sector. One of Transportation, Energy, Waste, Nature, Agriculture",
+    # "category", class(cprg_flight_out$category), "Category of emissions within given sector",
+    # "source", class(cprg_flight_out$source), "Source of emissions. Most detailed sub-category in this table",
+    "data_source", class(cprg_flight_out$data_source), "Activity data source",
+    "factor_source", class(cprg_flight_out$factor_source), "Emissions factor data source",
+    "value_emissions", class(cprg_flight_out$value_emissions), "Numerical value of emissions",
+    "units_emissions", class(cprg_flight_out$units_emissions), "Units and gas type of emissions",
+    "doublecount", class(cprg_flight_out$doublecount), "Is this emission counted in another sector?"
+  )
+
+saveRDS(cprg_flight_out, "./_agriculture/data/flight_industrial_point_sources_ctu.rds")
+saveRDS(cprg_flight_meta, "./_agriculture/data/flight_industrial_point_sources_ctu_meta.rds")
+
+### summarize to the yearly county level while IDing doublecounts
+flight_county_summary <- cprg_flight %>% 
+  group_by(reporting_year, county_name, doublecount) %>% 
+  summarize(co2e = sum(ghg_quantity_metric_tons_co2e)) %>% 
+  left_join(., cprg_county %>% 
+              select(county_name, geoid, state_name) %>%  
+              st_drop_geometry())
+
+cprg_flight_out <- 
+
+
+ggplot(county_summary %>% 
+         filter(doublecount == "No"), 
+       aes(x = reporting_year, y = co2e, col = county_name)) + 
   geom_line()
 
 leaflet(cprg_flight %>%
-  filter(reporting_year == 2021)) %>% 
+  filter(reporting_year == 2021,
+         doublecount == "No")) %>% 
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(
     lng = ~longitude,  # Replace 'longitude' with your actual longitude column name
