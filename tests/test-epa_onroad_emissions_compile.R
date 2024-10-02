0622530cad89c9cc809e8296beb2fa512f6178a8
Liz Roten <liz.roten@metc.state.mn.us>
2024-10-02 16:10:03 -0500

    update tests

--- tests/test-epa_onroad_emissions_compile.R
+++ tests/test-epa_onroad_emissions_compile.R
@@ -1,32 +1,37 @@
 testthat::test_that("Compiled onroad emissions", {
   epa_onroad_emissions_compile <- readRDS(file.path(here::here(), "_transportation/data/epa_onroad_emissions_compile.RDS"))
 
-  # test for values in factor/character columns  
-  
+  # test for values in factor/character columns
+
   testthat::expect_equal(
     unique(epa_onroad_emissions_compile$data_source),
-    c("EQUATES", "National Emissions Inventory", "Air Emissions Modeling"))
-  
+    c("EQUATES", "National Emissions Inventory", "Air Emissions Modeling")
+  )
+
   testthat::expect_equal(
     unique(epa_onroad_emissions_compile$pollutant),
-    c("emissions_metric_tons_co2e", "co2", "ch4", "n2o", "co", "no", 
-      "nox", "so2", "nh3", "pm10_pri", "pm25_pri")
+    c(
+      "emissions_metric_tons_co2e", "co2", "ch4", "n2o", "co", "no",
+      "nox", "so2", "nh3", "pm10_pri", "pm25_pri"
+    )
   )
-  
+
   testthat::expect_equal(
     unique(epa_onroad_emissions_compile$vehicle_type),
-    c("Motorcycles", "Passenger cars", "Passenger trucks", "Light commercial trucks", 
-      "Intercity buses", "Transit buses", "School buses", "Refuse trucks", 
-      "Single unit short-haul trucks", "Single unit long-haul trucks", 
-      "Motor homes", "Combination short-haul trucks", "Combination long-haul trucks", 
-      "Trucks and buses", "Gas stations")
+    c(
+      "Motorcycles", "Passenger cars", "Passenger trucks", "Light commercial trucks",
+      "Intercity buses", "Transit buses", "School buses", "Refuse trucks",
+      "Single unit short-haul trucks", "Single unit long-haul trucks",
+      "Motor homes", "Combination short-haul trucks", "Combination long-haul trucks",
+      "Trucks and buses", "Gas stations"
+    )
   )
-  
+
   testthat::expect_equal(
-    unique(epa_onroad_emissions_compile$fuel_type),
-    c("Gasoline", "Diesel", "Compressed natural gas (CNG)", "Ethanol (E-85)", 
-      "Electric")
+    sort(unique(epa_onroad_emissions_compile$fuel_type)),
+    c(
+      "Compressed natural gas (CNG)", "Diesel", "Electric", "Ethanol (E-85)",
+      "Gasoline"
+    )
   )
-  
-  
 })
