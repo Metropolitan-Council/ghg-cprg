899e7f0688e19300c78f121da8dc534f712d2880
Liz Roten <liz.roten@metc.state.mn.us>
2024-10-03 11:10:00 -0500

    add force_download option and implement
    doesn't re-download by default if the file already exists! Referenced in https://github.com/Metropolitan-Council/councilR/issues/82

--- R/download_read_table.R
+++ R/download_read_table.R
@@ -2,6 +2,8 @@
 #'
 #' @param url character, file location URL. Must end with either ".xlsx" or ".xls"
 #' @param exdir character, directory location where to save downloaded document
+#' @param force_download logical, whether to force a fresh download, regardless 
+#'   of whether the file exists already. Default value is `FALSE`.
 #' @param ... Additional arguments passed to readxl::read_excel() or readr::read_csv
 #'
 #' @return tibble
@@ -15,18 +17,22 @@
 #'
 download_read_table <- function(url,
                                 exdir,
+                                force_download = FALSE,
                                 ...) {
   # split URL to get file name
   url_split <- strsplit(url, split = "/")
   file_name <- tail(url_split[[1]], n = 1)
-
-
-  # download file and save in exdir
-  download.file(url,
-    destfile = file.path(exdir, file_name),
-    mode = "wb"
-  )
-
+  
+  # if the downloaded file does not already OR
+  # we are forcing a fresh download
+  # download the file and save in exdir
+  if(!file.exists(file.path(exdir, file_name)) | force_download == TRUE){
+    download.file(url,
+                  destfile = file.path(exdir, file_name),
+                  mode = "wb"
+    )
+  }
+  
   # read and return file
   if (fs::path_ext(file_name) == "csv") {
     readr::read_csv(
