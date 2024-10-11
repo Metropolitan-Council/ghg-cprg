76091722d269a0449dcd38c0cea3afecf5b267d0
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-10-10 19:59:58 -0500

    ghg factor formatting

--- _industrial/data-raw/compile_industrial_point_source_by_fuel_type.R
+++ _industrial/data-raw/compile_industrial_point_source_by_fuel_type.R
@@ -5,7 +5,8 @@ cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 cprg_ctu <- readRDS("_meta/data/cprg_ctu.RDS") %>% 
   mutate(city_name = str_to_title(gsub("ST.", "Saint", ctu_name)))
 industrial_hub <- readRDS("_meta/data/epa_ghg_factor_hub.RDS") %>% 
-  extract2("industrial_combustion")
+  extract2("industrial_combustion") %>% 
+  clean_names()
 
 
 ### download emissions by fuel type data: https://www.epa.gov/ghgreporting/data-sets
@@ -42,9 +43,9 @@ ind_fuel_data <-  read_excel(file.path(here::here(), "_industrial/data-raw/emiss
 #unique fuel types and matching to ghg factor hub
 
 unique(ind_fuel_data$specific_fuel_type)
-unique(industrial_hub$`Fuel type`)
+unique(industrial_hub$fuel_type       )
 
-ind_fuel_data %>% filter(!specific_fuel_type %in% industrial_hub$`Fuel type`) %>% 
+ind_fuel_data %>% filter(!specific_fuel_type %in% industrial_hub$fuel_type       ) %>% 
   distinct(specific_fuel_type)
 
 #only 5, manually changing them to match hub
@@ -60,7 +61,37 @@ ind_fuel_data <- ind_fuel_data %>%
 
 ### create conversion factors to back-translate CO2e to activity data
 
+industrial_hub <- industrial_hub %>% 
+  # remove mmBtu - all fuels also have volume/weight
+  filter(per_unit != "mmBtu",
+         emission != "mmBtu") %>% 
+  #convert to metric tons of gas
+  mutate(mt_gas = case_when(
+    grepl("CO2", emission) ~ value * units::as_units("kilogram") %>%
+      units::set_units("metric_ton"),
+    TRUE ~ value *  units::as_units("gram") %>%
+      units::set_units("metric_ton")
+  )) %>% 
+  # convert to CO2e using IPCC 4 assessment values (consistent with EPA dataset not ours)
+  mutate(mt_co2e = case_when(
+    grepl("CH4", emission) ~ mt_gas * 25,
+    grepl("N2O", emission) ~ mt_gas * 298,
+    TRUE ~ mt_gas
+  )) %>% 
+  # units per mt of co2e
+  mutate(unit_co2e = 1/mt_co2e)
 
+co2e_to_unit <- industrial_hub %>% 
+  filter(fuel_type %in% ind_fuel_data$specific_fuel_type,
+         !grepl("CO2",emission)) %>% 
+  pivot_wider(id_cols = c(fuel_type, per_unit),
+              names_from = emission,
+              values_from = unit_co2e)
+
+###
+
+ind_fuel_units <- left_join(ind_fuel_data,
+                            industrial_hub
 
 ### are all fuel data facilities present in unit data?
 
