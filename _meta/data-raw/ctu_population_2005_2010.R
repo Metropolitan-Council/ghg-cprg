e78eabe635abb149bf9f39b8f37b27035cb0e6e3
Zoey Yandell <zeyandell@gmail.com>
2024-10-16 17:15:43 -0500

    format and proportions

--- _meta/data-raw/ctu_population_2005_2010.R
+++ _meta/data-raw/ctu_population_2005_2010.R
@@ -13,8 +13,25 @@ ctu_list[[1]] <- slice(ctu_estimates, 5:26) %>%
 # this section on hold until we can hook it up to FRED
 
 # 2011-2019
-ctu_estimates_2011 <- readxl::read_xlsx("_meta/data-raw/IntercensalEstimates.xlsx")
-# what more do i do. she's already beautiful.
-# ik ik i have to figure out the COCTU stuff
-# and then let's assign some proportions on a county level
-# and fix naming conventions to align with what we had decided
+ctu_estimates_2011 <- readxl::read_xlsx("_meta/data-raw/IntercensalEstimates.xlsx") %>% 
+  mutate(
+    geoid = paste0("27", COUNTY_CODE)
+    ) %>% 
+  select(
+    countyfp = COUNTY_CODE,
+    geoid,
+    ctuid = CTU_CODE,
+    year = EST_YEAR,
+    ctu_population = POPTOTAL_EST
+  ) %>%
+  group_by(
+    countyfp, geoid, year
+  ) %>% 
+  mutate(
+    county_population = sum(ctu_population)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    ctu_proportion_of_county_pop = ctu_population/county_population
+  )
+
