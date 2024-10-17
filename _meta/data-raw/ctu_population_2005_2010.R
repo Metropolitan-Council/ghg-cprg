09b5892e65349fe0b4f7d3947c1f0d7b647392f7
Zoey Yandell <zeyandell@gmail.com>
2024-10-17 10:53:52 -0500

    import from FRED

--- _meta/data-raw/ctu_population_2005_2010.R
+++ _meta/data-raw/ctu_population_2005_2010.R
@@ -1,16 +1,18 @@
 source("R/_load_pkgs.R")
 
-ctu_estimates <- readxl::read_xls("_meta/data-raw/2009PopulationEstimates.xls") %>% 
-  select(
-    1:3
-  )
-ctu_list <- vector(mode = "list", length = 8)
-
-ctu_list[[1]] <- slice(ctu_estimates, 5:26) %>%
-  mutate(county = "Anoka")
+# ctu_estimates <- readxl::read_xls("_meta/data-raw/2009PopulationEstimates.xls") %>% 
+#   select(
+#     1:3
+#   )
+# ctu_list <- vector(mode = "list", length = 8)
+# 
+# ctu_list[[1]] <- slice(ctu_estimates, 5:26) %>%
+#   mutate(county = "Anoka")
 # see clean_tabula_tables
 # is this the best way to do it? maybe not but it's how i'm doing it
 # this section on hold until we can hook it up to FRED
+
+ctu_estimates_2005 <- councilR::import_from_FRED("POPULATION")
 
 # 2011-2019
 ctu_estimates_2011 <- readxl::read_xlsx("_meta/data-raw/IntercensalEstimates.xlsx") %>% 
