04c4febb152920366f278b30071f058733e6fd1b
Zoey Yandell <zeyandell@gmail.com>
2024-10-18 15:26:30 -0500

    2005-2009 placeholder

--- _meta/data-raw/ctu_population_2005_2010.R
+++ _meta/data-raw/ctu_population_2005_2010.R
@@ -1,20 +1,6 @@
 source("R/_load_pkgs.R")
 
-# ctu_estimates <- readxl::read_xls("_meta/data-raw/2009PopulationEstimates.xls") %>% 
-#   select(
-#     1:3
-#   )
-# ctu_list <- vector(mode = "list", length = 8)
-# 
-# ctu_list[[1]] <- slice(ctu_estimates, 5:26) %>%
-#   mutate(county = "Anoka")
-# see clean_tabula_tables
-# is this the best way to do it? maybe not but it's how i'm doing it
-# this section on hold until we can hook it up to FRED
-
-source("N:/CommDev/Research/Research/FRED/passwords.R")
-
-ctu_estimates_2005 <- councilR::import_from_FRED("COMMUNITY_DESIGNATION")
+# Matt working on 2009-2010
 
 # 2011-2019
 ctu_estimates_2011 <- readxl::read_xlsx("_meta/data-raw/IntercensalEstimates.xlsx") %>% 
