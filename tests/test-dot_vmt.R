fe19b27ba9a80ae9a55d1a44dfa601928a857ffd
Liz Roten <liz.roten@metc.state.mn.us>
2024-11-14 13:34:36 -0600

    update DOT VMT tests

--- tests/test-dot_vmt.R
+++ tests/test-dot_vmt.R
@@ -1,8 +1,15 @@
 testthat::test_that("state DOT VMT", {
   dot_vmt <- readRDS(file.path(here::here(), "_transportation/data/dot_vmt.RDS"))
-
+  
   testthat::expect_equal(nrow(dot_vmt), 235)
-
+  
+  # ensure we have all available years
+  testthat::expect_equal(
+    unique(dot_vmt$vmt_year),
+    c("2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", 
+      "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", 
+      "2017", "2018", "2019", "2020", "2021", "2022", "2023"))
+  
   # check match with PDFs
   dot_vmt %>%
     filter(
@@ -11,35 +18,16 @@ testthat::test_that("state DOT VMT", {
     ) %>%
     arrange(desc(vmt_year)) %>%
     magrittr::extract2("daily_vmt") %>%
-    testthat::expect_equal(c(1067675, 1074726, 949687, 1099233, 1068300))
-
-  # hennepin has highest annual
+    testthat::expect_equal(c(1103323, 1067675, 1074726, 949687, 1099233, 1068300))
+  
+  # Hennepin has highest annual
   dot_vmt %>%
     filter(annual_vmt == max(annual_vmt)) %>%
     magrittr::extract2("county_name") %>%
     testthat::expect_equal("Hennepin")
+  
+  dot_vmt$vmt_year %>% unique() %>% dput
+  
 })
 
 
-testthat::test_that("VMT have decreased since including only major categories", {
-  # skip this test if offline
-  testthat::skip_if_offline()
-  
-  # download specific version of epa_onroad_emissions_compile from GitHub
-  # Commit ID 17b4349fc5874562befaa56a85ef3f740bb1708a
-  file_name <- tempfile()
-  download.file("https://github.com/Metropolitan-Council/ghg-cprg/raw/17b4349fc5874562befaa56a85ef3f740bb1708a/_transportation/data/dot_vmt.RDS",
-                destfile = file_name, quiet = TRUE, mode = "wb"
-  )
-  
-  
-  prev_dot_vmt <- readRDS(file_name)
-  dot_vmt <- readRDS(file.path(here::here(), "_transportation/data/dot_vmt.RDS"))
-  
-  left_join(prev_dot_vmt, dot_vmt,
-            by = c("vmt_year", "geoid", "county_name", "cprg_area", "data_source")) %>% View
-  
-  waldo::compare(prev_dot_vmt, dot_vmt)
-  
-  
-})
