5aa9ce883857a97524cfaac7f7460dcb8c02ba4f
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2024-12-26 12:06:04 -0600

    small clarifiers

--- _waste/data-raw/solid_waste/02_compile_incineration_MN_allyrs.R
+++ _waste/data-raw/solid_waste/02_compile_incineration_MN_allyrs.R
@@ -7,7 +7,7 @@ if (!exists("mpca_score")) {
 # assign factors
 fcc <- .4 # fraction of carbon content in MSW, IPCC default
 ffc <- .4 # fraction of fossil carbon in MSW, IPCC default
-co2_factor <- fcc * ffc * 44 / 12
+co2_factor <- fcc * ffc * 44 / 12 # atomic weight of CO2:C
 co2_efficiency_wte <- .95 # efficiency of combustion for incineration, IPCC default
 co2_efficiency_onsite <- .71 # efficiency of combustion for onsite burning, GHG Protocol default (IPCC does not provide one)
 n2o_emissions_factor_wte <- 50 # aggregate emissions factor for incineration, g N2O/metric tons waste, GHG Protocol default
@@ -16,18 +16,21 @@ n2o_emissions_factor_onsite <- 150 # aggregate emissions factor for open burning
 incin_factors <- tibble(
   source = c("WTE", "Onsite"),
   co2 = co2_factor * c(co2_efficiency_wte, co2_efficiency_onsite),
-  n2o = 10^(-6) * c(n2o_emissions_factor_wte, n2o_emissions_factor_onsite)
+  n2o = c(n2o_emissions_factor_wte, n2o_emissions_factor_onsite) *
+    units::as_units("gram") %>%
+    units::set_units("metric_ton") %>% 
+    as.numeric()
 )
 
 incineration_emissions <- mpca_score %>%
   filter(source %in% c("Waste to energy", "Onsite")) %>%
   left_join(incin_factors, by = join_by(source)) %>%
   mutate(
-    "Tonnes CO2" = value_activity * co2,
-    "Tonnes N2O" = value_activity * n2o
+    "Metric tons CO2" = value_activity * co2,
+    "Metric tons N2O" = value_activity * n2o
   ) %>%
   pivot_longer(
-    c("Tonnes CO2", "Tonnes N2O"),
+    c("Metric tons CO2", "Metric tons N2O"),
     names_to = "units_emissions",
     values_to = "value_emissions"
   ) %>%
