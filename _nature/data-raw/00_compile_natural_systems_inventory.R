b03c3a73056cb525a3b9eb396b32ea0748e35cb9
ksmiff33 <kenneth.smith13@gmail.com>
2024-12-27 12:29:22 -0600

    Updated for new waterways functions

--- _nature/data-raw/00_compile_natural_systems_inventory.R
+++ _nature/data-raw/00_compile_natural_systems_inventory.R
@@ -1,18 +1,37 @@
 rm(list=ls())
 
+# 01 ----------------------------------------------------------------------
 # 01_compile_land_cover_carbon.R
 # Compiles sequestration rates by land cover type from peer-reviewed literature
 source("_nature/data-raw/01_compile_land_cover_carbon.R")
 
+# 01_compile_waterways_carbon.R
+# Compiles methane emissions factors by waterway type from MPCA estimates
+source("_nature/data-raw/01_compile_waterways_carbon.R")
+
+
+
+# 02 ----------------------------------------------------------------------
 # 02_compile_nlcd_land_cover.R
 # Takes annual NLCD images and estimates land area by land cover type
-# Note this is memory and time-intensive
+# NOTE: this is memory and time-intensive
 source("_nature/data-raw/02_compile_nlcd_land_cover.R")
 
+# 02_compile_nhd_waterways.R
+# Use National Hydrography Dataset to compile waterways for the region
+# NOTE: Currently relies on FedData package, may want to change that
+source("_nature/data-raw/02_compile_nhd_waterways.R")
+
+
+
+# 03 ----------------------------------------------------------------------
 # 03_compile_land_area_sequestration_from_nlcd.R
 # Applies sequestration rates to area estimates of land cover type to summarize
 # C sequestration at the county and ctu scale
 source("_nature/data-raw/03_compile_land_area_sequestration_from_nlcd.R")
 
-
+# 03_compile_waterways_emissions_from_nhd.R
+# Applies emissions factors to area estimates of waterway type to summarize
+# CH4 emissions at the county and ctu scale
+source("_nature/data-raw/03_compile_waterways_emissions_from_nhd.R")
 
