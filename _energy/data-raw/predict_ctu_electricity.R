cdd7abc5f6f4f85aee3d8a859dcf99dba51ff26c
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-01-17 16:09:21 -0600

    renv and load package update

--- _energy/data-raw/predict_ctu_electricity.R
+++ _energy/data-raw/predict_ctu_electricity.R
@@ -2,7 +2,6 @@
 
 source("R/_load_pkgs.R")
 source("_energy/data-raw/_energy_emissions_factors.R")
-library(randomForest)
 cprg_ctu <- read_rds("_meta/data/cprg_ctu.RDS") %>%
   filter(!county_name %in% c("Chisago", "Sherburne", "St. Croix", "Pierce"))
 
