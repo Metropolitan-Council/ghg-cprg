e06c5624b9b3447559c8f76570ff2db770933c6f
LimerickSam <150163967+LimerickSam@users.noreply.github.com>
2025-01-27 09:22:40 -0600

    Update _run_energy.R with call to Xcel script

--- _energy/data-raw/_run_energy.R
+++ _energy/data-raw/_run_energy.R
@@ -13,16 +13,18 @@ source("_energy/data-raw/minnesota_electricUtilities.R")
 source("_energy/data-raw/wisconsin_natGasUtilities.R")
 source("_energy/data-raw/wisconsin_electricUtilities.R")
 
-# next, run processing for MN and WI activity data
-
+# next, run processing for MN and WI county-level activity data (nat gas, then elec), derived from utility reports to State
 source("_energy/data-raw/minnesota_7610reporting_natGas.R")
 source("_energy/data-raw/wisconsin_natGas_estimate_2005_and_2021.R")
 
-# finalize processing
 source("_energy/data-raw/minnesota_7610reporting_electricity.R")
 source("_energy/data-raw/wisconsin_elec_estimate_2021.R")
 
 source("_energy/data-raw/nrel_slope_energy.R")
+
+# city-level activity data processing
+source("_energy/data-raw/minnesota_xcelCommunityReports_electricity.R")
+
 
 # Done!
 
