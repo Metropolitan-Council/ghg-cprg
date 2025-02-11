87f0c836ed065f2a0ad8d9ab1e07d6a00090c28d
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-02-11 10:21:29 -0600

    Update start_caption_index.R

--- R/start_caption_index.R
+++ R/start_caption_index.R
@@ -1,2 +1,6 @@
 # start caption index
 saveRDS("00", file.path(here::here(), "caption_index.RDS"))
+
+if(!fs::dir_exists(file.path(here::here(), "assets/captions"))){
+  fs::dir_create(file.path(here::here(), "assets/captions"))
+}
