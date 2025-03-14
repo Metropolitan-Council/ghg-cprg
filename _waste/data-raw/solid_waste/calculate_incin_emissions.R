f8c08d3d773822b5cf5760edd34ce067902ab1bd
Zoey Yandell <Zoey.Yandell@metc.state.mn.us>
2025-03-14 17:19:07 -0500

    create incineration function

--- _waste/data-raw/solid_waste/calculate_incin_emissions.R
+++ _waste/data-raw/solid_waste/calculate_incin_emissions.R
@@ -5,5 +5,37 @@
 #' units_activity, value_emissions, and units_emissions
 #' 
 calculate_incin_emissions <- function(solid_waste_data){
+  # assign factors
+  fcc <- .4 # fraction of carbon content in MSW, IPCC default
+  ffc <- .4 # fraction of fossil carbon in MSW, IPCC default
+  co2_factor <- fcc * ffc * 44 / 12 # atomic weight of CO2:C
+  co2_efficiency_wte <- .95 # efficiency of combustion for incineration, IPCC default
+  co2_efficiency_onsite <- .71 # efficiency of combustion for onsite burning, GHG Protocol default (IPCC does not provide one)
+  n2o_emissions_factor_wte <- 50 # aggregate emissions factor for incineration, g N2O/metric tons waste, GHG Protocol default
+  n2o_emissions_factor_onsite <- 150 # aggregate emissions factor for open burning, g N2O/metric tons waste, GHG Protocol default
   
+  incin_factors <- tibble::tibble(
+    source = c("Waste to energy", "Onsite"),
+    co2 = co2_factor * c(co2_efficiency_wte, co2_efficiency_onsite),
+    n2o = c(n2o_emissions_factor_wte, n2o_emissions_factor_onsite) *
+      units::as_units("gram") %>%
+      units::set_units("metric_ton") %>%
+      as.numeric()
+  )
+  
+  incineration_emissions <- solid_waste_data %>%
+    dplyr::filter(source %in% c("Waste to energy", "Onsite")) %>%
+    dplyr::left_join(incin_factors, by = dplyr::join_by(source)) %>%
+    dplyr::mutate(
+      "Metric tons CO2" = value_activity * co2,
+      "Metric tons N2O" = value_activity * n2o
+    ) %>%
+    tidyr::pivot_longer(
+      c("Metric tons CO2", "Metric tons N2O"),
+      names_to = "units_emissions",
+      values_to = "value_emissions"
+    ) %>%
+    dplyr::select(
+      -c(state_total, co2, n2o)
+    )
 }
