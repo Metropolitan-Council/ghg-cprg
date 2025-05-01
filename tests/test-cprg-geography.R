5d57a63d43d1575bf88f2ebe1ec9bc2fc088aaec
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-05-01 10:59:47 -0500

    Update test-cprg-geography.R

--- tests/test-cprg-geography.R
+++ tests/test-cprg-geography.R
@@ -27,7 +27,7 @@ testthat::test_that("CTU data is as expected", {
   testthat::expect_equal(nrow(cprg_ctu), 288)
   testthat::expect_equal(names(cprg_ctu), c("ctu_name", "ctu_class", "county_name", "state_name", "statefp", 
                                             "state_abb", "geoid_wis", "gnis", "cprg_area", "geometry", "ctu_id", 
-                                            "thrive_designation"))
+                                            "thrive_designation", "imagine_designation"))
 
   testthat::expect_equal(cprg_ctu$state_name %>% unique(), c("Minnesota", "Wisconsin"))
   testthat::expect_equal(nrow(filter(ctu_co_crosswalk, is.na(geog_unit_id.parent))), 0)
