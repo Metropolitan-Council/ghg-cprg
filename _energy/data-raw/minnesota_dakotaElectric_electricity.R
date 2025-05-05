7f536b1117907d989496c330cec9c4f7fec8e169
LimerickSam <150163967+LimerickSam@users.noreply.github.com>
2025-05-05 11:36:41 -0500

    Clean  up raw Dakota provided data

--- _energy/data-raw/minnesota_dakotaElectric_electricity.R
+++ _energy/data-raw/minnesota_dakotaElectric_electricity.R
@@ -2,4 +2,26 @@
 source("R/_load_pkgs.R")
 
 # Read city data
-city_raw <- read_xlsx(here("_energy", "data-raw", "dakotaElectricDataRequest", "METC_DEA_Usage_Breakdown.xlsx"))
+city_raw <- read_xlsx(here("_energy", "data-raw", "dakotaElectricDataRequest", "METC_DEA_Usage_Breakdown.xlsx")) %>%
+  mutate(
+    ctu_name = str_to_title(Municipality),
+    sector = case_when(
+      CUSTGROUP == 'C&I' ~ 'Commercial/Industrial',
+      CUSTGROUP == 'COM' ~ 'Commercial',
+      CUSTGROUP == 'DEA' ~ 'Dakota Electric Operations',
+      CUSTGROUP == 'IRR' ~ 'Irrigation Services',
+      CUSTGROUP == 'RES' ~ 'Residential',
+    ),
+    mwh_delivered = `Usage (kWh)`/ 1000,
+    utility = 'Dakota Electric'
+  ) %>%
+  select(
+    ctu_name,
+    ctu_class = MunicipalityClass,
+    year = ReportingYear,
+    sector,
+    customer_count = `Count Services`,
+    mwh_delivered
+  ) %>%
+  # Dakota's 2018 data is faulty -- starts in March
+  filter(!year == 2018)
