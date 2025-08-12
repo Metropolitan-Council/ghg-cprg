c4b286167284ce88d70a17eb1504aeef8105c15b
Liz Roten <liz.roten@metc.state.mn.us>
2025-08-12 14:48:03 -0500

    re-name

--- _transportation/data-raw/rtdm_forecast_ctu_vmt.R
+++ _transportation/data-raw/rtdm_forecast_ctu_vmt.R
@@ -4,12 +4,9 @@
 # Contact us for more detail.
 # 
 source("R/_load_pkgs.R")
+cprg_ctu <- readRDS("_meta/data/cprg_ctu.RDS")
+cprg_county <- readRDS("_meta/data/cprg_county.RDS")
 
 ctu_vmt_forecast <- readRDS("_transportation/data-raw/metc_travel_model/ctu_vmt_forecast.RDS")
+
 county_vmt_forecast <- readRDS("_transportation/data-raw/metc_travel_model/county_vmt_forecast.RDS")
-
-mndot_vmt_ctu <- readRDS("_transportation/data/mndot_vmt_ctu.RDS")
-mndot_vmt_county <- readRDS("_transportation/data/dot_vmt.RDS") %>% 
-  filter(data_source == "MnDOT")
-
-# find the proportion of 
