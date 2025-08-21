e801d6bdaa82371666354ef4a677c01eb5153bf7
Liz Roten <liz.roten@metc.state.mn.us>
2025-08-21 12:16:12 -0500

    centralize county-ctu-coctu index

--- _transportation/data-raw/vmt_model_data.R
+++ _transportation/data-raw/vmt_model_data.R
@@ -6,54 +6,7 @@ source("R/_load_pkgs.R")
 ctu_urbansim <- read_rds("_meta/data/urbansim_data.RDS") %>%
   # remove mystery COCTU
   filter(coctu_id_gnis != "13900649741")
-
-cprg_county <- readRDS("_meta/data/cprg_county.RDS")
-
-ctu_population <- readRDS("_meta/data/ctu_population.RDS") %>%
-  left_join(
-    cprg_county %>% sf::st_drop_geometry(),
-    join_by(geoid)
-  )
-
-cprg_ctu <- readRDS("_meta/data/cprg_ctu.RDS") %>%
-  left_join(
-    cprg_county %>% sf::st_drop_geometry(),
-    join_by(county_name, state_name, statefp, state_abb, cprg_area)
-  ) %>%
-  mutate(ctu_name_full = paste0(ctu_name, ", ", ctu_class)) %>%
-  left_join(
-    ctu_population %>%
-      select(geoid, gnis, coctu_id_fips, coctu_id_gnis) %>%
-      unique(),
-    by = c("gnis", "geoid")
-  ) %>%
-  filter(state_abb == "MN")
-
-# create index with all contextual information for each coctu_id_gnis
-# independent of year
-ctu_coctu_index <- ctu_population %>%
-  mutate(
-    ctu_name_full = paste0(ctu_name, ", ", ctu_class),
-    ctu_name_full_county = paste0(ctu_name_full, ", ", county_name)
-  ) %>%
-  # remove credit river township!
-  filter(
-    coctu_id_gnis != "13900663886",
-    coctu_id_gnis != "03700664099"
-  ) %>%
-  select(geoid, gnis, coctu_id_gnis, ctu_name, ctu_name_full, ctu_name_full_county, county_name) %>%
-  unique() %>%
-  left_join(
-    cprg_ctu %>%
-      select(ctu_name, gnis, imagine_designation) %>% sf::st_drop_geometry() %>% unique(),
-    by = join_by(gnis, ctu_name)
-  ) %>%
-  mutate(imagine_designation = case_when(
-    coctu_id_gnis == "03700664099" ~ "Agricultural", # Empire Township
-    coctu_id_gnis == "13900663886" ~ "Rural Residential", # Credit River township
-    TRUE ~ imagine_designation
-  ))
-
+source("_meta/data-raw/ctu_coctu_index.R")
 
 # create year and county series, all years
 ctu_year_series <- ctu_coctu_index %>%
