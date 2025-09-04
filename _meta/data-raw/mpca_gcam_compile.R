6ec57bef8a9741c191c18b377f0136cf8f348488
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-09-04 17:37:13 -0500

    update state gcam processing

--- _meta/data-raw/mpca_gcam_compile.R
+++ _meta/data-raw/mpca_gcam_compile.R
@@ -5,28 +5,29 @@ demographer_state_population <- readRDS("_meta/data/state_population_demographer
 # for comparison
 county_emissions <- readRDS("_meta/data/cprg_county_emissions.RDS")
 
-state_projections <- read_xlsx("_meta/data-raw/Minnesota GCAM Modeling Data.xlsx") %>%
+state_projections <- read_xlsx("_meta/data-raw/gcam_all_scenarios.xlsx",
+                               sheet = "Emissions by Inventory Category") %>%
   pivot_longer(
     cols = 6:27,
     names_to = "emissions_year",
-    values_to = "values_emissions"
+    values_to = "value_emissions"
   ) %>%
   clean_names() %>%
   # sum different GHG gases
   group_by(scenario, region, sector, source, units, emissions_year) %>%
-  summarize(values_emissions = sum(values_emissions)) %>%
+  summarize(value_emissions = sum(value_emissions)) %>%
   ungroup()
 
 scenarios_annual <- state_projections %>%
-  filter(sector != "Offsets Needed") %>%
-  mutate(source_sink = if_else(values_emissions < 0, "Sequestration", "Emission")) %>%
+  filter(sector != "Emission Reductions Needed") %>%
+  mutate(source_sink = if_else(value_emissions < 0, "Sequestration", "Emission")) %>%
   group_by(emissions_year, scenario, source_sink, units) %>%
-  summarize(values_emissions = sum(values_emissions)) %>%
+  summarize(value_emissions = sum(value_emissions)) %>%
   #### add proportion relative to 2005
   group_by(scenario, source_sink, units) %>%
-  mutate(value_2005 = values_emissions[emissions_year == 2005]) %>%
+  mutate(value_2005 = value_emissions[emissions_year == 2005]) %>%
   # Calculate the proportion
-  mutate(proportion_of_2005 = values_emissions / value_2005) %>%
+  mutate(proportion_of_2005 = value_emissions / value_2005) %>%
   ungroup() %>%
   select(-value_2005) %>%
   rename(units_emission = units)
@@ -38,7 +39,7 @@ scenarios_annual_meta <-
     "scenario", class(scenarios_annual$scenario), "Modeled scenario - MN GCAM",
     "source_sink", class(scenarios_annual$source_sink), "Emissions or sequestration activities",
     "units_emission", class(scenarios_annual$units_emission), "Units of emissions or sequestration",
-    "values_emissions", class(scenarios_annual$values_emissions), "Value of emissions or sequestration",
+    "value_emissions", class(scenarios_annual$value_emissions), "Value of emissions or sequestration",
     "proportion_of_2005", class(scenarios_annual$proportion_of_2005), "Proportion of emissions or sequestration relative to 2005 baseline"
   )
 
@@ -47,15 +48,15 @@ saveRDS(scenarios_annual_meta, "_meta/data/gcam/mpca_economy_wide_gcam_meta.RDS"
 
 
 scenarios_sector_annual <- state_projections %>%
-  filter(sector != "Offsets Needed") %>%
-  mutate(source_sink = if_else(values_emissions < 0, "Sequestration", "Emission")) %>%
+  filter(sector != "Emission Reductions Needed") %>%
+  mutate(source_sink = if_else(value_emissions < 0, "Sequestration", "Emission")) %>%
   group_by(emissions_year, scenario, source_sink, sector, units) %>%
-  summarize(values_emissions = sum(values_emissions)) %>%
+  summarize(value_emissions = sum(value_emissions)) %>%
   #### add proportion relative to 2005
   group_by(scenario, source_sink, sector, units) %>%
-  mutate(value_2005 = values_emissions[emissions_year == 2005]) %>%
+  mutate(value_2005 = value_emissions[emissions_year == 2005]) %>%
   # Calculate the proportion
-  mutate(proportion_of_2005 = values_emissions / value_2005) %>%
+  mutate(proportion_of_2005 = value_emissions / value_2005) %>%
   ungroup() %>%
   select(-value_2005) %>%
   rename(units_emission = units)
@@ -74,19 +75,20 @@ saveRDS(scenarios_sector_annual_meta, "_meta/data/gcam/mpca_sector_gcam_meta.RDS
 
 #### homogenize subsectors to match MC inventory work
 
-state_projections %>%
-  filter(sector == "Agriculture") %>%
-  distinct(source) %>%
-  print(n = 50)
-
-
-# only labeling categories where we have current emissions
-livestock <- c(
-  "American Bison", "Beef Cattle", "Dairy Cattle", "Dairy Heifers",
-  "Goats", "Horses", "Swine", "Poultry", "Sheep", "Mules and Asses"
-)
-
-cropland <- c("Biofuels", "Cereals", "Cropland") # omitting liming, mineral soils, grasslands (emitters?), rice, as these are not accounted for in our inv
+# #agriculture is modeled separately now.
+# state_projections %>%
+#   filter(sector == "Agriculture") %>%
+#   distinct(source) %>%
+#   print(n = 50)
+# 
+# 
+# # only labeling categories where we have current emissions
+# livestock <- c(
+#   "American Bison", "Beef Cattle", "Dairy Cattle", "Dairy Heifers",
+#   "Goats", "Horses", "Swine", "Poultry", "Sheep", "Mules and Asses"
+# )
+# 
+# cropland <- c("Biofuels", "Cereals", "Cropland") # omitting liming, mineral soils, grasslands (emitters?), rice, as these are not accounted for in our inv
 
 
 state_projections %>%
@@ -101,30 +103,16 @@ county_emissions %>%
 
 passenger_vehicles <- c("Light-duty trucks", "Motorcycle", "Passenger cars")
 
+
 state_projections %>%
   filter(sector == "Waste") %>%
   distinct(source) %>%
   print(n = 50)
 
 county_emissions %>%
-  filter(sector == "Energy") %>%
-  distinct(category) %>%
-  print(n = 50)
-
-waste <- c(
-  "MMSW landfills", "MMSW composting", "MMSW compost preprocessing",
-  "Yard waste composting", "Yard waste preprocessing"
-)
-
-state_projections %>%
-  filter(sector == "LULUCF", values_emissions < 0) %>%
+  filter(sector == "Waste") %>%
   distinct(source) %>%
   print(n = 50)
-
-sequestration <- c(
-  "Aboveground Biomass", "Belowground Biomass",
-  "Dead Wood", "Litter", "Mineral Soil"
-)
 
 state_projections %>%
   filter(sector == "Residential") %>%
@@ -133,14 +121,34 @@ state_projections %>%
 
 county_emissions %>%
   filter(sector == "Residential") %>%
-  distinct(category) %>%
+  distinct(source) %>%
+  print(n = 50)
+
+state_projections %>%
+  filter(sector == "Commercial") %>%
+  distinct(source) %>%
+  print(n = 50)
+
+county_emissions %>%
+  filter(sector == "Commercial") %>%
+  distinct(source) %>%
+  print(n = 50)
+
+state_projections %>%
+  filter(sector == "Industrial") %>%
+  distinct(source) %>%
+  print(n = 50)
+
+county_emissions %>%
+  filter(sector == "Industrial") %>%
+  distinct(source) %>%
   print(n = 50)
 
 state_projections <- state_projections %>%
   mutate(subsector_mc = case_when(
-    # agriculture
-    source %in% livestock ~ "Livestock",
-    source %in% cropland ~ "Cropland",
+    # # agriculture
+    # source %in% livestock ~ "Livestock",
+    # source %in% cropland ~ "Cropland",
     # transportation
     source %in% passenger_vehicles ~ "Passenger vehicles",
     source == "Heavy-duty trucks" ~ "Trucks",
@@ -151,34 +159,33 @@ state_projections <- state_projections %>%
     # commercial
     sector == "Commercial" & source == "Natural gas" ~ "Commercial natural gas",
     sector == "Commercial" & source %in% c(
-      "Natural gas",
       "Biofuel",
       "Coal",
       "Refined Liquids"
     ) ~ "Commercial fuel combustion",
     # industrial
-    sector == "Industry" & source == "Natural gas" ~ "Industrial natural gas",
-    sector == "Industry" & source %in% c("Coal", "Refined Liquids") ~ "Industrial fuel combustion",
-    sector == "Industry" & source %in% c("Oil refining") ~ "Refinery processes",
-    sector == "Industry" & source %in% c(
+    sector == "Industrial" & source == "Natural gas" ~ "Industrial natural gas",
+    sector == "Industrial" & source %in% c("Coal", "Oil", "Propane", "Refined Liquids") ~ "Industrial fuel combustion",
+    sector == "Industrial" & source %in% c("Oil refining") ~ "Refinery processes",
+    sector == "Industrial" & source %in% c(
       "Magnesium casting",
       "Other industrial process",
       "Semiconductor manufacture",
       "Paraffinic wax consumption"
     ) ~ "Industrial processes",
     # waste
-    source %in% waste ~ "Solid waste",
+    source %in% c("Landfills","RDF", "Incineration") ~ "Solid waste",
     source == "Wastewater treatment" ~ "Wastewater",
     # electricity
     sector == "Electricity" ~ "Electricity",
-    # natural systems
-    source %in% sequestration & sector != "Agriculture" ~ "Sequestration",
-    source == "Urban Trees" ~ "Urban tree",
+    # # natural systems
+    # source %in% sequestration & sector != "Agriculture" ~ "Sequestration",
+    # source == "Urban Trees" ~ "Urban tree",
     TRUE ~ "not_inventoried"
   )) %>%
   mutate(subsector_mc = case_when(
-    subsector_mc == "not_inventoried" & values_emissions < 0 ~ "sequestration_not_inventoried",
-    subsector_mc == "not_inventoried" & values_emissions >= 0 ~ "emissions_not_inventoried",
+    subsector_mc == "not_inventoried" & value_emissions < 0 ~ "sequestration_not_inventoried",
+    subsector_mc == "not_inventoried" & value_emissions >= 0 ~ "emissions_not_inventoried",
     TRUE ~ subsector_mc
   ))
 
@@ -187,20 +194,20 @@ county_emissions %>%
   print(n = 50)
 
 scenarios_sources_annual <- state_projections %>%
-  filter(sector != "Offsets Needed") %>%
-  mutate(source_sink = if_else(values_emissions < 0, "Sequestration", "Emission")) %>%
+  filter(sector != "Emission Reductions Needed") %>%
+  mutate(source_sink = if_else(value_emissions < 0, "Sequestration", "Emission")) %>%
   group_by(emissions_year, scenario, source_sink, sector, subsector_mc, units) %>%
-  summarize(values_emissions = sum(values_emissions)) %>%
+  summarize(value_emissions = sum(value_emissions)) %>%
   #### add proportion relative to 2005
   group_by(scenario, subsector_mc, source_sink, sector, units) %>%
   mutate(
-    value_2005 = values_emissions[emissions_year == 2005],
-    value_2020 = values_emissions[emissions_year == 2020]
+    value_2005 = value_emissions[emissions_year == 2005],
+    value_2020 = value_emissions[emissions_year == 2020]
   ) %>%
   # Calculate the proportion
   mutate(
-    proportion_of_2005 = values_emissions / value_2005,
-    proportion_of_2020 = values_emissions / value_2020
+    proportion_of_2005 = value_emissions / value_2005,
+    proportion_of_2020 = value_emissions / value_2020
   ) %>%
   ungroup() %>%
   select(-value_2005) %>%
