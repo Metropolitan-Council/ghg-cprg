7926d1e5c7985cc853e62ffc7b11c55ad19b9dc9
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-09-23 09:37:43 -0500

    add some elements

--- _meta/data-raw/compile_emissions_by_gas.R
+++ _meta/data-raw/compile_emissions_by_gas.R
@@ -1,9 +1,17 @@
 # compile emissions by gas type from all sectors into a single data table
 source("R/_load_pkgs.R")
 cprg_county <- readRDS("_meta/data/cprg_county.RDS")
+ef_hub <- readRDS("_meta/data/epa_ghg_factor_hub.RDS")
 
-
-transportation_gas <- 
+transportation_gas <- readRDS("_transportation/data/epa_onroad_emissions_compile.rds") %>% 
+  filter(emissions_year == 2022,
+         pollutant_code %in% c("CO2",
+                               "CH4",
+                               "N2O")) %>% 
+  mutate(mt_gas = as.numeric(emissions * units::as_units("gram") %>%
+      units::set_units("metric_ton")
+  )) %>% 
+  select(emissions_year, county_name, fuel_type, vehicle_type, pollutant_code, mt_gas)
 
 ## already by gas
 agriculture_gas <- readRDS("_agriculture/data/agricultural_emissions_county.rds")
@@ -18,8 +26,10 @@ waste_gas <- bind_rows(readRDS("_waste/data/solid_waste_MN_by_gas.RDS"),
 wastewater_gas <- readRDS("_waste/data/final_wastewater_allyrs.RDS")
 
 
-building_gas
+building_gas <- 
 
+building_elec <-
+  
 industrial_gas <- bind_rows(
   readRDS("_industrial/data/fuel_combustion_emissions_by_gas.RDS"),
   readRDS("_industrial/data/mpca_fuel_emissions_by_gas.RDS"),
