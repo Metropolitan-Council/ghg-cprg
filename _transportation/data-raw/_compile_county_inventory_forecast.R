90fb2e1e626e64a500d4e794394b8c58d178550a
Liz Roten <liz.roten@metc.state.mn.us>
2025-09-24 16:17:43 -0500

    create collar county projections based on change in similar county
    Carver is most similar, so 15% reduction from 2022 emissions is used for 2050 emissions

--- _transportation/data-raw/_compile_county_inventory_forecast.R
+++ _transportation/data-raw/_compile_county_inventory_forecast.R
@@ -3,6 +3,7 @@
 
 source("R/_load_pkgs.R")
 onroad_emissions <- readRDS("_transportation/data/onroad_emissions.RDS")
+collar_county_emissions_change <- readRDS("_transportation/data/rtdm_collar_county_emissions_change.RDS")
 county_emissions_forecast_meta <- readRDS("_transportation/data/rtdm_county_emissions_forecast_meta.RDS")
 
 onroad_emissions_forecast <- readRDS("_transportation/data/rtdm_county_emissions_forecast.RDS") %>%
@@ -16,7 +17,9 @@ onroad_emissions_forecast <- readRDS("_transportation/data/rtdm_county_emissions
     emissions_metric_tons_co2e = sum(emissions_metric_tons_co2e),
     .groups = "keep"
   ) %>%
-  select("county_name", "geoid", "emissions_year", "data_source", "emissions_metric_tons_co2e")
+  select("county_name", "geoid", "emissions_year", "data_source", "emissions_metric_tons_co2e") %>% 
+  bind_rows(collar_county_emissions_change %>% 
+              mutate(data_source = "Expected change in emissions in similar county applied to 2022 value"))
 
 
 onroad_emissions_summary <- onroad_emissions %>%
