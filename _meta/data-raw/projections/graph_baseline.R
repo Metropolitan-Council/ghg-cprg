9933909928ba5bb46e372bd12c05b7b83b5eef25
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-10-31 16:00:35 -0500

    update x axis text size

--- _meta/data-raw/projections/graph_baseline.R
+++ _meta/data-raw/projections/graph_baseline.R
@@ -415,6 +415,8 @@ plot_county_emissions <- function(
     theme_minimal() +
     theme(
       axis.ticks.x = element_blank(),
+      axis.text.y = element_text(size = 14),
+      axis.text.x = element_text(size = 14),
       panel.grid.major = element_line(
         color = "#3A3A3A",
         size = 0.75,
