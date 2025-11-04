b053fbe8eede5ae7e297283a696630f1b33cc8b1
Peter Wilfahrt <Peter.Wilfahrt@metc.state.mn.us>
2025-11-04 14:46:20 -0600

    update economy ppp script

--- _meta/data-raw/projections/06_compile_economy_ppp_emissions.R
+++ _meta/data-raw/projections/06_compile_economy_ppp_emissions.R
@@ -134,6 +134,17 @@ bau_line <- pathways %>%
   summarize(value_emissions = sum(value_emissions), .groups = "keep") %>%
   ungroup()
 
+ppp_line <- pathways %>%
+  filter(
+    scenario == "ppp",
+    value_emissions > 0,
+    emissions_year > 2022
+  ) %>%
+  group_by(emissions_year) %>%
+  summarize(value_emissions = sum(value_emissions), .groups = "keep") %>%
+  ungroup()
+
+
 pathways_pos <- pathways %>%
   filter(
     emissions_year <= 2025 | scenario == "ppp",
@@ -157,6 +168,9 @@ pathways_neg <- pathways %>%
   )
 
 sector_colors <- unlist(sector_colors_alt)
+
+text_bau <- bau_line %>% filter(emissions_year == 2050) %>% pull(value_emissions)
+text_ppp <- ppp_line %>% filter(emissions_year == 2050) %>% pull(value_emissions)
 
 
 ppp_projection_plot <- ggplot() +
@@ -182,13 +196,17 @@ ppp_projection_plot <- ggplot() +
     aes(x = emissions_year, y = value_emissions),
     linetype = "dashed", color = "black", linewidth = 1
   ) +
-  geom_point(
-    data = data.frame(emissions_year = 2050, value_emissions = net_zero),
-    aes(x = emissions_year, y = value_emissions),
-    shape = "*", # asterisk
-    size = 12, # make larger or smaller
-    stroke = 1.5, # line thickness of the asterisk
-    color = "black"
+  geom_line(
+    data = ppp_line,
+    aes(x = emissions_year , y = value_emissions),  # Remove linetype aesthetic
+    color = "black", linewidth = 1, linetype = "dotted"  # Set linetype directly
+  ) +
+  
+  #net-zero bar
+  geom_segment(
+    aes(x = 2049.5, xend = 2051.5, 
+        y = net_zero, yend = net_zero),
+    color = "black", linewidth = 1.5, linetype = "solid"
   ) +
   # Add labels and formatting
   labs(
@@ -200,10 +218,14 @@ ppp_projection_plot <- ggplot() +
   geom_hline(yintercept = 0, color = "black", linetype = "solid", alpha = 0.7) +
   # Clean theme
   theme_minimal() +
+  scale_x_continuous(
+    limits = c(2005, 2059),
+    breaks = seq(2010, 2059, by = 10)
+  ) +
   theme(
     plot.title = element_text(size = 22, face = "bold"),
     plot.subtitle = element_text(size = 18),
-    legend.position = "right",
+    legend.position = "left",
     legend.text = element_text(size = 16),
     legend.title = element_text(size = 18),
     axis.title = element_text(size = 16),
@@ -212,6 +234,19 @@ ppp_projection_plot <- ggplot() +
     axis.text = element_text(size = 14, color = "black"),
     plot.margin = ggplot2::margin(5.5, 5.5, 30, 5.5, "pt")
   ) +
+  # Add text annotation for BAU
+  annotate("text", x = 2050.5, y = text_bau, 
+           label = "Business-as-usual",
+           size = 5, hjust = 0, vjust = 0.5, fontface = "bold") +
+  
+  annotate("text", x = 2050.5, y = text_ppp, 
+           label = "Potential policy pathway",
+           size = 5, hjust = 0, vjust = 0.5, fontface = "bold") +
+  
+  # for net-zero bar
+  annotate("text", x = 2052, y = net_zero, 
+           label = "Net zero target",
+           size = 5, hjust = 0, vjust = 0, fontface = "bold") +
   # Format y-axis to show values in scientific notation or scaled
   scale_y_continuous(labels = scales::comma_format(scale = 1e-6, suffix = "M"))
 
