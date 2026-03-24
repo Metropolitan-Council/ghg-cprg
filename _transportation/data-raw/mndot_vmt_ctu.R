402f87e9088bffdcee27b86dacc9d4ee15dad430
Liz Roten <liz.roten@metc.state.mn.us>
2026-03-24 12:45:30 -0500

    only assign non-municipal Ramsey to White Bear Township starting in 2017

--- _transportation/data-raw/mndot_vmt_ctu.R
+++ _transportation/data-raw/mndot_vmt_ctu.R
@@ -975,7 +975,9 @@ white_bear_township <- vmt_city_raw %>%
   filter(
     correct_county_name == "Ramsey",
     ctu_name == "Nonmunicipal",
-    year >= 2006
+    #  Blaine-Ramsey county starts being reported in 2017. 
+    #  Only assign the non-municipal data to White Bear Township for years 2017 onward
+    year >= 2017
   ) %>%
   group_by(
     year, correct_county_name, ctu_name, ctu_name_full,
